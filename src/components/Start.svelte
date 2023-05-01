98aefe9e8e2f76e51031835c3998b739f7cd6e16
Rémi Angot <remi.angot@gmail.com>
2023-05-01 19:17:34 +0100

    style: suppression console.log

--- src/components/Start.svelte
+++ src/components/Start.svelte
@@ -208,9 +208,6 @@
     filteredReferentiel = Object.assign(keysToBeFirst, filteredReferentiel)
     referentielMap = toMap(filteredReferentiel)
     arrayReferentielFiltre = Array.from(referentielMap, ([key, obj]) => ({ key, obj }))
-    // console.log("amc? " + isAmcOnlySelected + " / interactif? " + isInteractiveOnlySelected)
-    console.log("tableau pour menu : ")
-    console.log(arrayReferentielFiltre)
   }
   updateReferentiel()
 
