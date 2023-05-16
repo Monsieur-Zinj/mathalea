54a459e3af591f3681ac8ade3ba88bc7c1f737b9
sylcha <sylvain.chambon@ac-toulouse.fr>
2023-05-16 22:56:10 +0200

    feat: composant SideMenuOutils

--- src/components/OutilsProf.svelte
+++ src/components/OutilsProf.svelte
@@ -5,9 +5,9 @@
   import { exercicesParams, globalOptions, darkMode, isExportMenuVisible, isSettingsMenuVisible, isSideMenuVisible, selectedExercises, isInIframe } from "./store"
   import referentiel from "../json/referentielProfs.json"
   import { onMount } from "svelte"
-  import { toMap } from "./utils/toMap"
   import { mathaleaUpdateExercicesParamsFromUrl, mathaleaUpdateUrlFromExercicesParams } from "../lib/mathalea"
   import EntreeListeOutils from "./outils/EntreeListeOutils.svelte"
+  import SideMenuOutils from "./outils/SideMenuOutils.svelte"
 
   //   const entrees = toMap(referentiel)
   let isMenuOpen: boolean = true
@@ -47,30 +47,5 @@
       <NavBarV2 subtitle="Outils pour la classe" subtitleType="design" />
     </div>
   {/if}
-  <aside
-    class="relative {isMenuOpen
-      ? '-translate-x-0'
-      : '-translate-x-full'} transition-all duration-500 transform w-72 bg-coopmaths-canvas-dark dark:bg-coopmathsdark-canvas-dark min-h-full h-screen flex flex-col items-center pb-2"
-  >
-    <div class="w-full flex justify-start font-bold text-xl text-coopmaths-struct px-4 pt-5 pb-2">Choix des outils</div>
-    <ul class="pl-4 flex flex-col">
-      {#each arrayReferentiel as item, i}
-        <li>
-          <EntreeListeOutils outil={item} />
-        </li>
-      {/each}
-    </ul>
-    <div
-      class="absolute top-0 -right-10 {isMenuOpen
-        ? '-translate-x-full'
-        : 'translate-x-0'}  transition-all duration-500 transform inline-flex justify-center items-center rounded-r-sm h-10 w-10 bg-coopmaths-canvas-dark"
-    >
-      <button
-        type="button"
-        on:click={() => {
-          isMenuOpen = !isMenuOpen
-        }}><i class="bx {isMenuOpen ? 'bx-x' : 'bx-right-arrow-alt'} scale-150 text-coopmaths-action" /></button
-      >
-    </div>
-  </aside>
+  <SideMenuOutils referentiel={arrayReferentiel} />
 </div>
