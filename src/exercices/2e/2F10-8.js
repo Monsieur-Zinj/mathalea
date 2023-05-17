f3986006b3c737e6f78c0e2496ed7a2cf7d66ee5
moragi <86921748+moragi@users.noreply.github.com>
2023-05-17 19:09:15 +0200

    ex: 2F10-8 exercice cloné du 3F20-1 de JC

--- src/exercices/2e/2F10-8.js
+++ src/exercices/2e/2F10-8.js
@@ -1,14 +1,14 @@
-import FonctionsAffines from '../3e/3F20-8.js'
-export const titre = 'Travailler avec les fonctions affines'
-export { interactifReady, interactifType } from '../3e/3F20-8.js'
-export const dateDePublication = '16/05/2023'
-/**clone de 3F20-1
+import FonctionsAffines from '../3e/3F20-1.js'
+export const titre = 'Faire un bilan sur les fonctions affines'
+export { interactifReady, interactifType } from '../3e/3F20-1.js'
+export const dateDePublication = '17/05/2023'
+/** clone de 3F20-1
  * Questions sur les fonctions affines
  * @author Jean-Claude Lhote cloné par Gilles Mora
  * @constructor
  */
-export const uuid = ''
+export const uuid = 'c1961'
 export const ref = '2F10-8'
 export default function FonctionsAffinesS () {
-    FonctionsAffines.call(this)
+  FonctionsAffines.call(this)
 }
