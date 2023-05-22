bc10479630572cea3ba59bc397e9cfcebccf15ae
Jean-Claude Lhote <lhotejc@gmail.com>
2023-05-22 08:29:33 +0200

    fix: problème avec nbQuestions 3F20-2

--- src/exercices/3e/3F20-2.js
+++ src/exercices/3e/3F20-2.js
@@ -28,30 +28,29 @@ export default function FonctionsAffinesOuLineaires () {
   this.comment = 'L\'exercice propose de panacher les questions de 3F20 et 3F20-1'
   this.sup = 1 // coefficient entier relatif
   this.nbQuestions = 8
-  this.sup2 = this.lycee ? '11' : '9'
+  this.sup2 = '11'
   this.spacingCorr = 3
   this.sup3 = '3'
-
   this.besoinFormulaireNumerique = ['Coefficient : ', 3, '1: Coefficient entier\n2: Coefficient rationnel\n3: Mélange']
+  this.besoinFormulaire2Texte = ['Types de questions', 'Nombres séparés par des tirets :\n1: Image par expression\n2: Image par valeurs\n3: Image par graphique\n4: Antécédent par expression\n5: Antécédent par valeurs\n6: Antécédent par graphique\n7: Expression par valeurs\n8: Expression par graphique\n9: Expression par graphique fonctions affines uniquement (formule des accroissements)\n10: Expression par valeurs fonctions affines uniquement (formule des accroissements)\n11: Mélange']
   this.besoinFormulaire3Texte = ['Type de fonction', 'nombres séparés par des tirets\n1: Linéaire\n2: Affine\n3: Mélange']
   this.nouvelleVersion = function () {
     fonctionsAffines.interactif = this.interactif
+    fonctionsAffines.nbQuestions = this.nbQuestions
     fonctionsAffines.lycee = this.lycee
     fonctionsAffines.sup = this.sup
     fonctionsAffines.sup2 = this.lycee ? '11' : '9'
     fonctionsAffines.numeroExercice = this.numeroExercice // indispensable pour l'interactif
     fonctionsAffines.nouvelleVersion()
     fonctionsLineaires.interactif = this.interactif
+    fonctionsLineaires.nbQuestions = this.nbQuestions
     fonctionsLineaires.lycee = this.lycee
     fonctionsLineaires.sup = this.sup
-    fonctionsLineaires.sup2 = 9
+    fonctionsLineaires.sup2 = '9'
     fonctionsLineaires.numeroExercice = this.numeroExercice // indispensable pour l'interactif
     fonctionsLineaires.nouvelleVersion()
-
-    this.besoinFormulaire2Texte = this.lycee
-      ? ['Types de questions', 'Nombres séparés par des tirets :\n1: Image par expression\n2: Image par valeurs\n3: Image par graphique\n4: Antécédent par expression\n5: Antécédent par valeurs\n6: Antécédent par graphique\n7: Expression par valeurs\n8: Expression par graphique\n9: Expression par graphique (formule des accroissements)\n10: Expression par valeurs (formule des accroissements)\n11: Mélange']
-      : ['Types de questions', 'Nombres séparés par des tirets :\n1: Image par expression\n2: Image par valeurs\n3: Image par graphique\n4: Antécédent par expression\n5: Antécédent par valeurs\n6: Antécédent par graphique\n7: Expression par valeurs\n8: Expression par graphique\n9: Mélange']
-    this.listeQuestions = []
+  
+     this.listeQuestions = []
     this.listeCorrections = []
     this.autoCorrection = []
     const choixFonction = gestionnaireFormulaireTexte({ saisie: this.sup3, min: 1, max: 2, defaut: 3, melange: 3, listeOfCase: ['linéaire', 'affine'], nbQuestions: this.nbQuestions, shuffle: true })
