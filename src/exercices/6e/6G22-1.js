7790d01b903e7e74a39ae2860886b599e445bc71
EricElter <48183436+Deseux@users.noreply.github.com>
2023-05-23 17:12:13 +0200

    feat : 6G22-1 amélioré (consigne, paramètres, AMC)

--- src/exercices/6e/6G22-1.js
+++ src/exercices/6e/6G22-1.js
@@ -1,19 +1,21 @@
 import Exercice from '../Exercice.js'
 import { mathalea2d } from '../../modules/2dGeneralites.js'
-import { listeQuestionsToContenu, randint, lettreDepuisChiffre, combinaisonListes, choice } from '../../modules/outils.js'
+import { listeQuestionsToContenu, randint, lettreDepuisChiffre, choice, numAlpha, gestionnaireFormulaireTexte } from '../../modules/outils.js'
 import { point, tracePoint, longueur, codageAngle, demiDroite, texteParPoint, rotation, homothetie, angle } from '../../modules/2d.js'
 import { propositionsQcm } from '../../modules/interactif/questionQcm.js'
 import { context } from '../../modules/context.js'
 export const interactifReady = true
 export const interactifType = 'qcm'
-export const amcReady = false
-export const titre = 'Connaitre le vocabulaire de base des angles'
+export const amcType = 'AMCHybride'
+export const amcReady = true
+export const titre = 'Connaître le vocabulaire de base des angles'
 
 export const dateDePublication = '03/12/2022'
+export const dateDeModifImportante = '23/05/2023'
 
 /**
  * Connaissance du vocabulaire de base des angles : nom, sommet, côté
- * @author Guillaume Valmont
+ * @author Guillaume Valmont (modifié par EE pour modifier le nb de questions et rajouter un paramètre)
  * 6G22-1
 */
 export const uuid = 'e8d33'
@@ -25,148 +27,187 @@ export default class VocabulaireDeBaseDesAngles extends Exercice {
     this.correctionDetailleeDisponible = true
     this.nbColsCorr = 2
     this.nbQuestions = 4
-    this.nbQuestionsModifiable = false
+    this.sup = 5
   }
 
   nouvelleVersion () {
     this.listeQuestions = []
     this.listeCorrections = []
     this.autoCorrection = []
-    const typeQuestionsDisponibles = ['nom', 'sommet', 'cote', 'autre']
-    const listeTypeQuestions = combinaisonListes(typeQuestionsDisponibles, this.nbQuestions)
-    const objets2d = []
-    const xMin = -4
-    const xMax = 4
-    const yMin = -3
-    const yMax = 3
-    const distanceMin = 3
-    const indiceNomA = randint(1, 26)
-    const indiceNomB = randint(1, 26, [indiceNomA])
-    const indiceNomC = randint(1, 26, [indiceNomA, indiceNomB])
-    const A = point(randint(xMin, xMax), randint(yMin, yMax), lettreDepuisChiffre(indiceNomA))
-    let B = point(randint(xMin, xMax), randint(yMin, yMax), lettreDepuisChiffre(indiceNomB))
-    while (longueur(A, B) < distanceMin) {
-      B = point(randint(xMin, xMax), randint(yMin, yMax), lettreDepuisChiffre(indiceNomB))
-    }
-    let C = point(randint(xMin, xMax), randint(yMin, yMax), lettreDepuisChiffre(indiceNomC))
-    while (longueur(A, C) < distanceMin || longueur(B, C) < distanceMin || angle(A, B, C) < 10 || angle(A, B, C) > 170) {
-      C = point(randint(xMin, xMax), randint(yMin, yMax), lettreDepuisChiffre(indiceNomC))
-    }
-    const points = [A, B, C]
-    const angleABC = codageAngle(A, B, C, 2)
-    const demiDroiteBA = demiDroite(B, A)
-    const demiDroiteCA = demiDroite(B, C)
-    objets2d.push(tracePoint(...points), texteParPoint(A.nom, rotation(A, B, -10)), texteParPoint(C.nom, rotation(C, B, 10)), texteParPoint(B.nom, homothetie(B, A, 1.2)), angleABC, demiDroiteBA, demiDroiteCA)
-    // On affiche le cadre mathalea2d
-    const pointsX = []
-    const pointsY = []
-    for (const point of points) {
-      pointsX.push(point.x)
-      pointsY.push(point.y)
-    }
-    const xmin = Math.min(...pointsX) - 2
-    const xmax = Math.max(...pointsX) + 2
-    const ymin = Math.min(...pointsY) - 2
-    const ymax = Math.max(...pointsY) + 2
-    const parametres2d = { xmin, ymin, xmax, ymax, pixelsParCm: 20, scale: 0.6 }
-    this.introduction = '' + mathalea2d(parametres2d, objets2d)
+
+    const listeTypeQuestions = gestionnaireFormulaireTexte({
+      saisie: this.sup,
+      max: 4,
+      melange: 5,
+      defaut: 5,
+      shuffle: true,
+      listeOfCase: ['nom', 'sommet', 'cote', 'autre'],
+      enleveDoublons: true,
+      nbQuestions: 1
+    })
 
     for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
       texte = ''
       texteCorr = ''
-      // On construit les questions
+      const propositionsAMC = []
+      const objets2d = []
+      const xMin = -4
+      const xMax = 4
+      const yMin = -3
+      const yMax = 3
+      const distanceMin = 3
+      const indiceNomA = randint(1, 26)
+      const indiceNomB = randint(1, 26, [indiceNomA])
+      const indiceNomC = randint(1, 26, [indiceNomA, indiceNomB])
+      const A = point(randint(xMin, xMax), randint(yMin, yMax), lettreDepuisChiffre(indiceNomA))
+      let B = point(randint(xMin, xMax), randint(yMin, yMax), lettreDepuisChiffre(indiceNomB))
+      while (longueur(A, B) < distanceMin) {
+        B = point(randint(xMin, xMax), randint(yMin, yMax), lettreDepuisChiffre(indiceNomB))
+      }
+      let C = point(randint(xMin, xMax), randint(yMin, yMax), lettreDepuisChiffre(indiceNomC))
+      while (longueur(A, C) < distanceMin || longueur(B, C) < distanceMin || angle(A, B, C) < 10 || angle(A, B, C) > 170) {
+        C = point(randint(xMin, xMax), randint(yMin, yMax), lettreDepuisChiffre(indiceNomC))
+      }
+      const points = [A, B, C]
+      const angleABC = codageAngle(A, B, C, 2)
+      const demiDroiteBA = demiDroite(B, A)
+      const demiDroiteCA = demiDroite(B, C)
+      objets2d.push(tracePoint(...points), texteParPoint(A.nom, rotation(A, B, -10)), texteParPoint(C.nom, rotation(C, B, 10)), texteParPoint(B.nom, homothetie(B, A, 1.2)), angleABC, demiDroiteBA, demiDroiteCA)
+      // On affiche le cadre mathalea2d
+      const pointsX = []
+      const pointsY = []
+      for (const point of points) {
+        pointsX.push(point.x)
+        pointsY.push(point.y)
+      }
+      const xmin = Math.min(...pointsX) - 2
+      const xmax = Math.max(...pointsX) + 2
+      const ymin = Math.min(...pointsY) - 2
+      const ymax = Math.max(...pointsY) + 2
+      const parametres2d = { xmin, ymin, xmax, ymax, pixelsParCm: 20, scale: 0.6 }
+      const texteFigure = `À propos de l'angle ci-dessous, compléter ${this.listeTypeQuestions === 1 ? 'la' : 'chaque'} phrase par la case adéquate.<br>` + mathalea2d(parametres2d, objets2d)
 
-      const nomDirectCorrect = '\\widehat{' + A.nom + B.nom + C.nom + '}'
-      const nomIndirrectIncorrect = '\\widehat{' + B.nom + C.nom + A.nom + '}'
-      let questionReponse
-      switch (listeTypeQuestions[i]) {
-        case 'nom':
-          questionReponse =
+      for (let ee = 0, texteQuestion, texteCorrQuestion; ee < listeTypeQuestions.length;) {
+        texteQuestion = ''
+        texteCorrQuestion = ''
+        // On construit les questions
+
+        const nomDirectCorrect = '\\widehat{' + A.nom + B.nom + C.nom + '}'
+        const nomIndirrectIncorrect = '\\widehat{' + B.nom + C.nom + A.nom + '}'
+        let questionReponse
+        switch (listeTypeQuestions[ee]) {
+          case 'nom':
+            questionReponse =
           {
-            question: `$${nomDirectCorrect}$ est :`,
+            question: (context.isAmc ? '' : ((ee !== 0 ? '<br>' : '') + numAlpha(ee))) + `$${nomDirectCorrect}$ est :`,
             propositions: ['le sommet', 'un côté', 'le nom de l\'angle', 'rien de cela'],
             reponses: ['le nom de l\'angle'],
             explications: `C'est l'angle de sommet $${B.nom}$, formé par les demi-droites $[${B.nom}${A.nom})$ et $[${B.nom}${C.nom})$.`
           }
-          break
-        case 'sommet':
-          questionReponse =
+            break
+          case 'sommet':
+            questionReponse =
           {
-            question: `$${B.nom}$ est :`,
+            question: (context.isAmc ? '' : ((ee !== 0 ? '<br>' : '') + numAlpha(ee))) + `$${B.nom}$ est :`,
             propositions: ['le sommet', 'un côté', 'le nom de l\'angle', 'rien de cela'],
             reponses: ['le sommet'],
             explications: 'Le sommet de l\'angle est l\'origine commune des demi-droites qui le forment.'
           }
 
-          break
-        case 'cote':
-          questionReponse =
+            break
+          case 'cote':
+            questionReponse =
           {
-            question: `$[${B.nom}${C.nom})$ est :`,
+            question: (context.isAmc ? '' : ((ee !== 0 ? '<br>' : '') + numAlpha(ee))) + `$[${B.nom}${C.nom})$ est :`,
             propositions: ['le sommet', 'un côté', 'le nom de l\'angle', 'rien de cela'],
             reponses: ['un côté'],
             explications: 'Les côtés sont les demi-droites qui forment l\'angle.'
           }
 
-          break
-        case 'autre': {
-          let question = ''
-          let explications = ''
-          switch (choice(['sommet', 'cote', 'nom'])) {
-            case 'sommet':
-              question = `$${A.nom}$ est :`
-              explications = 'Le sommet de l\'angle est l\'origine commune des demi-droites qui le forment.'
-              break
-            case 'cote':
-              question = `$${B.nom}${A.nom}$ est :`
-              explications = 'Les côtés sont des demi-droites et se notent donc avec un crochet et une parenthèse.'
-              break
-            case 'nom':
-              question = `$${nomIndirrectIncorrect}$ est :`
-              explications = `C'est l'angle de sommet $${B.nom}$, formé par les demi-droites $[${B.nom}${A.nom})$ et $[${B.nom}${C.nom})$, c'est donc l'angle $${nomDirectCorrect}$.`
-              break
-          }
-          questionReponse =
-          {
-            question,
-            propositions: ['le sommet', 'un côté', 'le nom de l\'angle', 'rien de cela'],
-            reponses: ['rien de cela'],
-            explications
-          }
+            break
+          case 'autre': {
+            let question = ''
+            let explications = ''
+            switch (choice(['sommet', 'cote', 'nom'])) {
+              case 'sommet':
+                question = `$${A.nom}$ est :`
+                explications = 'Le sommet de l\'angle est l\'origine commune des demi-droites qui le forment.'
+                break
+              case 'cote':
+                question = `$${B.nom}${A.nom}$ est :`
+                explications = 'Les côtés sont des demi-droites et se notent donc avec un crochet et une parenthèse.'
+                break
+              case 'nom':
+                question = `$${nomIndirrectIncorrect}$ est :`
+                explications = `C'est l'angle de sommet $${B.nom}$, formé par les demi-droites $[${B.nom}${A.nom})$ et $[${B.nom}${C.nom})$, c'est donc l'angle $${nomDirectCorrect}$.`
+                break
+            }
+            questionReponse =
+            {
+              question: context.isAmc ? question : ((ee !== 0 ? '<br>' : '') + numAlpha(ee) + question),
+              propositions: ['le sommet', 'un côté', 'le nom de l\'angle', 'rien de cela'],
+              reponses: ['rien de cela'],
+              explications
+            }
 
-          break
+            break
+          }
+        }
+        const propositions = []
+        for (const proposition of questionReponse.propositions) {
+          let statut = false
+          for (const reponse of questionReponse.reponses) {
+            if (proposition === reponse) statut = true
+          }
+          propositions.push({
+            texte: proposition,
+            statut,
+            feedback: ''
+          })
+        }
+        this.autoCorrection[ee + i * listeTypeQuestions.length] = {
+          enonce: questionReponse.question,
+          options: { ordered: true },
+          propositions
+        }
+        const monQcm = propositionsQcm(this, ee + i * listeTypeQuestions.length)
+        texteQuestion += questionReponse.question + '<br>'
+        texteQuestion += monQcm.texte
+        texteCorrQuestion += questionReponse.question + '<br>'
+        texteCorrQuestion += monQcm.texteCorr
+        texteCorrQuestion += this.correctionDetaillee ? (questionReponse.explications + '<br><br>') : '<br>'
+        texte += texteQuestion
+        texteCorr += texteCorrQuestion
+        if (context.isAmc) {
+          propositionsAMC[ee] = {
+            type: 'qcmMult', // on donne le type de la première question-réponse qcmMono, qcmMult, AMCNum, AMCOpen
+            enonce: questionReponse.question,
+            propositions
+          }
+        }
+
+        ee++
+      }
+
+      if (context.isAmc) {
+        this.autoCorrection[i] = {
+          enonce: texteFigure,
+          enonceAvant: true, // EE : ce champ est facultatif et permet (si false) de supprimer l'énoncé ci-dessus avant la numérotation de chaque question.
+          enonceCentre: true, // EE : ce champ est facultatif et permet (si true) de centrer le champ 'enonce' ci-dessus.
+          melange: true, // EE : ce champ est facultatif et permet (si false) de ne pas provoquer le mélange des questions.
+          options: { avecSymboleMult: true }, // facultatif. Par défaut, multicols est à false. Ce paramètre provoque un multicolonnage (sur 2 colonnes par défaut) des propositions : pratique quand on met plusieurs AMCNum. !!! Attention, cela ne fonctionne pas, nativement, pour AMCOpen. !!!
+          propositions: propositionsAMC
         }
       }
-      const propositions = []
-      for (const proposition of questionReponse.propositions) {
-        let statut = false
-        for (const reponse of questionReponse.reponses) {
-          if (proposition === reponse) statut = true
-        }
-        propositions.push({
-          texte: proposition,
-          statut,
-          feedback: ''
-        })
-      }
-      this.autoCorrection[i] = {
-        enonce: questionReponse.question,
-        options: { ordered: true },
-        propositions
-      }
-      const monQcm = propositionsQcm(this, i)
-      texte += context.isAmc ? '' : questionReponse.question + '<br>'
-      texte += monQcm.texte
-      texteCorr += context.isAmc ? '' : questionReponse.question + '<br>'
-      texteCorr += monQcm.texteCorr
-      this.correctionDetaillee ? texteCorr += questionReponse.explications + '<br><br>' : texteCorr += '<br>'
-      if (this.questionJamaisPosee(i, ...pointsX, ...pointsY, listeTypeQuestions[i])) {
-        this.listeQuestions.push(texte)
+
+      if (this.questionJamaisPosee(i, ...pointsX, ...pointsY)) {
+        this.listeQuestions.push(texteFigure + texte)
         this.listeCorrections.push(texteCorr)
         i++
       }
       cpt++
     }
     listeQuestionsToContenu(this)
+    this.besoinFormulaireTexte = ['Cas à traiter ', 'Nombres séparés par des tirets\n1 : Nom de l\'angle\n2 : Sommet de l\'angle\n3 : Côté de l\'angle\n4 : Rien de cela\n5 : Mélange']
   }
 }
