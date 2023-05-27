28c58b8268f36c485bcdc0bace697264c2082286
EricElter <48183436+Deseux@users.noreply.github.com>
2023-05-27 22:15:35 +0200

    fix : 6M30 et ses dérivés : le paramètre type de solides refonctionne.

--- src/exercices/4e/4G53.js
+++ src/exercices/4e/4G53.js
@@ -10,7 +10,9 @@ export const ref = '4G53'
 export default function CalculDeVolumes4e () {
   CalculDeVolumes.call(this)
   this.titre = titre
+  // this.titre = 'dfdflkmdl dlk dlk fldk fldkfldkfldkffl kdflk'
   this.sup = 1
   this.classe = 4
+  this.sup4 = 7
   this.besoinFormulaire4Texte = ['Type de solides', 'Nombres séparés par des tirets\n1  : Cubes\n2 : Pavés droits\n3 : Cylindres\n4 : Prismes droits\n5 : Cônes\n6 : Pyramides\n7 : Mélange']
 }
