eac83b59b89d8c1f2d4ca4de5bc3872a83811678
sylcha <sylvain.chambon@ac-toulouse.fr>
2023-06-24 19:42:52 +0200

    fix: header ressource vue élève

--- src/components/exercice/ExerciceHtml.svelte
+++ src/components/exercice/ExerciceHtml.svelte
@@ -1,7 +1,9 @@
-<script lang='ts'>
-  import { onMount } from 'svelte'
-  import HeaderExercice from './HeaderExercice.svelte'
-  import type TypeExercice from '../utils/typeExercice'
+<script lang="ts">
+  import { onMount } from "svelte"
+  import HeaderExercice from "./HeaderExercice.svelte"
+  import type TypeExercice from "../utils/typeExercice"
+  import { globalOptions } from "../store"
+  import HeaderExerciceVueEleve from "./HeaderExerciceVueEleve.svelte"
   export let exercice: TypeExercice
   export let indiceExercice: number
   export let indiceLastExercice: number
@@ -15,21 +17,24 @@
     interactifReady: false,
     randomReady: true,
     settingsReady: false,
-    correctionReady: false
-}
+    correctionReady: false,
+  }
 
   onMount(async () => {
     divExercice.appendChild(exercice.html)
   })
 
-  $ : {
+  $: {
     headerExerciceProps.indiceExercice = indiceExercice
     headerExerciceProps.indiceLastExercice = indiceLastExercice
   }
-  
 </script>
 
-<HeaderExercice {...headerExerciceProps}/>
+{#if $globalOptions.v === "eleve"}
+  <HeaderExerciceVueEleve {...headerExerciceProps} />
+{:else}
+  <HeaderExercice {...headerExerciceProps} />
+{/if}
 <section class="m-10">
-  <div bind:this={divExercice}></div>
+  <div bind:this={divExercice} />
 </section>
