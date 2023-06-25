d850e7f2a10ce456b7f1b9f380eb9710ac92d4d1
sylcha <sylvain.chambon@ac-toulouse.fr>
2023-06-25 18:50:41 +0200

    fix: long titre dans chips

--- src/components/setup/ChipExo.svelte
+++ src/components/setup/ChipExo.svelte
@@ -8,12 +8,14 @@
   }
 </script>
 
-<div
-  id="exo-{indice}"
-  class="inline-flex items-center justify-center rounded-full bg-coopmaths-struct dark:bg-coopmathsdark-struct text-coopmaths-canvas dark:text-coopmathsdark-canvas text-xs px-1 py-[1px] shadow-sm cursor-move text-ellipsis whitespace-nowrap overflow-hidden"
->
-  {text.replaceAll('_', ' ')}
-  <button class="ml-2 bg-transparent hover focus:outline-none cursor-pointer" on:click={remove}>
+<div id="exo-{indice}" class="flex justify-center w-full items-center rounded-full bg-coopmaths-struct dark:bg-coopmathsdark-struct px-1 py-[1px] shadow-sm cursor-move">
+  <div class="block w-10/12 px-1 text-center text-coopmaths-canvas dark:text-coopmathsdark-canvas text-xs text-ellipsis overflow-hidden whitespace-nowrap">
+    {text.replaceAll("_", " ")}
+  </div>
+  <button class="ml-2 w-2/12 bg-transparent hover focus:outline-none cursor-pointer" on:click={remove}>
     <i class="text-coopmaths-action hover:text-coopmaths-action-lightest dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest bx bx-x font-semibold text-base" />
   </button>
 </div>
+
+<style>
+</style>
--- src/components/setup/ChipsList.svelte
+++ src/components/setup/ChipsList.svelte
@@ -48,7 +48,7 @@
 </script>
 
 <div
-  class="w-full grid justify-items-stretch place-content-stretch grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-8 2xl:grid-cols-10 gap-2 p-0 items-center overflow-x-auto whitespace-nowrap"
+  class="w-full grid justify-items-stretch place-content-stretch grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-8 2xl:grid-cols-10 gap-2 p-0 items-center overflow-x-auto whitespace-nowrap"
   id="chips-list"
 >
   {#each listIdsForChips as id, indice (id.key)}
