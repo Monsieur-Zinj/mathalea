6cf0c031f11a00e4daf029ecfc4e7d4be9ee452d
Jean-claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2023-06-25 06:55:00 +0000

    feat: nouvel exo interactif 2F22-1 (à améliorer) + betaModeleSpline.js qui en...

--- src/modules/fonctionsMaths.js
+++ src/modules/fonctionsMaths.js
@@ -1,8 +1,8 @@
-import { number, add, unequal, largerEq, fraction, equal, multiply, inv, max, polynomialRoot, round, acos, abs } from 'mathjs'
+import { number, add, unequal, largerEq, fraction, equal, multiply, inv, max, polynomialRoot, round, acos, abs, det } from 'mathjs'
 import FractionEtendue from './FractionEtendue.js'
 import { calcul, arrondi, ecritureAlgebrique, egal, randint, rienSi1, ecritureAlgebriqueSauf1, choice } from './outils.js'
-import { ObjetMathalea2D } from './2dGeneralites.js'
-import { Courbe, Segment } from './2d.js'
+import { ObjetMathalea2D, colorToLatexOrHTML } from './2dGeneralites.js'
+import { Courbe, Segment, point, tracePoint } from './2d.js'
 /**
 * Convertit un angle de radian vers degrés et fonction inverse
 * @Example
@@ -200,12 +200,10 @@ export function expTrinome (a, b, c) {
 class Spline {
   /**
    * Passer au moins deux noeuds, sinon ça ne peut pas fonctionner
-   * @param {x: number, y:number,nombreDerive:number}[] noeuds la liste des noeuds avec leur nombre dérivé
+   * @param {x: number, y:number,deriveeGauche:number,deriveeDroit:number, isVisible:boolean}[] noeuds la liste des noeuds avec leurs nombres dérivés
    */
   constructor (noeuds) {
     this.polys = []
-    this.x = []
-    this.y = []
     if (noeuds.length < 2) { // on ne peut pas interpoler une courbe avec moins de 2 noeuds
       window.notify('Spline : nombre de noeuds insuffisant', { noeuds })
     }
@@ -213,23 +211,26 @@ class Spline {
     for (let i = 0; i < noeuds.length - 1; i++) {
       const x0 = noeuds[i].x
       const y0 = noeuds[i].y
-      const d0 = noeuds[i].nombreDerive
+      const d0 = noeuds[i].deriveeDroit
       const x1 = noeuds[i + 1].x
       const y1 = noeuds[i + 1].y
-      const d1 = noeuds[i + 1].nombreDerive
+      const d1 = noeuds[i + 1].deriveeGauche
       const ligne1 = [x0 ** 3, x0 ** 2, x0, 1]
       const ligne2 = [x1 ** 3, x1 ** 2, x1, 1]
       const ligne3 = [3 * x0 ** 2, 2 * x0, 1, 0]
       const ligne4 = [3 * x1 ** 2, 2 * x1, 1, 0]
       const matrice = [ligne1, ligne2, ligne3, ligne4]
+      if (det(matrice) === 0) {
+        window.notify('Spline : impossible de trouver un polynome ici car la matrice n\'est pas inversible, il faut revoir vos noeuds : ', { noeudGauche: noeuds[i], noeudDroit: noeuds[i + 1] })
+        return
+      }
       const matriceInverse = inv(matrice)
       const vecteur = [y0, y1, d0, d1]
       this.polys.push(new Polynome({ isUseFraction: false, coeffs: multiply(matriceInverse, vecteur).reverse().map(coef => round(coef, 3)) }))
     }
-    for (let i = 0; i < noeuds.length; i++) {
-      this.x.push(noeuds[i].x)
-      this.y.push(noeuds[i].y)
-    }
+    this.x = noeuds.map((noeud) => noeud.x)
+    this.y = noeuds.map((noeud) => noeud.y)
+    this.visible = noeuds.map((noeud) => noeud.isVisible) // On récupère la visibilité des noeuds pour la courbe
     this.n = this.y.length // on a n valeurs de y et donc de x, soit n-1 intervalles numérotés de 1 à n-1.
     // this.step = step // on en a besoin pour la dérivée...
     this.fonctions = this.convertPolyFunction()
@@ -247,6 +248,11 @@ class Spline {
     return f
   }
 
+  /**
+   * retourne les solutions de f(x) = y sur son domaine de définition
+   * @param {number} y
+   * @returns {number[]}
+   */
   solve (y) {
     const antecedents = []
     for (let i = 0; i < this.polys.length; i++) {
@@ -281,6 +287,137 @@ class Spline {
       }
     }
     return antecedents
+  }
+
+  nombreAntecedentsEntiers (y) {
+    const solutions = this.solve(y)
+    const solutionsEntieres = solutions.filter(sol => Number.isInteger(sol))
+    return solutionsEntieres.length
+  }
+
+  nombreAntecedents (y) {
+    const solutions = this.solve(y)
+    return solutions.length
+  }
+
+  trouveYPourNAntecedentsEntiers (n, yMin, yMax) {
+    if (Number.isInteger(yMin) && Number.isInteger(yMax)) {
+      for (let y = yMin; y <= yMax; y++) {
+        console.log(`Pour y = ${y}, nombre de solutions entières : ${this.nombreAntecedentsEntiers(y)} sur nombre de solutions ${this.nombreAntecedents(y)}`)
+        if (this.nombreAntecedentsEntiers(y) === n && this.nombreAntecedents(y) === n) {
+          console.log(`je retourne ${y}`)
+          return y
+        }
+      }
+    } else {
+      window.notify('trouveYPourNAntecedentsEntiers() appelé avec des valeurs incorrectes', { n, yMin, yMax })
+    }
+    window.notify('trouveYPourNAntecedentsEntiers() : Je n\'ai rien trouvé !', { n, yMin, yMax })
+    return 'aucune' // normalement, il ne devrait jamais retourner cette valeur.
+  }
+
+  amplitude () {
+    let yMin = 1000
+    let yMax = -1000
+    const derivees = this.derivees
+    for (let i = 0; i < this.x.length - 1; i++) {
+      let maxLocal, minLocal
+      if (derivees[i].deg === 2) {
+        const a = derivees[i].monomes[2]
+        const b = derivees[i].monomes[1]
+        const c = derivees[i].monomes[0]
+        const delta = b ** 2 - 4 * a * c
+        if (delta < 0) { // la dérivée ne s'annule pas donc la fonction est monotone du signe de a
+          if (a > 0) { // la fonction est croissante don le max est atteint en x[i+1]
+            maxLocal = this.image(this.x[i + 1])
+            minLocal = this.image(this.x[i])
+          } else {
+            maxLocal = this.image(this.x[i])
+            minLocal = this.image(this.x[i + 1])
+          }
+        } else if (delta === 0) { // la dérivée s'annule une seule fois mais il faut vérifier que c'est sur l'intervalle x[i] x[i+1]
+          const racine = -b / 2 / a
+          if (racine > this.x[i] && racine < this.x[i]) { // ça peut encore être un max ou un min !
+            if (a > 0) { // c'est un minimum
+              maxLocal = Math.max(this.image(this.x[i]), this.image(this.x[i + 1]))
+              minLocal = this.image(racine)
+            } else { // c'est un maximum
+              maxLocal = this.image(racine)
+              minLocal = Math.min(this.image(this.x[i]), this.image(this.x[i + 1]))
+            }
+          } else { // la racine n'est pas dans cet intervalle, donc la dérivée est monotone ici
+            maxLocal = Math.max(this.image(this.x[i]), this.image(this.x[i + 1]))
+            minLocal = Math.min(this.image(this.x[i]), this.image(this.x[i + 1]))
+          }
+        } else { // delta >0 deux racines !
+          const tiDelta = Math.sqrt(delta)
+          const r1 = a > 0 ? (-b - tiDelta) / 2 / a : (-b + tiDelta) / 2 / a
+          const r2 = a > 0 ? (-b + tiDelta) / 2 / a : (-b - tiDelta) / 2 / a
+          if (this.x[i] < r1 && r1 < this.x[i + 1]) { // r1 est dans l'intervalle
+            if (this.x[i] < r2 && r2 < this.x[i + 1]) { // r2 aussi !
+              if (a > 0) { // croissant puis decroissant puis croissant : le max est soit en r1 soit en x[i+1]
+                maxLocal = Math.max(this.image(r1), this.image(this.x[i + 1]))
+                minLocal = Math.min(this.image(this.x[i]), this.image(r2))
+              } else { // a<0 décroissant puis croissant puis décroissant
+                minLocal = Math.min(this.image(r1), this.image(this.x[i + 1]))
+                maxLocal = Math.max(this.image(this.x[i]), this.image(r2))
+              }
+            } else { // r1 est dedans mais pas r2
+              if (a > 0) { // on a un max en r1 et le min est soit en x[i] soit en x[i+1]
+                maxLocal = this.image(r1)
+                minLocal = Math.min(this.x[i], this.image(this.x[i + 1]))
+              } else { // minimum en r1, max en x[i] ou x[i+1]
+                minLocal = this.image(r1)
+                maxLocal = Math.max(this.x[i], this.image(this.x[i + 1]))
+              }
+            }
+          } else { // r1 n'est pas dans l'intervalle mais peut-être r2 y est
+            if (this.x[i] < r2 && r2 < this.x[i + 1]) {
+              if (a > 0) { // on a le min en r2 et le max en x[i] ou en x[i+1]
+                minLocal = this.image(r2)
+                maxLocal = Math.max(this.x[i], this.image(this.x[i + 1]))
+              } else { // on a le max en r2 et le min en x[i] ou en x[i+1]
+                maxLocal = this.image(r2)
+                minLocal = Math.min(this.x[i], this.image(this.x[i + 1]))
+              }
+            } else { // ni r1, ni r2 ne sont dans l'intervalle. La fonction est monotone
+              if (a > 0) {
+                if (r2 < this.x[i] || r1 > this.x[i + 1]) { // strictement croissante
+                  maxLocal = this.image(this.x[i + 1])
+                  minLocal = this.image(this.x[i])
+                } else { // normalemennt r1<x[i] et r2>x[i+1] strictement décroissante
+                  maxLocal = this.image(this.x[i])
+                  minLocal = this.image(this.x[i + 1])
+                }
+              } else {
+                if (r2 < this.x[i] || r1 > this.x[i + 1]) { // strictement décroissante
+                  maxLocal = this.image(this.x[i])
+                  minLocal = this.image(this.x[i + 1])
+                } else { // normalemennt r1<x[i] et r2>x[i+1] strictement croissante
+                  maxLocal = this.image(this.x[i + 1])
+                  minLocal = this.image(this.x[i])
+                }
+              }
+            }
+          }
+        }
+      } else if (derivees[i].deg === 1) { // derivée affine, monotone croissante ou décroissante selon le signe de derivee[i].monomes[1]
+        const a = derivees[i].monomes[1]
+        if (a > 0) {
+          maxLocal = this.image(this.x[i + 1])
+          minLocal = this.image(this.x[i])
+        } else {
+          maxLocal = this.image(this.x[i])
+          minLocal = this.image(this.x[i + 1])
+        }
+      } else { // constante !
+        minLocal = this.image(this.x[i])
+        maxLocal = this.image(this.x[i])
+      }
+      yMin = Math.min(yMin, minLocal)
+      yMax = Math.max(yMax, maxLocal)
+    }
+    return { yMin, yMax }
   }
 
   get fonction () {
@@ -319,17 +456,31 @@ class Spline {
     return derivees
   }
 
+  /**
+   * crée l'objet mathalea2d correspondant à la courbe tracée
+   * @param {Repere} repere
+   * @param {number} step
+   * @param {string} color
+   * @param {number} epaisseur
+   * @param {boolean} ajouteNoeuds
+   * @param {{*}} optionsNoeuds
+   * @returns {Trace}
+   */
   courbe ({
     repere,
     step = 0.1,
     color = 'black',
-    epaisseur = 1
+    epaisseur = 1,
+    ajouteNoeuds = false,
+    optionsNoeuds = {}
   } = {}) {
     return new Trace(this, {
       repere,
       step,
       color,
-      epaisseur
+      epaisseur,
+      ajouteNoeuds,
+      optionsNoeuds
     })
   }
 }
@@ -343,22 +494,31 @@ export function spline (noeuds) {
  * @author Jean-Claude Lhote
  */
 export function trieNoeuds (noeuds) {
-  let xInter, yInter
+  let xInter, yInter, dGaucheInter, dDroitInter, isVisibleInter
   for (let i = 0; i < noeuds.length - 1; i++) {
     for (let j = i + 1; j < noeuds.length; j++) {
       if (noeuds[i].x > noeuds[j].x) {
         xInter = noeuds[i].x
-        noeuds[i].x = noeuds[j].x
-        noeuds[j].x = xInter
         yInter = noeuds[i].y
+        dGaucheInter = noeuds[i].deriveeGauche
+        dDroitInter = noeuds[i].deriveeDroit
+        isVisibleInter = noeuds[i].isVisible
+        noeuds[i].x = noeuds[j].x
         noeuds[i].y = noeuds[j].y
+        noeuds[i].deriveeGauche = noeuds[j].deriveeGauche
+        noeuds[i].deriveeDroit = noeuds[j].deriveeDroit
+        noeuds[i].isVisible = noeuds[j].isVisible
+        noeuds[j].x = xInter
         noeuds[j].y = yInter
+        noeuds[j].deriveeGauche = dGaucheInter
+        noeuds[j].deriveeDroit = dDroitInter
+        noeuds[j].isVisible = isVisibleInter
       } else if (egal(noeuds[i].x, noeuds[j].x)) {
-        window.notify('Deux couples ont la même valeur de x ! je ne peux pas trier', { noeuds })
         return false
       }
     }
   }
+  console.log(`Noeuds après ${JSON.stringify(noeuds)}`)
   return true
 }
 
@@ -549,17 +709,31 @@ class SplineCatmullRom {
     return maSpline
   }
 
+  /**
+   * crée l'objet mathalea2d correspondant à la courbe tracée
+   * @param {Repere} repere
+   * @param {number} step
+   * @param {string} color
+   * @param {number} epaisseur
+   * @param {boolean} ajouteNoeuds
+   * @param {{*}} optionsNoeuds
+   * @returns {Trace}
+   */
   courbe ({
     repere,
     step = 0.1,
     color = 'black',
-    epaisseur = 1
+    epaisseur = 1,
+    ajouteNoeuds = false,
+    optionsNoeuds = {}
   } = {}) {
     return new Trace(this, {
       repere,
       step,
       color,
-      epaisseur
+      epaisseur,
+      ajouteNoeuds,
+      optionsNoeuds
     })
   }
 }
@@ -591,17 +765,21 @@ export function splineCatmullRom ({ tabY = [], x0 = -5, step = 1 } = {}) {
  */
 class Trace extends ObjetMathalea2D {
   /**
-   * @param spline La splineCatmulRom dont on veut la Trace
-   * @param repere le repère associé
-   * @param step le pas entre deux points
-   * @param color la couleur
-   * @param epaisseur son épaisseur
+   * @param {Spline | SplineCatmullRom}spline La splineCatmulRom ou Spline dont on veut la Trace
+   * @param {Repere} repere le repère associé
+   * @param {number} step le pas entre deux points
+   * @param {string} color la couleur
+   * @param {number} epaisseur son épaisseur
+   * @param {boolean} ajouteNoeuds si true, des points sont ajoutés aux endroits des noeuds
+   * @param {{*}} optionsNoeud
    */
   constructor (spline, {
     repere,
     step = 0.1,
     color = 'black',
-    epaisseur = 1
+    epaisseur = 1,
+    ajouteNoeuds = true,
+    optionsNoeuds = {}
   } = {}) {
     super()
     const objets = []
@@ -620,6 +798,34 @@ class Trace extends ObjetMathalea2D {
         s.epaisseur = epaisseur
         objets.push(s)
       }
+      if (ajouteNoeuds && spline.visible[i]) {
+        const noeud = point(spline.x[i], spline.y[i])
+        const traceNoeud = tracePoint(noeud)
+        if (optionsNoeuds) {
+          if (optionsNoeuds.color) {
+            traceNoeud.color = colorToLatexOrHTML(optionsNoeuds.color)
+            traceNoeud.couleurDeRemplissage = colorToLatexOrHTML(optionsNoeuds.color)
+          }
+          if (optionsNoeuds.epaisseur) { traceNoeud.epaisseur = optionsNoeuds.epaisseur }
+          if (optionsNoeuds.style) { traceNoeud.style = optionsNoeuds.style }
+          if (optionsNoeuds.taille) { traceNoeud.taille = optionsNoeuds.taille }
+        }
+        objets.push(traceNoeud)
+      }
+    }
+    if (ajouteNoeuds && spline.visible[spline.n - 1]) {
+      const noeud = point(spline.x[spline.n - 1], spline.y[spline.n - 1])
+      const traceNoeud = tracePoint(noeud)
+      if (optionsNoeuds) {
+        if (optionsNoeuds.color) {
+          traceNoeud.color = colorToLatexOrHTML(optionsNoeuds.color)
+          traceNoeud.couleurDeRemplissage = colorToLatexOrHTML(optionsNoeuds.color)
+        }
+        if (optionsNoeuds.epaisseur) { traceNoeud.epaisseur = optionsNoeuds.epaisseur }
+        if (optionsNoeuds.style) { traceNoeud.style = optionsNoeuds.style }
+        if (optionsNoeuds.taille) { traceNoeud.taille = optionsNoeuds.taille }
+      }
+      objets.push(traceNoeud)
     }
     this.svg = function (coeff) {
       let code = ''
