ba253d0a59e60cc0b9a112b5ca5a6dd349608207
Jean-Claude Lhote <lhotejc@gmail.com>
2023-07-01 08:42:17 +0200

    refacto: Spline this.fonction

--- src/modules/mathFonctions/Spline.js
+++ src/modules/mathFonctions/Spline.js
@@ -70,7 +70,6 @@ class Spline {
     this.n = this.y.length // on a n valeurs de y et donc de x, soit n-1 intervalles numérotés de 1 à n-1.
     // this.step = step // on en a besoin pour la dérivée...
     this.fonctions = this.convertPolyFunction()
-    this.fonction = x => this.image(x)
   }
 
   /**
