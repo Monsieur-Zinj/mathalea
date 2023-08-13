e11e7939ba25ef605353d9d5d53c8b68d11c772a
Rémi Angot <remi.angot@gmail.com>
2023-08-13 08:30:02 +0200

    Enregistre les paramètres

--- src/exercices/apps/challengeRelatif.ts
+++ src/exercices/apps/challengeRelatif.ts
@@ -1,9 +1,10 @@
 import { get } from 'svelte/store'
-import { resultsByExercice, globalOptions } from '../../components/store'
+import { globalOptions, resultsByExercice } from '../../components/store'
 
 class challengeRelatif {
   typeExercice: string
   numeroExercice: number
+  sup: string
   titre: string
   container: HTMLDivElement
   iframe: HTMLIFrameElement
@@ -15,8 +16,6 @@ class challengeRelatif {
     this.iframe.setAttribute('width', '400px')
     this.iframe.setAttribute('height', '300px')
     this.iframe.classList.add('my-10')
-
-    this.iframe.setAttribute('src', 'https://coopmaths.fr/challenge/?mathalea')
     this.iframe.setAttribute('allowfullscreen', '')
     this.container.appendChild(this.iframe)
     const updateVideoSize = () => {
@@ -28,24 +27,26 @@ class challengeRelatif {
         this.iframe.setAttribute('height', (document.body.offsetHeight * 1.5).toString())
       }
     }
-    const initExercice = () => {
-      const vue = get(globalOptions).v
-      const message = { type: 'mathaleaSendNumeroExercice', numeroExercice: this.numeroExercice, vue }
-      this.iframe.contentWindow.postMessage(message, '*')
-    }
     window.addEventListener('resize', updateVideoSize)
     this.container.addEventListener('addedToDom', updateVideoSize)
-    this.container.addEventListener('exerciceChange', updateVideoSize)
-    this.iframe.addEventListener('load', initExercice)
   }
 
   get html () {
     this.handleScore()
+    let url = `https://coopmaths.fr/challenge/?mathalea&numeroExercice=${this.numeroExercice}`
+
+    if (this.sup !== undefined) {
+      url += `&params=${this.sup}`
+    }
+    if (get(globalOptions).v === 'eleve') {
+      url += '&v=eleve'
+    }
+    this.iframe.setAttribute('src', url)
     return this.container
   }
 
-  async handleScore () {
-    window.addEventListener('message', async (event) => {
+  handleScore () {
+    window.addEventListener('message', (event) => {
       if (event.data?.type !== 'mathaleaSendScore') return
       const numberOfPoints = parseInt(event.data.score)
       const indice = parseInt(event.data.numeroExercice)
