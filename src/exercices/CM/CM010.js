2c9ce932878c8595e800de582563d889e0abb4c3
Rémi Angot <remi.angot@gmail.com>
2023-08-30 07:34:26 +0200

    fix: url d'imports

--- src/exercices/CM/CM010.js
+++ src/exercices/CM/CM010.js
@@ -4,7 +4,7 @@ import { texNombre } from '../../lib/outils/texNombre.js'
 import Exercice from '../Exercice.js'
 import { listeQuestionsToContenu, randint } from '../../modules/outils.js'
 import { setReponse } from '../../lib/interactif/gestionInteractif.js'
-import { ajouteChampTexteMathLive } from '../../../lib/interactif/questionMathLive.js'
+import { ajouteChampTexteMathLive } from '../../lib/interactif/questionMathLive.js'
 
 export const titre = 'Tiers'
 export const amcReady = true
