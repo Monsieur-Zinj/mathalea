1507b6092c6c44a0b79e47648e31d17d88404bd6
Rémi Angot <remi.angot@gmail.com>
2023-09-04 09:47:44 +0200

    fix: chargement impossible avec recorder=capytale

--- src/components/sidebar/SideMenuApps.svelte
+++ src/components/sidebar/SideMenuApps.svelte
@@ -1,6 +1,10 @@
 <script>
   import { getContext } from "svelte"
-  const { toogleThirdAppsChoiceDialog } = getContext("thirdAppsChoiceContext")
+  const thirdAppsChoiceContext = getContext("thirdAppsChoiceContext")
+  let toogleThirdAppsChoiceDialog
+  if (thirdAppsChoiceContext !== undefined) {
+    toogleThirdAppsChoiceDialog = thirdAppsChoiceContext.toogleThirdAppsChoiceDialog
+  }
 </script>
 
 <button type="button" class="px-6 py-2 font-bold text-xl text-coopmaths-struct dark:text-coopmathsdark-struct" on:click={toogleThirdAppsChoiceDialog}> Applications tierces </button>
