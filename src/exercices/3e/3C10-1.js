a13aca0eebb4cbef1326ebba027941e39bad9577
EricElter <48183436+Deseux@users.noreply.github.com>
2023-09-05 21:09:49 +0200

    feat : Amélioration 4C30-3 et création de 3C10-1

---
+++ src/exercices/3e/3C10-1.js
@@ -0,0 +1,20 @@
+import EcritureDecimalePuissance from '../4e/4C30-3.js'
+export const titre = 'Donner l\'écriture entière ou fractionnaire d\'une puissance'
+export const dateDePublication = '05/09/2023'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const amcReady = true
+export const amcType = 'AMCNum'
+/**
+ * Clone de 4C30-3 pour les 3emes
+ *
+ * @author Eric Elter
+ */
+
+export const uuid = 'eb865'
+export const ref = '3C10-1'
+export default function EcritureDecimalePuissance3e () {
+  EcritureDecimalePuissance.call(this)
+  this.sup = 3
+  this.besoinFormulaireNumerique = ['Niveau de difficulté', 3, '1 : Exposants positifs\n2 : Exposants négatifs\n3 : Mélange']
+}
