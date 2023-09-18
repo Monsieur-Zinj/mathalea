5257a7ad368b75f8b2f0e867aa16277347449433
Rémi Angot <remi.angot@gmail.com>
2023-09-18 21:59:26 +0200

    fix: erreur si nouvelleVersion n'existe pas

--- src/lib/mathalea.ts
+++ src/lib/mathalea.ts
@@ -455,7 +455,7 @@ export function mathaleaHandleExerciceSimple (exercice: TypeExercice, isInteract
   exercice.listeCanReponsesACompleter = []
   for (let i = 0, cptSecours = 0; i < exercice.nbQuestions && cptSecours < 50;) {
     seedrandom(exercice.seed + i + cptSecours, { global: true })
-    exercice.nouvelleVersion()
+    exercice.nouvelleVersion?.()
     if (exercice.questionJamaisPosee(i, exercice.question)) {
       setReponse(exercice, i, exercice.reponse, { formatInteractif: exercice.formatInteractif } || {})
       exercice.listeQuestions.push(
