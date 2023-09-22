739a62ce4dcca581d5ef0846ab1d6fc78d54a65e
sylcha <sylvain.chambon@ac-toulouse.fr>
2023-09-22 18:54:52 +0200

    feat: décla des référentiels + diagram

---
+++ src/lib/referentiels.ts
@@ -0,0 +1,48 @@
+export interface Feature {
+  name: string
+  isActive: boolean
+  type: string
+}
+
+export interface AppTierce {
+  uuid: string
+  title: string
+  presentation: string
+  impPath: string
+}
+
+export interface AppTierceGroup {
+  rubrique: string
+  liste: AppTierce[]
+}
+
+export interface ReferentielBase {
+  uuid: string
+  url: string
+  tags: string[]
+}
+
+export interface ReferentielExercice extends ReferentielBase {
+  id: string
+  titre: string
+  features: Feature[]
+}
+
+export interface ReferentielStatic extends ReferentielBase {
+  png: string[]
+  pngCor: string[]
+}
+
+export interface ReferentielExamen extends ReferentielStatic {
+  mois: string
+  annee: string
+  lieu: string
+  typeExercice: string
+  numeroInitial: string
+  urlcor: string
+}
+
+export interface ReferentielRessource extends ReferentielBase {
+  id: string
+  titre: string
+}
