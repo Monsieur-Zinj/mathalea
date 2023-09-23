310317aa75dba1303f10d1e88aef83e6d3060457
Eric Elter <eric.elter@ac-normandie.fr>
2023-09-23 16:25:56 +0000

    feat : Paramétrage plus fin sur 7 exos de 5ème

--- src/exercices/5e/5L14-3.js
+++ src/exercices/5e/5L14-3.js
@@ -2,18 +2,17 @@ import EcrireUneExpressionNumerique from './_Ecrire_une_expression_numerique.js'
 export const titre = 'Traduire une phrase par une expression et la calculer'
 export const amcReady = true
 export const amcType = 'AMCHybride'
+export const dateDeModifImportante='21/09/2023'
 
 /**
  * @author Jean-Claude Lhote
  */
+
 export const uuid = '2c600'
 export const ref = '5L14-3'
 export default function TraduireUnePhraseParUneExpressionLitteraleEtCalculer () {
   EcrireUneExpressionNumerique.call(this)
   this.version = 3
-  this.titre = titre
   this.litteral = true
-  this.sup = NaN
-  this.sup4 = 1
-  this.besoinFormulaire4Numerique = ['Niveau de difficulté', 4, '1 : de 1 à 3 opérations\n2 : de 2 à 3 opérations\n3 : jusqu\'à 4 opérations\n4 : opérations plus complexes']
-}
+  this.sup4 = '1-2-3'
+  }
