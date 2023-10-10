06e9161dbf1bff2e05e252af1cf1e43c1912161c
EricElter <48183436+Deseux@users.noreply.github.com>
2023-10-10 20:34:31 +0200

    feat : 5G11-1 devient AMCOpen

--- src/exercices/5e/5G11-1.js
+++ src/exercices/5e/5G11-1.js
@@ -1,5 +1,7 @@
 import ConstruireParSymetrie from '../6e/_Construire_par_symetrie.js'
 export const titre = 'Construire le symétrique d\'un point par symétrie centrale'
+export const amcReady = true
+export const amcType = 'AMCOpen'
 export const dateDeModifImportante = '14/11/2021'
 /**
  * Référence 6G11-2
