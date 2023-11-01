2502fc73bcec19f51096edf58a30b610cd0d523d
Jean-claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2023-11-01 04:07:26 +0000

    Liste Ok pour latex

--- src/lib/interactif/listeDeroulante/ListeDeroulante.ts
+++ src/lib/interactif/listeDeroulante/ListeDeroulante.ts
@@ -34,6 +34,13 @@ type AllChoiceType = (string | CHoiceValue)
 type AllChoicesType = AllChoiceType[]
 
 /**
+ * Cette fonction est amenée à disparaître : elle palie à un bug du MathfieldElement qui intercepte les actions du pointer alors qu'il est readOnly.
+ * @param htmlString
+ */
+function setPointEventsToNone (htmlString: string) {
+  return htmlString.replaceAll('pointer-events:auto', 'pointer-events:none')
+}
+/**
  * La fonction qui affiche l'un des choix, que ce soit du texte, du latex ou une image.
  * @param {HTMLLIElement} li l'élément de liste qui doit contenir le choix
  * @param {AllChoiceType} choice le choix en question
@@ -46,11 +53,12 @@ function afficheChoice (li: HTMLLIElement | HTMLSpanElement, choice: AllChoiceTy
   } else if (typeof choice === 'object') {
     if ('latex' in choice) {
       const mf: MathfieldElement = new MathfieldElement()
-      const divOver = document.createElement('div')
       mf.value = choice.latex ?? choice.value
       mf.readOnly = true
+      // à supprimer lorsque le bug des MathfieldElement readOnly sera fixé.
+      const shadowRoot = mf.shadowRoot
+      if (shadowRoot) mf.shadowRoot.innerHTML = setPointEventsToNone(shadowRoot.innerHTML)
       li.appendChild(mf)
-      li.appendChild(divOver)
     } else if ('image' in choice) {
       const image = document.createElement('img')
       image.src = choice.image ?? choice.value
@@ -70,13 +78,10 @@ function afficheChoice (li: HTMLLIElement | HTMLSpanElement, choice: AllChoiceTy
  * Il faut passer par ListeDeroulante.create() et pas new ListeDeroulante(…)
  * @class
  */
-class ListeDeroulante extends HTMLElement {
-  type: string = 'ld' // Je ne sais pas ce que signifie 'ld'... et je penser que le type doit être un tupple...
+class ListeDeroulante {
   disabled: boolean = false
   choices: AllChoicesType // La liste des choix
   givenChoices?: AllChoicesType // Je ne vois pas pourquoi on aurait ça...
-  onChange?: (choice: AllChoiceType) => void // Callback éventuelle à appeler avec le choix fait à chaque changement
-  decalage?: 0 | 1 = 0 // Un décalage éventuel en pixels…
   private _kbIndex: number = -1 // L'index du choix sélectionné au clavier
   changed: boolean = false // Passe à true dès qu'on a fait une sélection (ou dès le départ si le choix initial est sélectionnable)
   private readonly _offset: number = 0 // Décalage éventuel d'index entre le tableau de choix fourni et celui qu'on manipule (0 ou 1 suivant que le premier choix est sélectionnable ou pas)
@@ -90,125 +95,44 @@ class ListeDeroulante extends HTMLElement {
   spanSelected?: HTMLSpanElement
   ulContainer?: HTMLUListElement
   initialChoice?: AllChoiceType
-  centre?: boolean
   width?: number
 
   /**
      * @constructor
      * @param options
      * @param choices
-     * @param options.onChange
      * @param options.choix0
-     * @param options.decalage
      */
-  constructor (choices: AllChoicesType, { onChange, choix0, decalage }: {
-        onChange?: ((choice: AllChoiceType) => void) | null,
+  constructor (choices: AllChoicesType, { choix0 }: {
         choix0?: boolean,
-        decalage?: number,
-    } = { decalage: 0, choix0: false }) {
-    super()
+    } = { choix0: false }) {
     if (arguments.length > 2) throw Error('nombre d’arguments invalides')
     if (!Array.isArray(choices)) throw Error('Il faut passer une liste de choix')
-    /**
-         * Le type de liste, peut valoir ld|zsm1|zsm2|zsm3, actuellement utilisé par ZoneStyleAffiche seulement
-         * @type {string}
-         */
-    this.type = 'ld'
-    /**
-         * true lorsgue la liste est désactivée
-         * @type {boolean}
-         */
-    this.disabled = false
-    /**
-         * La liste des choix
-         * @type {string[]}
-         */
-    this.choices = [...choices]
-    /**
-         * La liste de choix donnée initialement (idem choices si choix0, sinon choices n'a pas le 1er elt)
-         * @type {string[]}
-         */
-    /**
-         * true si la liste se déroule vers le haut
-         * @type {boolean}
-         */
-    /**
-         * Callback éventuelle à appeler avec le choix fait à chaque changement
-         */
-    this.onChange = typeof onChange === 'function'
-      ? onChange
-      : undefined
-    /**
-         * Un décalage éventuel en pixels…
-         * @type {number}
-         */
-    this.decalage = decalage ? 1 : 0
-    /**
-         * L'index du choix sélectionné au clavier
-         * @type {number}
-         */
-    this._kbIndex = -1
-    /**
-         * Passe à true dès qu'on a fait une sélection (ou dès le départ si le choix initial est sélectionnable)
-         * @type {boolean}
-         */
-    this.changed = false
-    /**
-         *
-         * @type {number}
-         * @private
-         */
-    this._offset = choix0 ? 0 : 1
-    /**
-         * Le choix courant
-         * @type {string}
-         */
-    this.reponse = ''
+    this.disabled = false // mettre à true pour désactiver la liste
+    this.choices = [...choices] // La liste des choix
+    this.givenChoices = [...choices] // idem, mais celle-là restera complète avec l'entête de liste si le choix0 retire cet élément de choices lors de l'init.
 
-    /**
-         * Liste des elts contenant les choix (les &lt;li&gt;)
-         * @type {HTMLDivElement[]}
-         * @private
-         */
-    this._elts = []
-  }
-
-  connectedCallback () {
-    this.show()
-  }
-
-  disconnectedCallback () {
-    this.hide()
-  }
-
-  adoptedCallback () {
-    this._replace()
-    this.show()
-  }
-
-  attributeChangedCallback (/* name: string, oldValue: string, newValue:string */) {
-    this.reset()
+    this._kbIndex = -1 // L'index du choix sélectionné au clavier
+    this.changed = false // Passe à true dès qu'on a fait une sélection (ou dès le départ si le choix initial est sélectionnable)
+    this._offset = choix0 ? 0 : 1 // permet d'atteindre le 1er élément sélectionnable de la liste
+    this.reponse = '' // Le choix courant
+    this._elts = [] // Liste des elts contenant les choix (les <li>)
   }
 
   /**
      * Crée les éléments dans le DOM (appelé par create) et ajoute les listeners
      * @private
      */
-  _init ({ centre, conteneur, sansFleche, select }:{centre?: boolean, conteneur?: HTMLElement, sansFleche?: boolean, select?: number} = { centre: false, sansFleche: false, select: 0 }) {
+  _init ({ conteneur, sansFleche, select }:{ conteneur?: HTMLElement, sansFleche?: boolean, select?: number} = { sansFleche: false, select: 0 }) {
     /**
          * Le span qui va contenir la liste (tous les éléments que l'on crée, enfant de conteneur)
          * @type {HTMLSpanElement}
          */
     if (!conteneur) throw Error('Le conteneur est obligatoire pour la liste déroulante !')
     this.container = conteneur
-    this.centre = centre ?? false
-    // flèche à gauche ?
+    this.container.classList.add('listeDeroulante')
     const char = '˅'
-    /**
-         * Le span de l'élément sélectionné
-         * @type {HTMLSpanElement}
-         */
-    this.spanSelected = document.createElement('span')
+    this.spanSelected = document.createElement('span') // Le span de l'élément sélectionné
     this.container.appendChild(this.spanSelected)
     this.spanSelected.className = 'currentChoice'
     this.spanSelected.role = 'listbox'
@@ -222,7 +146,6 @@ class ListeDeroulante extends HTMLElement {
     }
     this.container.onclick = this._clickListener
 
-    // keydown, pour navigation au clavier
     /**
          * listener de keydown sur spanSelected, pour sélection au clavier
          * @param {KeyboardEvent} event
@@ -264,59 +187,24 @@ class ListeDeroulante extends HTMLElement {
     }
     // la liste qui peut être masquée (il doit passer au-dessus du reste, les boutons sont à 50, les modales à 90)
     this.ulContainer = document.createElement('ul')
-    this.ulContainer.style.zIndex = '60'
-    if (this.centre) {
-      this.ulContainer.style.alignItems = 'center'
-      this.ulContainer.style.justifyContent = 'center'
-    }
     this.container.appendChild(this.ulContainer)
 
     // si le premier élément de la liste n'est pas sélectionnable, on le sort de la liste
     this.initialChoice = this._offset ? this.choices.shift() : this.choices[0]
-    this.givenChoices = this.choices.map(el => typeof el === 'string' ? el : el.value)
-
     // les choix
     for (const [index, choice] of this.choices.entries()) {
       const li = document.createElement('li')
       li.role = 'option'
-      if (centre) {
-        li.style.alignItems = 'center'
-        li.style.justifyContent = 'center'
-      }
       this.ulContainer?.appendChild(li)
-
-      // si y'a du mathLive dans choice, faut que ce soit visible, sinon mathLive peut faire des bêtises dans ses calculs de dimensionnement
       afficheChoice(li, choice)
-      if (typeof choice === 'object' && 'latex' in choice) {
-        const theDiv = li.querySelector('div')
-        if (theDiv) {
-          theDiv.addEventListener('click', (event) => {
-            event.stopPropagation() // faut pas propager à container sinon il va rouvrir la liste après select()
-            this.select(index, { withoutOffset: true })
-          })
-        }
-      } else {
-        li.addEventListener('click', (event) => {
-          event.stopPropagation() // faut pas propager à container sinon il va rouvrir la liste après select()
-          this.select(index, { withoutOffset: true })
-        })
-      }
-
+      li.addEventListener('click', (event) => {
+        event.stopPropagation() // faut pas propager à container sinon il va rouvrir la liste après select()
+        this.select(index, { withoutOffset: true })
+      })
       this._elts.push(li)
     }
     // Il faut faire ça pour récupérer this.width à l'init ce qui permet de fixer la largeur minimum du spanSelected et d'aligner la liste à droite du spanSelected
     this.show() // fixe le this.width car il est undefined.
-    // ça ne marche pas dans Mathalea : le div conteneur  n'existe pas encore dans le dom... du coup, tout est à zéro !
-    this.spanSelected.style.minWidth = this.width + 'px'
-    for (const elt of this._elts) {
-      const div = elt.querySelector('div')
-      if (div) {
-        const mf = div.parentElement?.querySelector('math-field')
-        const height = mf ? mf.clientHeight : 25
-        div.style.width = String(this.width ?? 70) + 'px'
-        div.style.height = String(Math.max(height, 25)) + 'px'
-      }
-    }
     this.hide()
 
     this.spanSelected.addEventListener('keydown', this._keydownListener)
@@ -340,11 +228,11 @@ class ListeDeroulante extends HTMLElement {
     })
     // et le choix initial, on passe par select/reset pour éviter de dupliquer du code, mais ils ne doivent pas appeler le listener à l'init
     // (y'a des sections qui nous filent en listener  un truc qui n'est pas encore prêt, faut pas l'appeler tout de suite)
-    if (select) this.select(select, { withoutCallback: true })
+    if (select) this.select(select)
     // si choix0, on considère ça comme une réponse déjà mise
-    else if (this._offset === 0) this.select(0, { withoutCallback: true })
+    else if (this._offset === 0) this.select(0)
     // sinon reset
-    else this.reset({ withoutCallback: true })
+    else this.reset()
     this._replace()
   }
 
@@ -364,7 +252,7 @@ class ListeDeroulante extends HTMLElement {
       const height = this.container.offsetHeight
       const widthSpan = this.spanSelected?.offsetWidth ?? 0
       if (this.ulContainer) {
-        this.ulContainer.style.top = `${height + (this.decalage ?? 0)}px`
+        this.ulContainer.style.top = `${height}px`
         this.ulContainer.style.left = `${widthSpan - (this.width ?? 0)}px`
       }
     }
@@ -372,12 +260,8 @@ class ListeDeroulante extends HTMLElement {
 
   /**
      * Remet la liste dans son état initial
-     * @param {Object} [options]
-     * @param {boolean} [options.withoutCallback] Passer true pour ne pas appeler la callback
      */
-  reset ({ withoutCallback }: {
-        withoutCallback?: boolean
-    } = {}) {
+  reset () {
     this.hide()
     if (this.spanSelected) {
       empty(this.spanSelected)
@@ -388,7 +272,6 @@ class ListeDeroulante extends HTMLElement {
       this.reponse = ''
       this.changed = false
       this._replace() // au cas où la hauteur de spanSelected aurait changé
-      if (!withoutCallback && this.onChange && this.initialChoice) this.onChange(this.initialChoice)
     }
   }
 
@@ -397,11 +280,9 @@ class ListeDeroulante extends HTMLElement {
      * @param {number} index index dans choices
      * @param {Object} [options]
      * @param {boolean} [options.withoutOffset] Passer true si l'index est celui du tableau après avoir éventuellement retiré son 1er élément (à priori usage interne seulement)
-     * @param {boolean} [options.withoutCallback] Passer true pour ne pas appeler la callback (seulement à l'init à priori)
      */
-  select (index: number, { withoutOffset, withoutCallback }: {
+  select (index: number, { withoutOffset }: {
         withoutOffset?: boolean,
-        withoutCallback?: boolean
     } = {}) {
     if (this.spanSelected != null) {
       if (this.disabled) return
@@ -417,18 +298,9 @@ class ListeDeroulante extends HTMLElement {
       empty(this.spanSelected)
       const choix = this.choices[realIndex]
       afficheChoice(this.spanSelected, choix)
-      if (typeof choix === 'object' && 'latex' in choix) {
-        const mf = this.spanSelected.querySelector('math-field')
-        const div = this.spanSelected.querySelector('div')
-        if (mf && div) {
-          div.style.height = String(Math.max(mf.clientHeight, 25)) + 'px'
-          div.style.width = String(Math.max(mf.clientWidth, 70)) + 'px'
-        }
-      }
       this.reponse = String(typeof choix === 'string' ? choix : typeof choix === 'object' ? choix?.value : 'undefined')
       if (this.reponse === 'undefined') throw Error(`Un problème avec la valeur de ce choix : ${choix}`)
       this.changed = true
-      if (this.onChange && !withoutCallback && choix != null) this.onChange(choix)
       this._kbIndex = index
       for (const [i, li] of this._elts.entries()) {
         if (i === index) li.classList.add('selected')
@@ -487,75 +359,35 @@ class ListeDeroulante extends HTMLElement {
   }
 
   /**
-     * Retourne l'index de la réponse courante (dans le tableau fourni initialement)
-     * @return {number}
-     */
-  getReponseIndex (): number {
-    const index = this.givenChoices?.indexOf(this.reponse)
-    if (index == null || index < 0) return 0 // si on a rien choisi index vaut -1
-    return index
-  }
-
-  /**
-     * Retourne true si la correction est passée et a indiqué que c'était bon, false si c'était faux, undefined si corrige n'a pas été appelé
-     * @type {boolean|undefined}
-     */
-  isOk (): boolean | undefined {
-    if (this.spanSelected) {
-      if (this.spanSelected.classList.contains('ok')) return true
-      if (this.spanSelected.classList.contains('ko')) return false
-    }
-    return undefined
-    // sinon ça retourne undefined
-  }
-
-  /**
      * Ajoute la liste dans le dom et retourne l'objet ListeDeroulante qu'elle a créé
      * @name ListeDeroulante
      * @param {HTMLElement|string} conteneur
      * @param {CHoiceValue[]} choices la liste des choix
      * @param {Object} [parametres]  Les paramètres de cette liste
-     * @param {(AllChoiceType)=>void} [parametres.onChange] fonction à exécuter lors de la sélection d'un choix (appelée avec la valeur du choix)
-     * @param {boolean} [parametres.centre=false] les choix sont centrés (et pas alignés à gauche)
      * @param {boolean} [parametres.sansFleche=false] passer true pour ne pas mettre la flèche permettant le déroulement
      * @param {number} [parametres.select=0] index de choices à sélectionner dès le départ (ignoré si choix est précisé)
-     * @param {boolean} [parametres.decalage=false] true pour un décalage , quand le j3pcontainer est bizarre, faut redécaler à la main la liste
      * @return {ListeDeroulante}
      */
   static create (conteneur: HTMLElement, choices: AllChoicesType, {
-    onChange,
-    centre,
     choix0,
     sansFleche,
-    select,
-    decalage
+    select
   }: {
-        onChange?: null | ((choice: AllChoiceType) => void),
-        centre?: boolean,
         choix0?: boolean,
         sansFleche?: boolean,
         select?: number,
-        dansModale?: boolean,
-        decalage?: number
     } = {
-    onChange: null,
-    centre: false,
     choix0: false,
     sansFleche: false,
-    select: 0,
-    dansModale: false,
-    decalage: 0
+    select: 0
   }): ListeDeroulante {
     const ld = new ListeDeroulante(choices, {
-      onChange,
-      choix0,
-      decalage
+      choix0
     })
     if (!conteneur) throw Error('Impossible de créer la liste déroulante, pas de conteneur')
-    ld._init({ centre, conteneur, sansFleche, select })
+    ld._init({ conteneur, sansFleche, select })
     return ld
   }
 }
-customElements.define('liste-deroulante', ListeDeroulante)
 
 export default ListeDeroulante
