b0b398dbb24ea2a8769a80ae3929f9aa63df5534
Cristobal-84 <76251099+Cristobal-84@users.noreply.github.com>
2023-11-15 13:24:12 +0100

    2N51-5 : Changement titre

--- src/exercices/2e/2N51-5.js
+++ src/exercices/2e/2N51-5.js
@@ -1,5 +1,5 @@
 import ExerciceEquation1Tiret2 from '../3e/3L13-1.js'
-export const titre = 'Équations du premier degré (utilisant la distributivité)'
+export const titre = 'Résoudre une équation du premier degré (utilisant la distributivité)'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 
