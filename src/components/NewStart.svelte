e70274efe46f3dcfe89dc7ffec7cda729adea52b
sylcha <sylvain.chambon@ac-toulouse.fr>
2023-11-22 16:24:06 +0100

    fix: icone bouton menu choix

--- src/components/NewStart.svelte
+++ src/components/NewStart.svelte
@@ -41,6 +41,7 @@
   let divExercices: HTMLDivElement
   let isNavBarVisible: boolean = true
   let chipsListDisplayed: boolean = false
+  let sidenavOpen: boolean = false
   /**
    * Démarrage
    */
@@ -300,13 +301,19 @@
           class="hidden md:flex justify-center items-center absolute left-0 bottom-0 h-10 w-10 bg-coopmaths-canvas-dark dark:bg-coopmathsdark-canvas-dark"
         >
           <button
+            type="button"
             data-te-sidenav-toggle-ref
             data-te-target="#choiceSideMenuWrapper"
             aria-controls="#choiceSideMenuWrapper"
             aria-haspopup="true"
+            on:click={() => {
+              sidenavOpen = !sidenavOpen
+            }}
           >
             <i
-              class="bx bx-right-arrow-alt text-2xl text-coopmaths-action dark:text-coopmathsdark-action hover:text-coopmaths-action-lightest hover:dark:text-coopmathsdark-action-lightest"
+              class="bx {sidenavOpen
+                ? 'bx-right-arrow-alt'
+                : 'bx-x'} text-2xl text-coopmaths-action dark:text-coopmathsdark-action hover:text-coopmaths-action-lightest hover:dark:text-coopmathsdark-action-lightest"
             />
           </button>
         </div>
