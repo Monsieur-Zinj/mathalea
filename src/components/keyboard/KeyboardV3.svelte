2b5ccee0c37eb4fe21969bb209b8b91579650fb9
sylcha <sylvain.chambon@ac-toulouse.fr>
2023-12-19 17:56:49 +0100

    feat: ajout de la fonte mono

--- src/components/keyboard/KeyboardV3.svelte
+++ src/components/keyboard/KeyboardV3.svelte
@@ -32,7 +32,7 @@
   <div
     transition:fly={{ y: '100%' }}
     bind:this={divKeyboard}
-    class="bg-coopmaths-canvas-darkest dark:bg-coopmathsdark-canvas-darkest p-4 flex flex-row space-x-4 justify-center items-center w-full fixed bottom-0 left-0 right-0 z-[9999]"
+    class="bg-coopmaths-struct dark:bg-coopmathsdark-struct p-4 flex flex-row space-x-4 justify-center items-center w-full fixed bottom-0 left-0 right-0 z-[9999]"
   >
     <div class="grid grid-cols-{numberOfBlocks} gap-3 md:gap-6">
       {#each numeric2 as block}
--- src/components/keyboard/KeycapV2.svelte
+++ src/components/keyboard/KeycapV2.svelte
@@ -38,7 +38,7 @@
 
 <button
   bind:this={button}
-  class="min-w-[20px] md:min-w-[30px] lg:min-w-[40px] xl:min-w-[80px] h-full flex justify-center items-center text-xs md:text-base text-coopmaths-corpus-light dark:text-coopmathsdark-corpus-light active:text-coopmaths-canvas dark:active:text-coopmathsdark-canvas bg-coopmaths-canvas-darkest dark:bg-coopmathsdark-canvas-darkest active:bg-coopmaths-action dark:active:bg-coopmathsdark-action {isSpecial ? 'border-2' : 'border'} border border-coopmaths-action dark:border-coopmathsdark-action py-1.5 px-2 md:py-2 md:px-4 text-center rounded-md"
+  class="min-w-[20px] md:min-w-[30px] lg:min-w-[40px] xl:min-w-[80px] h-full flex justify-center items-center text-xs md:text-base text-coopmaths-corpus-light dark:text-coopmathsdark-corpus-light active:text-coopmaths-canvas dark:active:text-coopmathsdark-canvas  active:bg-coopmaths-action dark:active:bg-coopmathsdark-action {isSpecial ? 'bg-coopmaths-struct-lightest dark:bg-coopmathsdark-struct-lightest' : 'bg-coopmaths-canvas dark:bg-coopmathsdark-canvas-light'}  py-1.5 px-2 md:py-2 md:px-4 text-center rounded-md"
   on:click={clickKeycap}
 >
   <div id="key-{data.key}" class="relative">
