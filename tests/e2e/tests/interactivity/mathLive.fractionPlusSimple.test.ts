e4c2c77b1a6d450b7a1a0c986748a2fe8ac10292
Rémi Angot <remi.angot@gmail.com>
2023-12-31 06:36:42 +0100

    test: fractionPlusSipmle ajout de faux positifs
    Il fallait s'assurer qu'une fraction égale mais pas plus simple ne soit pas comptée juste

--- tests/e2e/tests/interactivity/mathLive.fractionPlusSimple.test.ts
+++ tests/e2e/tests/interactivity/mathLive.fractionPlusSimple.test.ts
@@ -1,30 +1,33 @@
+import FractionEtendue from '../../../../src/modules/FractionEtendue'
 import { checkFeedback, getQuestions, inputAnswer, runTest } from '../../helpers/run'
 import type { Page } from 'playwright'
-import { KatexHandler } from '../../helpers/KatexHandler'
 
-async function test (page: Page) {
-  const urlExercice = 'http://localhost:5173/alea/?uuid=f8f4e&id=5N13&n=10&d=10&s=11&s2=false&i=1&cd=1' // Mettre ici l'url de l'exercice (éventuellement avec la graine mais push sans la graine)
+async function testFractionSimplifiee (page: Page) {
+  // Ce test s'assure que les fractions simplifiée pas au maximum sont acceptées
+  // et qu'une fraction égale (celle de l'énoncé) mais pas simplifiée n'est pas acceptée
+  const urlExercice = 'http://localhost:5173/alea/?uuid=f8f4e&id=5N13&n=50&d=10&s=50&s2=false&alea=EeW9&i=1&cd=1'
   const questions = await getQuestions(page, urlExercice)
+
   for (const question of questions) {
-    const katexFrac = new KatexHandler(page, question, { has: page.locator('mfrac') })
-    const fraction = await katexFrac.getFraction()
-    const { num, den } = fraction ?? { num: undefined, den: undefined }
-    if (num == null || den == null) throw Error(`getFraction n'a pas trouvé la fraction : ${fraction}`)
-    const [n, d] = [num, den].map(Number)
-    let reponse
-    for (const diviseur of [2, 3, 5, 7, 11, 13, 17]) {
-      if (n % diviseur === 0 && d % diviseur === 0) {
-        reponse = question.isCorrect
-          ? `${(n / diviseur).toFixed(0)}/${(d / diviseur).toFixed(0)}`
-          : `${(n / diviseur).toFixed(0)}/${(d).toFixed(0)}`
-        break
+    const den = Number(question.katex.elements[0][0])
+    const num = Number(question.katex.elements[0][1])
+    const fraction = new FractionEtendue(num, den)
+    let reponse = ''
+    if (question.isCorrect) {
+      reponse = `${fraction.n}/${fraction.d}` // fraction irréductible
+      for (const k of [2, 3, 5, 7, 11, 13, 17, 19, 23]) {
+        if (num % k === 0 && den % k === 0) {
+          reponse = `${num / k}/${den / k}` // fraction simplifiée mais pas forcément irréductible
+          break
+        }
       }
+    } else {
+      reponse = `${fraction.num}/${fraction.den}` // fraction égale mais non simplifiée
     }
-    if (reponse == null) throw Error(`Je n'ai pas réussi à simplifier ${fraction}`)
     await inputAnswer(page, question, reponse)
   }
+
   await checkFeedback(page, questions)
   return true
 }
-
-runTest(test, import.meta.url, { pauseOnError: true }) // true pendant le développement, false ensuite
+runTest(testFractionSimplifiee, import.meta.url, { pauseOnError: false })
