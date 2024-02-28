32936e57a641ada7396a6eebf3fd1dcaa50e307a
Gilles Mora <gilles.mora@ac-normandie.fr>
2024-02-28 14:45:31 +0000

    Gilles can3 2024

---
+++ src/exercices/can/3e/can3a-2024.ts
@@ -0,0 +1,97 @@
+import { setReponse } from '../../../lib/interactif/gestionInteractif'
+import Exercice from '../../Exercice'
+
+import Question11 from '../can3a-2024/can3-2024-Q11.js'
+import Question12 from '../can3a-2024/can3-2024-Q12.js'
+import Question13 from '../can3a-2024/can3-2024-Q13.js'
+import Question14 from '../can3a-2024/can3-2024-Q14.js'
+import Question15 from '../can3a-2024/can3-2024-Q15.js'
+import Question16 from '../can3a-2024/can3-2024-Q16.js'
+import Question17 from '../can3a-2024/can3-2024-Q17.js'
+import Question18 from '../can3a-2024/can3-2024-Q18.js'
+import Question19 from '../can3a-2024/can3-2024-Q19.js'
+import Question20 from '../can3a-2024/can3-2024-Q20.js'
+import Question21 from '../can3a-2024/can3-2024-Q21.js'
+import Question22 from '../can3a-2024/can3-2024-Q22.js'
+import Question23 from '../can3a-2024/can3-2024-Q23.js'
+import Question24 from '../can3a-2024/can3-2024-Q24.js'
+import Question25 from '../can3a-2024/can3-2024-Q25.js'
+import Question26 from '../can3a-2024/can3-2024-Q26.js'
+import Question27 from '../can3a-2024/can3-2024-Q27.js'
+import Question28 from '../can3a-2024/can3-2024-Q28.js'
+import Question29 from '../can3a-2024/can3-2024-Q29.js'
+import Question30 from '../can3a-2024/can3-2024-Q30.js'
+import { ajouteChampTexteMathLive } from '../../../lib/interactif/questionMathLive'
+
+export const titre = 'CAN cinquième sujet 2024'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const uuid = 'f3ceb'
+// export const ref = 'can3a-2024'
+
+/**
+ * Annales CAN 2024
+ * @author Gilles Mora
+*/
+
+export default class Can3a2024 extends Exercice {
+  constructor () {
+    super()
+    this.nbQuestions = 30
+    this.nbQuestionsModifiable = false
+    this.formatChampTexte = 'largeur01 inline nospacebefore'
+    this.formatInteractif = 'calcul'
+    this.besoinFormulaireCaseACocher = ['Sujet officiel']
+    this.sup = false
+  }
+
+  nouvelleVersion () {
+    this.listeQuestions = []
+    this.listeCorrections = []
+    this.autoCorrection = []
+    this.listeCanEnonces = []
+    this.listeCanReponsesACompleter = []
+
+    let indexQuestion = 0
+    for (const Question of [
+      Question11,
+      Question12,
+      Question13,
+      Question14,
+      Question15,
+      Question16,
+      Question17,
+      Question18,
+      Question19,
+      Question20,
+      Question21,
+      Question22,
+      Question23,
+      Question24,
+      Question25,
+      Question26,
+      Question27,
+      Question28,
+      Question29,
+      Question30]) {
+      const Q = new Question()
+      Q.canOfficielle = this.sup
+      Q.interactif = this.interactif
+      Q.nouvelleVersion()
+      this.formatChampTexte = Q.formatChampTexte
+      this.formatInteractif = Q.formatInteractif
+      setReponse(this, indexQuestion, Q.reponse, { formatInteractif: Q.formatInteractif })
+      let texte = Q.question
+      if (this.interactif) {
+        texte += ajouteChampTexteMathLive(this, indexQuestion, Q.formatChampTexte || '', Q.optionsChampTexte || {})
+      }
+      this.canEnonce = Q.canEnonce
+      this.canReponseACompleter = ''
+      this.listeCanEnonces.push(Q.canEnonce!)
+      this.listeCanReponsesACompleter.push(Q.canReponseACompleter!)
+      this.listeQuestions.push(texte!)
+      this.listeCorrections.push(Q.correction!)
+      indexQuestion++
+    }
+  }
+}
