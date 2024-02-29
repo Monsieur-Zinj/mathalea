a68b04dcdc21b3e5c1f325ba91bd20c530f8cdb0
Rémi Angot <remi.angot@gmail.com>
2024-02-29 13:54:37 +0100

    Suppression de toString pour les fractionEtendue
    Avec toString, les méthode de comparaison comme fractionEgaleCompare ne fonctionnent plus

--- src/modules/FractionEtendue.js
+++ src/modules/FractionEtendue.js
@@ -444,9 +444,9 @@ class FractionEtendue extends Fraction {
    *
    * @returns {string}
    */
-  toString () {
-    return this.valeurDecimale.toFixed(4)
-  }
+  // toString () {
+  //   return this.valeurDecimale.toFixed(4)
+  // }
 
   /**
    * Pour la conversion automatique en number
