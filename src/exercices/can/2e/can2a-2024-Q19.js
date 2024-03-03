cb8912756a13d61920b09fa078a340b7de979381
lhotejc <lhotejc@gmail.com>
2024-03-03 16:43:47 +0100

    configuration de fillInTheBlank pour les exercices simples.

--- src/exercices/can/2e/can2a-2024-Q19.js
+++ src/exercices/can/2e/can2a-2024-Q19.js
@@ -3,6 +3,7 @@ import { miseEnEvidence } from '../../../lib/outils/embellissements'
 import { randint } from '../../../modules/outils.js'
 import { choice } from '../../../lib/outils/arrayOutils'
 import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+import { numberCompare } from '../../../lib/interactif/comparisonFunctions'
 export const titre = 'Calculer avec une puissance'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -19,18 +20,17 @@ export default class NomExercice extends Exercice {
     this.typeExercice = 'simple'
     this.nbQuestions = 1
     this.formatChampTexte = 'largeur01 inline nospacebefore ' + KeyboardType.clavierDeBaseAvecFraction
-    this.formatInteractif = 'calcul'
-    this.optionsChampTexte = { texteAvant: '$? =$' }
+    this.formatInteractif = 'fillInTheBlank'
+    this.compare = numberCompare
   }
 
   nouvelleVersion () {
     if (this.canOfficielle) {
       this.question = '$\\left(2^3\\right)^4=2^{\\ldots}$'
       if (this.interactif) {
-        this.question = `$\\left(2^3\\right)^4=2^{?}$<br>
-        `
+        this.question = '\\left(2^3\\right)^4=2^{%{champ1}}'
       }
-      this.reponse = 12
+      this.reponse = '12'
       this.correction = `On utilise la formule $\\left(a^n\\right)^p=a^{n\\times p}$
         avec $a=2$,  $n=3$ et $p=4$.<br>
         $\\left(2^3\\right)^4 =2^{3\\times 4}=${miseEnEvidence(this.reponse)}$`
@@ -44,11 +44,14 @@ export default class NomExercice extends Exercice {
           n = randint(2, 5)
           p = randint(2, 5)
           s = n + p
-          this.reponse = `${a}^{${n + p}}`
-          this.question = `Écrire sous la forme d'une puissance de $${a}$ :<br>
-            $${a}^{${n}}\\times ${a}^{${p}}$`
+          this.reponse = s.toString()
+          if (this.interactif) {
+            this.question = `\\text{Écrire sous la forme d'une puissance de }${a}\\quad :\\quad ${a}^{${n}}\\times ${a}^{${p}}=${a}^{%{champ1}}`
+          } else {
+            this.question = `Écrire sous la forme d'une puissance de $${a}$ :<br> $${a}^{${n}}\\times ${a}^{${p}}$`
+          }
           this.correction = `On utilise la formule $a^n\\times a^m=a^{n+m}$ avec $a=${a}$, $n=${n}$ et $p=${p}$.<br>
-            $${a}^{${n}}\\times ${a}^{${p}}=${a}^{${n}+${p}}=${miseEnEvidence(this.reponse)}$`
+            $${a}^{${n}}\\times ${a}^{${p}}=${a}^{${n}+${p}}=${miseEnEvidence(`${a}^{${n + p}}`)}$`
 
           break
 
@@ -57,12 +60,17 @@ export default class NomExercice extends Exercice {
           p = randint(2, 5)
           n = randint(2, 4, p)
           s = n * p
-          this.reponse = `${a}^{${s}}`
-          this.question = `Écrire sous la forme d'une puissance de $${a}$ :<br>
+          this.reponse = s.toString()
+          if (this.interactif) {
+            this.question = `\\text{Écrire sous la forme d'une puissance de }${a}\\quad :\\quad \\left(${a}^{${n}}\\right)^{${p}}=${a}^{%{champ1}}`
+          } else {
+            this.question = `Écrire sous la forme d'une puissance de $${a}$ :<br>
             $\\left(${a}^{${n}}\\right)^{${p}}$`
+          }
+
           this.correction = `On utilise la formule $\\left(a^n\\right)^p=a^{n\\times p}$
             avec $a=${a}$,  $n=${n}$ et $p=${p}$.<br>
-            $\\left(${a}^{${n}}\\right)^{${p}}=${a}^{${n}\\times ${p}}=${miseEnEvidence(this.reponse)}$`
+            $\\left(${a}^{${n}}\\right)^{${p}}=${a}^{${n}\\times ${p}}=${miseEnEvidence(`${a}^{${s}}`)}$`
 
           break
       }
