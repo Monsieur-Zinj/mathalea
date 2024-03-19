601852fd2266b2e6c36b6634799783e9dea2b914
Nathan Scheinmann <nathan.scheinmann@edu.ge.ch>
2024-03-19 19:49:55 +0000

    création des fichiers nécessaires pour lancer updateMenuCH afin de créer le json exercicesCH

--- tasks/updateMenuCH.js
+++ tasks/updateMenuCH.js
@@ -12,7 +12,7 @@
  * Les titres des niveaux, thèmes et sous-thèmes sont gérés dans src/json/levelsThemesList.json
  *
  * Pour ajouter un nouveau chapitre, il faut donc l'écrire dans emptyRef2022.json puis éventuellement
- * mettre à jour src/json/levelsThemesList.json ou src/json/codeToLevelList.json
+ * mettre à jour src/levelsThemesList.json ou src/codeToLevelList.json
  *
  * ToDo : arrêter l'utilisation de referentielRessources.json
  *
@@ -34,12 +34,14 @@ async function readInfos (dirPath) {
       if (stat.isDirectory()) {
         await readInfos(filePath)
       } else if (stat.isFile()) {
-        // Si ce n'est pas un fichier .js ou .ts, on ne fait rien
-        if (file.match(/\.jsx?|\.ts$/) &&
+        // Check if it's a .js or .ts file, and exclude certain files
+        if (
+          file.match(/\.jsx?|\.ts$/) &&
           !file.startsWith('_') &&
           file !== 'deprecatedExercice.js' &&
-          file !== 'MetaExerciceCan.ts' &&
-          file !== 'Exercice.ts') {
+          file !== 'MetaExercice.ts' &&
+          file !== 'Exercice.ts'
+        ) {
           const infos = {}
           const data = await fs.readFile(filePath, 'utf8')
           const matchUuid = data.match(/export const uuid = '(.*)'/)
@@ -55,73 +57,93 @@ async function readInfos (dirPath) {
             // Pas d'erreur pour les fichiers beta
             if (!filePath.includes('/beta/')) console.error('\x1b[31m%s\x1b[0m', `uuid non trouvé dans ${filePath}`)
           }
-          const matchRef = data.match(/export const ref = '(.*)'/)
+          // const matchRefFR = data.match(/export const ref = '(.*)'/)
+          // if (matchRefFR) {
+          //   infos.idFR = matchRefFR[1]
+          // } else {
+          //   if (!filePath.includes('beta') &&
+          //     !filePath.includes('/apps/') &&
+          //     !filePath.includes('/ressources/')
+          //   ) {
+          //     console.error('\x1b[31m%s\x1b[0m', `ref non trouvé dans ${filePath}`)
+          //   }
+          // }
+          // Extract refs if present
+          const matchRef = data.match(/export const refs = {[^]*'fr-ch': \[([^\]]*)\]/)
           if (matchRef) {
-            infos.id = matchRef[1]
+            const refsArray = matchRef[1].split(',').map(ref => ref.trim().replace(/'/g, ''))
+
+            if (refsArray.length === 0) {
+              console.error('\x1b[31m%s\x1b[0m', `Empty refs array in ${filePath}`)
+            } else {
+              refsArray.forEach(ref => {
+                // const newInfos = { ...infos, id: ref }
+                if (matchRef) {
+                  infos.id = ref // matchRef[1]
+                }
+                const matchTitre = data.match(/export const titre = '(.*)'/) ||
+            data.match(/export const titre = "(.*)"/) ||
+            data.match(/export let titre = '(.*)'/)
+                if (matchTitre) {
+                  // ToDo : Est-ce qu'il y a d'autres caractères spéciaux à gérer que l'apostrophe ?
+                  infos.titre = matchTitre[1].replaceAll('\\\'', '\'').replaceAll('\\\\', '\\')
+                } else {
+                  console.error('\x1b[31m%s\x1b[0m', `titre non trouvé dans ${filePath}`)
+                }
+                const matchDate = data.match(/export const dateDePublication = '([^']*)'/)
+                if (matchDate) {
+                  infos.datePublication = matchDate[1]
+                }
+                const matchDateModif = data.match(/export const dateDeModifImportante = '([^']*)'/)
+                if (matchDateModif) {
+                  infos.dateModification = matchDateModif[1]
+                }
+                infos.features = {}
+                const matchInteractif = data.match(/export const interactifReady = (.*)/)
+                const matchInteractifType = data.match(/export const interactifType = (.*)/)
+                if (matchInteractif && matchInteractif[1] === 'true') {
+                  infos.features.interactif = {
+                    isActive: true,
+                    type: matchInteractifType[1] || ''
+                  }
+                } else {
+                  infos.features.interactif = {
+                    isActive: false,
+                    type: ''
+                  }
+                  exercicesNonInteractifs.push(filePath)
+                }
+                const matchAmcType = data.match(/export const amcType = '(.*)'/)
+                if (matchAmcType) {
+                  infos.features.amc = {
+                    isActive: true,
+                    type: matchAmcType[1] || ''
+                  }
+                } else {
+                  infos.features.amc = {
+                    isActive: false,
+                    type: ''
+                  }
+                }
+                infos.typeExercice = 'alea'
+                if (infos.id !== undefined) {
+                  exercicesShuffled[infos.id] = infos
+                  refToUuid[infos.id] = infos.uuid
+                }
+              })
+            }
           } else {
             if (!filePath.includes('beta') &&
-              !filePath.includes('/apps/') &&
-              !filePath.includes('a-2024') &&
-              !filePath.includes('/ressources/')
+                !filePath.includes('/apps/') &&
+                !filePath.includes('/ressources/')
             ) {
               console.error('\x1b[31m%s\x1b[0m', `ref non trouvé dans ${filePath}`)
             }
           }
-          const matchTitre = data.match(/export const titre = '(.*)'/) ||
-            data.match(/export const titre = "(.*)"/) ||
-            data.match(/export let titre = '(.*)'/)
-          if (matchTitre) {
-            // ToDo : Est-ce qu'il y a d'autres caractères spéciaux à gérer que l'apostrophe ?
-            infos.titre = matchTitre[1].replaceAll('\\\'', '\'').replaceAll('\\\\', '\\')
-          } else {
-            console.error('\x1b[31m%s\x1b[0m', `titre non trouvé dans ${filePath}`)
-          }
-          const matchDate = data.match(/export const dateDePublication = '([^']*)'/)
-          if (matchDate) {
-            infos.datePublication = matchDate[1]
-          }
-          const matchDateModif = data.match(/export const dateDeModifImportante = '([^']*)'/)
-          if (matchDateModif) {
-            infos.dateModification = matchDateModif[1]
-          }
-          infos.features = {}
-          const matchInteractif = data.match(/export const interactifReady = (.*)/)
-          const matchInteractifType = data.match(/export const interactifType = (.*)/)
-          if (matchInteractif && matchInteractif[1] === 'true') {
-            infos.features.interactif = {
-              isActive: true,
-              type: matchInteractifType[1] || ''
-            }
-          } else {
-            infos.features.interactif = {
-              isActive: false,
-              type: ''
-            }
-            exercicesNonInteractifs.push(filePath)
-          }
-          const matchAmcType = data.match(/export const amcType = '(.*)'/)
-          if (matchAmcType) {
-            infos.features.amc = {
-              isActive: true,
-              type: matchAmcType[1] || ''
-            }
-          } else {
-            infos.features.amc = {
-              isActive: false,
-              type: ''
-            }
-          }
-          infos.typeExercice = 'alea'
-          if (infos.id !== undefined) {
-            exercicesShuffled[infos.id] = infos
-            refToUuid[infos.id] = infos.uuid
-          }
         }
       }
-    })
-  )
+    }))
 }
-
 /**
  * Crée une Uuid de 5 caractères hexadécimaux (1M de possibilités)
  * @returns {string}
@@ -156,7 +178,7 @@ const refToUuid = {}
  * On utilise emptyRef2022 pour initialiser referentiel2022 avec les niveaux et les catégories
  * En cas de création de niveau ou de chapitre, il faudra mettre à jour ce fichier
  */
-const emptyRef2022 = readFileSync('tasks/emptyRef2022.json')
+const emptyRef2022 = readFileSync('tasks/emptyRefCH.json')
 const referentiel2022 = JSON.parse(emptyRef2022)
 
 const themesPath = []
@@ -188,11 +210,11 @@ readInfos(exercicesDir, uuidMap)
       obj[key] = exercicesShuffled[key]
       return obj
     }, {})
-    fs.writeFile('src/json/exercices.json', JSON.stringify(exercices, null, 2))
-    fs.writeFile('src/json/exercicesNonInteractifs.json', JSON.stringify(exercicesNonInteractifs.sort(), null, 2))
+    fs.writeFile('src/json/exercicesCH.json', JSON.stringify(exercices, null, 2))
+    fs.writeFile('src/json/exercicesNonInteractifsCH.json', JSON.stringify(exercicesNonInteractifs.sort(), null, 2))
     uuidToUrl = handleExerciceSvelte(uuidToUrl)
-    fs.writeFile('src/json/uuidsToUrl.json', JSON.stringify(uuidToUrl, null, 2))
-    fs.writeFile('src/json/refToUuid.json', JSON.stringify(refToUuid, null, 2))
+    fs.writeFile('src/json/uuidsToUrlCH.json', JSON.stringify(uuidToUrl, null, 2))
+    fs.writeFile('src/json/refToUuidCH.json', JSON.stringify(refToUuid, null, 2))
     for (const themePath of themesPath) {
       const theme = themePath.split('.').pop()
       for (const key in exercices) {
@@ -212,12 +234,12 @@ readInfos(exercicesDir, uuidMap)
         }
       }
     }
-    fs.writeFile('src/json/referentielGeometrieDynamique.json', JSON.stringify(referentiel2022['Géométrie dynamique'], null, 2))
+    fs.writeFile('src/json/referentielGeometrieDynamiqueCH.json', JSON.stringify(referentiel2022['Géométrie dynamique'], null, 2))
     delete referentiel2022['Géométrie dynamique']
-    fs.writeFile('src/json/referentiel2022.json', JSON.stringify(referentiel2022, null, 2).replaceAll('"c3"', '"CM1/CM2"'))
+    fs.writeFile('src/json/referentiel2022CH.json', JSON.stringify(referentiel2022, null, 2).replaceAll('"c3"', '"CM1/CM2"'))
   })
   .then(() => {
-    console.log('uuidsToUrl, referentiel2022 et referentielGeometrieDynamique ont été mis à jour')
+    console.log('uuidsToUrlCH, referentiel2022CH et referentielGeometrieDynamiqueCH ont été mis à jour')
   })
   .catch((err) => {
     console.error(err)
