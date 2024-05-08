0906c2a6a8738215829c99058af03e7c7dcbfae1
Rémi Angot <remi.angot@gmail.com>
2024-05-08 08:41:36 +0200

    Update can2a-2024-mai.ts

--- src/exercices/can/can2a-2024-2/can2a-2024-mai.ts
+++ src/exercices/can/can2a-2024-2/can2a-2024-mai.ts
@@ -1,11 +1,17 @@
 import Exercice from '../../Exercice'
 import { listeQuestionsToContenu } from '../../../modules/outils.js'
-import { ajouteChampTexteMathLive } from '../../../lib/interactif/questionMathLive'
+import { ajouteChampTexteMathLive, remplisLesBlancs } from '../../../lib/interactif/questionMathLive'
 import { miseEnEvidence } from '../../../lib/outils/embellissements'
 import { handleAnswers } from '../../../lib/interactif/gestionInteractif'
-import { calculCompare, equalFractionCompare, expressionDeveloppeeEtReduiteCompare, intervalsCompare } from '../../../lib/interactif/comparisonFunctions'
+import { calculCompare, equalFractionCompare, expressionDeveloppeeEtReduiteCompare, intervalsCompare, scientificCompare } from '../../../lib/interactif/comparisonFunctions'
 import { context } from '../../../modules/context'
 import { sp } from '../../../lib/outils/outilString'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+import { milieu, point } from '../../../lib/2d/points'
+import { grille } from '../../../lib/2d/reperes'
+import { segment, segmentAvecExtremites } from '../../../lib/2d/segmentsVecteurs'
+import { texteParPosition } from '../../../lib/2d/textes'
+import { mathalea2d } from '../../../modules/2dGeneralites'
 
 export const titre = 'Titre de l\'exercice'
 export const dateDePublication = '7/5/2024'
@@ -66,14 +72,14 @@ export default class nomExercice extends Exercice {
     handleAnswers(this, i, { reponse: { value: '0.08', compare: calculCompare } })
 
     i = 6
-    this.listeQuestions[i] = 'Augmenter une quantité de $17~\\%$ revient à la multiplier par...' + ajouteChampTexteMathLive(this, i)
+    this.listeQuestions[i] = 'Augmenter une quantité de $17~\\%$ revient à la multiplier par ' + ajouteChampTexteMathLive(this, i)
     this.listeCorrections[i] = `$1 + 17~\\% = \\dfrac{117}{100} = 1{,}17$ <br>  Augmenter une quantité de $17~\\%$ revient à la multiplier par $${miseEnEvidence('1{,}17')}$.`
     // @ts-expect-error problème de typage pour reponse
     handleAnswers(this, i, { reponse: { value: '1.17', compare: calculCompare } })
 
     i = 7
-    this.listeQuestions[i] = 'Médiane de la série :<br>15 ; 2 ; 12 ; 10 ; 7' + ajouteChampTexteMathLive(this, i)
-    this.listeCorrections[i] = `Série ordonnée : 2 ; 7 ; 10 ; 12 ; 15<br>L'effectif total est 5 donc la médiane est la 3e valeur : $${miseEnEvidence('10')}$.`
+    this.listeQuestions[i] = 'Médiane de la série :<br>$15\\;;\\;2\\;;\\;12\\;;\\;10\\;;\\;7$<br>' + ajouteChampTexteMathLive(this, i)
+    this.listeCorrections[i] = `Série ordonnée : $2\\;;\\;7\\;;\\;10\\;;\\;12\\;;\\;15$<br>L'effectif total est 5 donc la médiane est la 3e valeur : $${miseEnEvidence('10')}$.`
     // @ts-expect-error problème de typage pour reponse
     handleAnswers(this, i, { reponse: { value: '10', compare: calculCompare } })
 
@@ -115,7 +121,7 @@ export default class nomExercice extends Exercice {
     handleAnswers(this, i, { reponse: { value: '-15', compare: calculCompare } })
 
     i = 10
-    this.listeQuestions[i] = 'Si je parcours $1{,}5~\\text{km}$ en $10~\\text{min}$, quelle est ma vitesse moyenne en km/h ?<br>' + ajouteChampTexteMathLive(this, i)
+    this.listeQuestions[i] = 'Si je parcours $1{,}5~\\text{km}$ en $10~\\text{min}$, quelle est ma vitesse moyenne en $\\text{km/h}$ ?<br>' + ajouteChampTexteMathLive(this, i)
     this.listeCorrections[i] = `$1{,}5~\\text{km}$ en $10~\\text{min}$ donc $1{,}5~\\text{km} \\times 6 = 9~\\text{km}$ en une heure.<br>Ma vitesse moyenne est donc de $${miseEnEvidence('9')}~\\text{km/h}$.`
     // @ts-expect-error problème de typage pour reponse
     handleAnswers(this, i, { reponse: { value: '9', compare: calculCompare } })
@@ -127,13 +133,17 @@ export default class nomExercice extends Exercice {
     handleAnswers(this, i, { reponse: { value: '12', compare: calculCompare } })
 
     i = 12
-    this.listeQuestions[i] = 'TODO Liste déroulante' + ajouteChampTexteMathLive(this, i)
-    this.listeCorrections[i] = `$\\Big(2\\sqrt{3}\\Big)^2 = 4 \\times 3 =  ${miseEnEvidence('12')} $`
+    this.listeQuestions[i] = 'Comparer $0,7$ et $0,7^2$<br>'
+    this.listeQuestions[i] += '$0,7$' + ajouteChampTexteMathLive(this, i, KeyboardType.clavierCompare + ' largeur01') + '$0,7^2$'
+    this.listeCorrections[i] = `$0,7 \\lt 1$ donc $0{,}7~${miseEnEvidence('\\gt')}~0{,}7^2 $`
     // @ts-expect-error problème de typage pour reponse
-    handleAnswers(this, i, { reponse: { value: '12', compare: calculCompare } })
+    handleAnswers(this, i, { reponse: { value: '>', compare: calculCompare } })
 
     i = 13
-    this.listeQuestions[i] = 'Quel est le périmètre d\'un carré de $49~\\text{cm}^2$ ?<br>' + ajouteChampTexteMathLive(this, i) + ' cm'
+    this.listeQuestions[i] = 'Quel est le périmètre d\'un carré de $49~\\text{cm}^2$ ?<br>'
+    if (this.interactif) {
+      this.listeQuestions[i] += ajouteChampTexteMathLive(this, i) + ' cm'
+    }
     this.listeCorrections[i] = `L'aire est de $49~\\text{cm}^2$ donc c'est un carré de $7~\\text{cm}$ de côté.<br> Son périmètre est : $4 \\times 7~\\text{cm} =  ${miseEnEvidence('28')}~\\text{cm}$`
     // @ts-expect-error problème de typage pour reponse
     handleAnswers(this, i, { reponse: { value: '28', compare: calculCompare } })
@@ -145,16 +155,131 @@ export default class nomExercice extends Exercice {
     handleAnswers(this, i, { reponse: { value: ']-1;3[', compare: intervalsCompare } })
 
     i = 15
-    this.listeQuestions[i] = 'Soient $A(5\\;;\\;9)$ et $B(7\\;;\\;3)$ dans un repère orthonormé.<br>Déterminer les coordonnées du vecteur $\\overrightarrow{AB}$.<br>' + ajouteChampTexteMathLive(this, i) + ' cm'
-    this.listeCorrections[i] = `$AB = (x_B - x_A \\;;\\; y_B - y_A) = (7-5 \\;;\\; 3-9)   ${miseEnEvidence('(2 \\;;\\; -6 )')}$`
+    this.listeQuestions[i] = 'Soient $A(5\\;;\\;9)$ et $B(7\\;;\\;3)$ dans un repère orthonormé.<br>Déterminer les coordonnées du vecteur $\\overrightarrow{AB}$.<br>' + ajouteChampTexteMathLive(this, i)
+    this.listeCorrections[i] = `$\\overrightarrow{AB}(x_B - x_A \\;;\\; y_B - y_A) \\iff \\overrightarrow{AB}(7-5 \\;;\\; 3-9)  \\iff\\overrightarrow{AB}${miseEnEvidence('(2 \\;;\\; -6 )')}$`
     // @ts-expect-error problème de typage pour reponse
     handleAnswers(this, i, { reponse: { value: '(2;-6)', compare: calculCompare } })
 
     i = 16
-    this.listeQuestions[i] = 'Soient $A(5\\;;\\;9)$ et $B(7\\;;\\;3)$ dans un repère orthonormé.<br>Déterminer la longueur $AB$.<br>' + ajouteChampTexteMathLive(this, i) + ' cm'
+    this.listeQuestions[i] = 'Soient $A(5\\;;\\;9)$ et $B(7\\;;\\;3)$ dans un repère orthonormé.<br>Déterminer la longueur $AB$.<br>' + ajouteChampTexteMathLive(this, i)
     this.listeCorrections[i] = `$AB = \\sqrt{{x_{\\overrightarrow{AB}}}^2 + {y_{\\overrightarrow{AB}}}^2}=\\sqrt{2^2 + 6^2} =   ${miseEnEvidence('\\sqrt{40}')}$`
     // @ts-expect-error problème de typage pour reponse
     handleAnswers(this, i, { reponse: { value: '\\sqrt{40}', compare: calculCompare } })
+
+    i = 17
+    this.listeQuestions[i] = 'Écriture scientifique de $0{,}0314$' + ajouteChampTexteMathLive(this, i)
+    this.listeCorrections[i] = `$0{,}0314 =   ${miseEnEvidence('3{,}14\\times10^{-2}')}$`
+    // @ts-expect-error problème de typage pour reponse
+    handleAnswers(this, i, { reponse: { value: '3{,}14\\times10^{-2}', compare: scientificCompare } })
+
+    i = 18
+    this.listeQuestions[i] = 'Solutions de $x(x-3)=0$ :<br>'
+    if (this.interactif) {
+      this.listeQuestions[i] += '<em>(Écrire les solutions dans l\'ordre croissant.)</em><br>'
+      this.listeQuestions[i] += remplisLesBlancs(this, i, 'S=\\{%{champ1} \\;;\\; %{champ2}\\}\\text{.}')
+    }
+    this.listeCorrections[i] = `Un produit est nul si et seulement si l'un au moins de ses facteurs est nul.<br>$S=${miseEnEvidence('\\{0\\;;\\;3\\}')}$`
+    handleAnswers(this, i, {
+      // @ts-expect-error problème de typage pour reponse
+      bareme: (listePoints) => [Math.min(listePoints[0], listePoints[1]), 1],
+      champ1: { value: '0', compare: calculCompare },
+      champ2: { value: '3', compare: calculCompare }
+    },
+    { formatInteractif: 'mathlive' }
+    )
+
+    i = 19
+    this.listeQuestions[i] = 'Valeur de $5x - 8$ pour $x=\\dfrac{4}{5}$<br>' + ajouteChampTexteMathLive(this, i)
+    this.listeCorrections[i] = `$5\\times\\dfrac{4}{5} - 8 = 4 - 8 =   ${miseEnEvidence('-4')}$`
+    // @ts-expect-error problème de typage pour reponse
+    handleAnswers(this, i, { reponse: { value: '-4', compare: calculCompare } })
+
+    i = 20
+    const objets = []
+    objets.push(grille(-1, 0, 7, 4, 'gray', 1, 1))
+    const A = point(0, 3)
+    const B = point(0, 1)
+    const C = point(2, 1)
+    const D = point(2, 3)
+    const E = point(4, 3)
+    const F = point(4, 1)
+    const J = point(5, 1)
+    const K = point(5, 2)
+    const G = point(0, 4)// unite
+    const H = point(4, 4)// unite
+    const s1 = segmentAvecExtremites(G, H)
+    s1.epaisseur = 3
+    const s2 = segment(A, B, 'blue')
+    s2.epaisseur = 3
+    const s3 = segment(B, C, 'blue')
+    s3.epaisseur = 3
+    const s4 = segment(C, D, 'blue')
+    s4.epaisseur = 3
+    const s5 = segment(D, E, 'blue')
+    s5.epaisseur = 3
+    const s6 = segment(E, F, 'blue')
+    s6.epaisseur = 3
+    const s7 = segment(F, J, 'blue')
+    s7.epaisseur = 3
+    const s8 = segment(J, K, 'blue')
+    s8.epaisseur = 3
+    const xmin = -1
+    const ymin = 0
+    const xmax = 7
+    const ymax = 5
+    objets.push(
+      texteParPosition('1 u.l.', milieu(G, H).x, milieu(G, H).y + 0.7, 'milieu', 'black', context.isHtml ? 1 : 0.7),
+      s1, s2, s3, s4, s5, s6, s7, s8)
+    this.listeQuestions[i] = '<br>' + mathalea2d({
+      xmin,
+      ymin,
+      xmax,
+      ymax,
+      pixelsParCm: 20,
+      mainlevee: false,
+      amplitude: 0.5,
+      scale: 0.5,
+      style: 'margin: auto'
+    }, objets) + '<br>Longueur de la ligne brisée en unités de longueur (u.l) :<br>' + ajouteChampTexteMathLive(this, i)
+    // @ts-expect-error problème de typage pour reponse
+    handleAnswers(this, i, { reponse: { value: '3', compare: equalFractionCompare } })
+    this.listeCorrections[i] = `$L = ${miseEnEvidence('3')}$ u.l.`
+
+    i = 21
+    this.listeQuestions[i] = 'Un sac contient $15$ jetons identiques numérotés de $1$ à $15$. On tire un jeton au hasard.<br>Quelle est la probabilité qu\'il porte un nombre premier ?<br>'
+    this.listeQuestions[i] += ajouteChampTexteMathLive(this, i)
+    this.listeCorrections[i] = 'Les nombres premiers inférieurs ou égaux à $15$ sont : $2\\;;\\;3\\;;\\;5\\;;\\;7\\;;\\;11\\;;\\;13$.'
+    this.listeCorrections[i] += `<br>La probabilité de tirer un nombre premier est donc : $${miseEnEvidence('\\dfrac{6}{15}')}$`
+    // @ts-expect-error problème de typage pour reponse
+    handleAnswers(this, i, { reponse: { value: '\\dfrac{6}{15}', compare: equalFractionCompare } })
+
+    i = 22
+    this.listeQuestions[i] = 'La décomposition en produit de facteurs premiers de 30 est : <br>'
+    this.listeQuestions[i] += ajouteChampTexteMathLive(this, i)
+    this.listeCorrections[i] = `$30 = ${miseEnEvidence('2\\times3\\times5')}$`
+    // @ts-expect-error problème de typage pour reponse
+    handleAnswers(this, i, { reponse: { value: '2\\times3\\times5', compare: calculCompare } })
+
+    i = 23
+    this.listeQuestions[i] = 'Soient $A(4\\;;\\;9)$ et $B(2\\;;\\;4)$.<br>Déterminer le coefficient directeur de la droite $(AB)$.<br>' + ajouteChampTexteMathLive(this, i)
+    this.listeCorrections[i] = `$m=\\dfrac{y_B - y_A}{x_B - x_A} = \\dfrac{4 - 9}{2 - 4} = ${miseEnEvidence('\\dfrac{5}{2}')}$`
+    // @ts-expect-error problème de typage pour reponse
+    handleAnswers(this, i, { reponse: { value: '\\dfrac{5}{2}', compare: equalFractionCompare } })
+
+    i = 24
+    this.listeQuestions[i] = 'Coordonnées du point $M$ milieu du segment $[AB]$ où : $A(-2\\;;\\;3)$ et $B(2\\;;\\;7)$ :<br>'
+    if (this.interactif) {
+      this.listeQuestions[i] += remplisLesBlancs(this, i, 'M\\Big(\\{%{champ1} \\;;\\; %{champ2}\\Big)\\text{.}')
+    }
+    this.listeCorrections[i] = `$M\\Big( \\dfrac{x_A + x_B}{2}  \\;;\\;  \\dfrac{y_A + y_B}{2}\\Big) \\iff M\\Big( \\dfrac{-2 + 2}{2}  \\;;\\;  \\dfrac{3 + 7}{2}\\Big) \\iff M\\Big(${miseEnEvidence('0\\;;\\;5')}\\Big)$`
+    handleAnswers(this, i, {
+      // @ts-expect-error problème de typage pour reponse
+      bareme: (listePoints) => [Math.min(listePoints[0], listePoints[1]), 1],
+      champ1: { value: '0', compare: equalFractionCompare },
+      champ2: { value: '5', compare: equalFractionCompare }
+    },
+    { formatInteractif: 'mathlive' }
+    )
 
     listeQuestionsToContenu(this)
   }
