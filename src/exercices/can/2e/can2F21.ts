6dc4e5ee7c87c1097fd858a1eaf26cd5c09c7ba0
Rémi Angot <remi.angot@gmail.com>
2024-05-13 12:37:27 +0200

    numberCompare n'existe plus je l'ai remplacé par calculCompare

--- src/exercices/can/2e/can2F21.ts
+++ src/exercices/can/2e/can2F21.ts
@@ -2,9 +2,9 @@ import Exercice from '../../Exercice'
 import { miseEnEvidence } from '../../../lib/outils/embellissements'
 import { randint } from '../../../modules/outils'
 import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
-import { numberCompare } from '../../../lib/interactif/comparisonFunctions'
 import { choice } from '../../../lib/outils/arrayOutils'
 import { ecritureAlgebrique, reduireAxPlusB, rienSi1 } from '../../../lib/outils/ecritures'
+import { calculCompare } from '../../../lib/interactif/comparisonFunctions'
 export const titre = 'Déterminer un seuil avec un fonction affine'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -27,7 +27,7 @@ export default class seuilFctAff extends Exercice {
     this.nbQuestions = 1
     this.formatChampTexte = 'largeur01 nospacebefore ' + KeyboardType.clavierDeBase
     this.formatInteractif = 'calcul'
-    this.compare = numberCompare
+    this.compare = calculCompare
   }
 
   nouvelleVersion () {
