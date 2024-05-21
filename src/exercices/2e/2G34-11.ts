844b7ce55b14436293354afd17ded7c81ef382d2
EricElter <48183436+Deseux@users.noreply.github.com>
2024-05-21 08:09:37 +0200

    feat : remplacement de equalFractionCompare au profit de fonctionComparaison

--- src/exercices/2e/2G34-11.ts
+++ src/exercices/2e/2G34-11.ts
@@ -11,7 +11,7 @@ import { remplisLesBlancs } from '../../lib/interactif/questionMathLive'
 import { handleAnswers } from '../../lib/interactif/gestionInteractif'
 import { miseEnEvidence } from '../../lib/outils/embellissements'
 import { eqToLatex, printSystem } from '../../lib/outils/systemeEquations.js'
-import { equalFractionCompare } from '../../lib/interactif/comparisonFunctions.js'
+import { fonctionComparaison } from '../../lib/interactif/comparisonFunctions.js'
 export const titre = 'Déterminer le point d\'intersection de deux droites données graphiquement'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -237,7 +237,7 @@ export default class IntersectionDroites extends Exercice {
               bareme: (listePoints: number[]) => [Math.min(listePoints[0], listePoints[1]), 1],
               champ1: { value: pi12[0].texFractionSimplifiee },
               champ2: { value: pi12[1].texFractionSimplifiee },
-              compare: equalFractionCompare
+              compare: fonctionComparaison
             },
             { formatInteractif: 'fillInTheBlank' }
             )
@@ -255,7 +255,7 @@ export default class IntersectionDroites extends Exercice {
               champ4: { value: pi13[1].texFractionSimplifiee },
               champ5: { value: pi23[0].texFractionSimplifiee },
               champ6: { value: pi23[1].texFractionSimplifiee },
-              compare: equalFractionCompare
+              compare: fonctionComparaison
             },
             { formatInteractif: 'fillInTheBlank' }
             )
