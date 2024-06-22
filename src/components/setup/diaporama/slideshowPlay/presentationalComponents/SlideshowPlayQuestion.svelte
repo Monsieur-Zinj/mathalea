6e150ae52749f05b3d932d50f5e4e6eeb7d1da8e
Guillaume Valmont <guillaume.valmont@pm.me>
2024-06-22 19:31:31 +0400

    Les cartons entre les questions sont maintenant responsives

--- src/components/setup/diaporama/slideshowPlay/presentationalComponents/SlideshowPlayQuestion.svelte
+++ src/components/setup/diaporama/slideshowPlay/presentationalComponents/SlideshowPlayQuestion.svelte
@@ -13,16 +13,16 @@
 
 <main class="bg-coopmaths-canvas text-coopmaths-corpus dark:bg-coopmathsdark-canvas dark:text-coopmathsdark-corpus min-h-[80%] p-4">
   <div
-    class="{nbOfVues > 1
+    class="place-content-stretch justify-items-center w-full h-full {nbOfVues > 1
       ? 'grid grid-cols-2 gap-4 auto-rows-fr'
-      : 'grid grid-cols-1'} place-content-stretch justify-items-center w-full h-full"
+      : 'grid grid-cols-1'}"
   >
     {#each [...Array(nbOfVues).keys()] as i}
       <div
         id="diapocell{i}"
-        class="relative min-h-[100%] max-h-[100%] flex flex-col justify-center justify-self-stretch place-items-stretch p-2 {nbOfVues > 1
+        class="relative min-h-[100%] max-h-[100%] flex flex-col justify-center justify-self-stretch place-items-stretch p-2 text-center {nbOfVues > 1
           ? 'bg-coopmaths-canvas-dark dark:bg-coopmathsdark-canvas-dark'
-          : ''} text-center"
+          : ''}"
       >
         {#if nbOfVues > 1}
           <div class="absolute bg-coopmaths-struct text-coopmaths-canvas dark:bg-coopmathsdark-struct dark:text-coopmathsdark-canvas font-black text-4xl -top-1 -left-1 rounded-tl-2xl w-1/12 h-1/12">
@@ -37,28 +37,20 @@
           {#if isQuestionVisible}
             <div class="font-light" id="consigne{i}">
               <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-              {@html consignes[i][
-                order[currentQuestion]
-              ]}
+              {@html consignes[i][order[currentQuestion]]}
             </div>
             <div class="py-4" id="question{i}">
               <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-              {@html questions[i][
-                order[currentQuestion]
-              ]}
+              {@html questions[i][order[currentQuestion]]}
             </div>
           {/if}
           {#if isCorrectionVisible}
             <div
               id="correction{i}"
-              class=" {isCorrectionVisible
-                ? 'bg-coopmaths-warn-light bg-opacity-30 dark:bg-coopmathsdark-warn-light dark:bg-opacity-30 my-10'
-                : ''}"
+              class="bg-coopmaths-warn-light bg-opacity-30 dark:bg-coopmathsdark-warn-light dark:bg-opacity-30 my-10"
             >
               <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-              {@html corrections[i][
-                order[currentQuestion]
-              ]}
+              {@html corrections[i][order[currentQuestion]]}
             </div>
           {/if}
         </div>
@@ -66,16 +58,13 @@
     {/each}
   </div>
   <dialog
-    class="absolute top-0 left-0 h-full w-full bg-coopmaths-struct text-coopmaths-canvas dark:bg-coopmathsdark-struct dark:text-coopmathsdark-canvas text-[150px] font-extralight min-w-full min-h-full"
     id="transition"
+    class="absolute top-0 left-0 h-full w-full bg-coopmaths-struct text-coopmaths-canvas dark:bg-coopmathsdark-struct dark:text-coopmathsdark-canvas text-[150px] font-extralight min-w-full min-h-full"
   >
-    <div
-      class="flex w-full min-h-full h-full justify-center items-center"
-    >
+    <div class="flex w-full min-h-full h-full justify-center items-center">
       <div
         class="radial-progress"
-        style="--value:{((currentQuestion + 1) / questions[0].length) *
-          100}; --size:500px; --thickness: 20px;"
+        style="--value:{((currentQuestion + 1) / questions[0].length) * 100};"
       >
         {currentQuestion + 1} / {questions[0].length}
       </div>
@@ -87,4 +76,18 @@
   dialog::backdrop {
     backdrop-filter: blur(4px);
   }
+  .radial-progress {
+    font-size: 20vw;
+    --size: 60vw;
+    --thickness: 3vw;
+  }
+
+  @media (min-aspect-ratio: 1/1) {
+    .radial-progress {
+      font-size: 20vh;
+      --size: 60vh;
+      --thickness: 3vh;
+    }
+  }
+
 </style>
