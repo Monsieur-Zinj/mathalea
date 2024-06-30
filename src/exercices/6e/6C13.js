a857449b6e5f3c6d6417f746d6784ed7b991cb46
lhotejc <lhotejc@gmail.com>
2024-06-30 17:25:24 +0200

    6C13 devient interactif

--- src/exercices/6e/6C13.js
+++ src/exercices/6e/6C13.js
@@ -6,8 +6,14 @@ import {
   gestionnaireFormulaireTexte
 } from '../../modules/outils.js'
 import choisirExpressionNumerique from '../5e/_choisirExpressionNumerique.js'
+import { handleAnswers } from '../../lib/interactif/gestionInteractif'
+import { ajouteChampTexteMathLive, ajouteFeedback } from '../../lib/interactif/questionMathLive'
+import { propositionsQcm } from '../../lib/interactif/qcm'
 export const titre = 'Traduire des phrases en calculs et réciproquement'
 export const dateDeModifImportante = '08/05/2023'
+export const interactifReady = true
+export const interactifType = ['qcm', 'mathLive']
+
 /**
  * Mettre en relation un calcul, une traduction en français, une expression, un résultat, pour les décliner dans différents exercices.
  * Exercice sur le vocabulaire : somme,différence, produit, quotient...
@@ -52,39 +58,117 @@ export default function VocabulaireEtOperations () {
       expf = resultats[0]
       expn = resultats[1]
       expc = resultats[2]
+      const [nb1, nb2] = expn.match(/(\d+{*,*}*\d*)/g)
+      const operation = expn.match(/(\+|-|\\div|\\times)/g)
+      const resultString = expc.split('= ')[1]
+      const resultat = resultString.replace('$', '') // pour enlever le $
+      const motCle = expf.match(/([a-zA-Zéè]+)/g)[1]
+      const phrases = [
+        'La somme de § et de £',
+        'La différence entre § et £',
+        'Le produit de § par £',
+        'Le quotient de § par £'
+      ].map(el => el.replace('§', `$${nb1}$`).replace('£', `$${nb2}$`))
+      const calculs = [
+        '$§+£$',
+        '$§-£$',
+        '$§\\times £$',
+        '$§\\div £$'
+      ].map(el => el.replace('§', `$${nb1}$`).replace('£', `$${nb2}$`))
       texte = ''
       texteCorr = ''
+      let propsQcm
+      let monQcm
       switch (listeTypeDeQuestions[i]) {
-        case 1:
-          texte +=
-            numAlpha(i) +
-            'Traduire la phrase par un calcul (il n\'est pas demandé d\'effectuer ce calcul) : '
+        case 1: // proposer le calcul
+          texte += numAlpha(i) + (this.interactif
+            ? 'Choisir le bon calcul pour calculer '
+            : 'Traduire la phrase par un calcul (il n\'est pas demandé d\'effectuer ce calcul) : ')
           expf = 'l' + expf.substring(1)
           texte += `${expf}.`
           expf = 'L' + expf.substring(1)
           texteCorr += numAlpha(i) + `${expf} s'écrit ${expn}.`
+          propsQcm = {
+            texte,
+            propositions: [
+              {
+                texte: calculs[0],
+                statut: calculs[0].includes(operation)
+              },
+              {
+                texte: calculs[1],
+                statut: calculs[1].includes(operation)
+              },
+              {
+                texte: calculs[2],
+                statut: calculs[2].includes(operation)
+              },
+              {
+                texte: calculs[3],
+                statut: calculs[3].includes(operation)
+              }
+            ],
+            options: {
+              ordered: false
+            }
+          }
+          this.autoCorrection[i] = propsQcm
+          monQcm = propositionsQcm(this, i)
+          texte += this.interactif ? monQcm.texte : ''
           break
-        case 2:
+        case 2: // proposer une expression en français
           if (expn.indexOf('ou') > 0) { expn = expn.substring(0, expn.indexOf('ou')) } // on supprime la deuxième expression fractionnaire
-          texte +=
-            numAlpha(i) + 'Traduire le calcul par une phrase en français : '
+          texte += numAlpha(i) + (this.interactif
+            ? 'Choisir la bonne expression pour traduire '
+            : 'Traduire le calcul par une phrase en français : ')
           texte += `${expn}.`
           expf = 'l' + expf.substring(1)
           texteCorr += numAlpha(i) + `${expn} est ${expf}.`
+          propsQcm = {
+            texte,
+            propositions: [
+              {
+                texte: phrases[0],
+                statut: phrases[0].includes(motCle)
+              },
+              {
+                texte: phrases[1],
+                statut: phrases[1].includes(motCle)
+              },
+              {
+                texte: phrases[2],
+                statut: phrases[2].includes(motCle)
+              },
+              {
+                texte: phrases[3],
+                statut: phrases[3].includes(motCle)
+              }
+            ],
+            options: {
+              ordered: false
+            }
+          }
+          this.autoCorrection[i] = propsQcm
+          monQcm = propositionsQcm(this, i)
+          texte += this.interactif ? monQcm.texte : ''
           break
-        case 3:
-          texte +=
-            numAlpha(i) +
-            'Traduire la phrase par un calcul et effectuer ce calcul : '
+        case 3: // proposer un résultat
+          texte += numAlpha(i) + (this.interactif
+            ? 'Donner le résultat de : '
+            : 'Traduire la phrase par un calcul et effectuer ce calcul : ')
           expf = 'l' + expf.substring(1)
           texte += `${expf}.`
+          if (this.interactif) {
+            texte += ajouteChampTexteMathLive(this, i, 'inline largeur01') + ajouteFeedback(this, i)
+            handleAnswers(this, i, { reponse: { value: resultat } }, { formatInteractif: 'mathlive' })
+          }
           expf = 'L' + expf.substring(1)
           texteCorr += numAlpha(i) + `${expf} s'écrit ${expn}.<br>`
           texteCorr += `${expc}`
           break
       }
-      texte += this.nbQuestions - 1 === i ? '<br>' : ''
-      if (this.listeQuestions.indexOf(texte) === -1) {
+      // texte += this.nbQuestions - 1 === i ? '<br>' : ''
+      if (this.questionJamaisPosee(i, expn)) {
         // Si la question n'a jamais été posée, on en crée une autre
         this.listeQuestions.push(texte)
         this.listeCorrections.push(texteCorr)
