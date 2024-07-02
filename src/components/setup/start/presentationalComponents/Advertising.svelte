b47581a661fb3597d8a558422a6656aef9cebe4d
Rémi Angot <remi.angot@gmail.com>
2024-07-02 10:05:55 +0200

    Pub pour le cahier 2nde

--- src/components/setup/start/presentationalComponents/Advertising.svelte
+++ src/components/setup/start/presentationalComponents/Advertising.svelte
@@ -1,19 +1,26 @@
 <a
   href="https://coopmaths.fr/www/cahiers-de-vacances"
   target="_blank"
-  class="flex flex-col items-center mx-auto max-w-sm p-3 rounded-lg shadow
+  class="flex flex-col items-center mx-auto max-w-md p-3 rounded-lg shadow
     bg-coopmaths-canvas-dark border border-gray-200 hover:bg-gray-200
     dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
 >
   <h5 class="mb-2 text-2xl font-bold tracking-tight text-coopmaths-struct dark:text-white">
     Nouveau
   </h5>
-  <img
-    src="/alea/assets/images/cahier_vers_la_1ere_spe.png"
-    alt="Cahier de vacances vers la première spécialité"
-    class="max-w-48 mb-4"
-  />
+  <div class="flex space-x-2">
+    <img
+      src="/alea/assets/images/cahier_vers_la_2nde.png"
+      alt="Cahier de vacances vers la première spécialité"
+      class="max-w-48 mb-4 flex"
+    />
+    <img
+      src="/alea/assets/images/cahier_vers_la_1ere_spe.png"
+      alt="Cahier de vacances vers la première spécialité"
+      class="max-w-48 mb-4 flex"
+    />
+  </div>
   <p class="text-center font-normal text-gray-700 dark:text-gray-400">
-    Découvrez le cahier de vacances pour préparer la rentrée en première spécialité.
+    Découvrez les cahiers de vacances pour préparer la rentrée en seconde ou en première spécialité.
   </p>
 </a>
