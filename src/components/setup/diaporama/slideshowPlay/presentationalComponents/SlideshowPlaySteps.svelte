1d7ee58d1133628bb225da3dab6d3f72dc24716b
Guillaume Valmont <guillaume.valmont@pm.me>
2024-07-05 18:23:22 +0400

    À chaque changement de diapo, on scroll de façon à avoir la barre de chargement ainsi que le numéro de la diapo +/- 5 en vue

--- src/components/setup/diaporama/slideshowPlay/presentationalComponents/SlideshowPlaySteps.svelte
+++ src/components/setup/diaporama/slideshowPlay/presentationalComponents/SlideshowPlaySteps.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+
   export let isManualModeActive: boolean | undefined
   export let currentQuestionNumber: number
   export let totalQuestionsNumber: number
@@ -6,55 +8,39 @@
   export let ratioTime: number
   export let slideDuration: number
 
-  let stepsUl: HTMLUListElement
+  let stepsUl: HTMLElement | null
+  onMount(() => {
+    stepsUl = document.getElementById('stepsUl')
+  })
 
   $: {
     if (stepsUl) {
-      const steps = stepsUl.querySelectorAll('li')
-      if (typeof steps !== 'undefined') {
-        if (steps[currentQuestionNumber]) steps[currentQuestionNumber].scrollIntoView()
-        if (steps[currentQuestionNumber + 5]) {
-          steps[currentQuestionNumber + 5].scrollIntoView()
-        }
-        if (
-          steps[currentQuestionNumber - 5] &&
-          !isInViewport(steps[currentQuestionNumber - 5])
-        ) {
-          steps[currentQuestionNumber - 5].scrollIntoView()
-        }
-      }
+      const steps = stepsUl.querySelectorAll('button')
+      if (steps[currentQuestionNumber]) steps[currentQuestionNumber].scrollIntoView()
+      if (steps[currentQuestionNumber + 5]) steps[currentQuestionNumber + 5].scrollIntoView()
+      if (steps[currentQuestionNumber - 5]) steps[currentQuestionNumber - 5].scrollIntoView()
+      const diapoProgressContainer = document.getElementById('diapoProgressContainer')
+      if (diapoProgressContainer) diapoProgressContainer.scrollIntoView()
     }
   }
 
-function isInViewport (element: HTMLElement): boolean {
-  const rect = element.getBoundingClientRect()
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  )
-}
 </script>
 
 <div
+  id="diapoProgressContainer"
   class:invisible={isManualModeActive}
   class="flex flex-row flex-shrink-0 h-6 border
     border-coopmaths-warn dark:border-coopmathsdark-warn"
 >
   <div
-    id="diapoProgressBar"
     class="bg-coopmaths-warn dark:bg-coopmathsdark-warn"
     style="width: {ratioTime}%; transition: width {slideDuration / 100}s linear"
   />
 </div>
-<ul class="steps w-full mt-3">
+<ul id="stepsUl" class="steps w-full mt-3">
   {#each [...Array(totalQuestionsNumber).keys()] as i}
     <button
       on:click={() => goToQuestion(i)}
-      on:keydown={() => goToQuestion(i)}
-      tabindex="0"
       class="cursor-pointer
         step dark:step-info
         {currentQuestionNumber === i ? 'step-current' : ''}
