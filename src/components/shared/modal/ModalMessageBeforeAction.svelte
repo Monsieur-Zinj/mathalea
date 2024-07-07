4dcd7ca823a5fdd3ac745a59a5fd0f44bc739684
Guillaume Valmont <guillaume.valmont@pm.me>
2024-07-07 15:22:05 +0400

    ModalMessageBeforeAction dispose maintenant aussi d'une version dark mode

--- src/components/shared/modal/ModalMessageBeforeAction.svelte
+++ src/components/shared/modal/ModalMessageBeforeAction.svelte
@@ -67,34 +67,26 @@
 -->
 
 <div
-  class="fixed hidden inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"
+  class="fixed hidden inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50"
   id={modalId}
 >
-  <!--
-  <slot name="header" />
-  <slot name="content" /> -->
-  <!--modal content-->
-  <div
-    class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-coopmaths-canvas"
+  <div class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md
+    bg-coopmaths-canvas dark:bg-coopmathsdark-canvas"
   >
     <div class="mt-3 text-center">
-      <div
-        class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-coopmaths-warn-100"
-      >
+      <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-coopmaths-warn-100">
         <div class="h-6 w-6 text-coopmaths-warn-darkest">
           <i class="bx bx-sm {icon}" />
         </div>
       </div>
       <div class="text-3xl pt-4 leading-6 font-medium text-coopmaths-warn-dark">
-        <slot name="header">
-          <span>Successful!</span>
-        </slot>
+        <slot name="header" />
       </div>
       <div class="mt-2 px-7 py-3">
-        <div class="text-sm text-coopmaths-corpus-light">
-          <slot name="content">
-            <span>Account has been successfully registered!</span>
-          </slot>
+        <div class="text-sm
+          text-coopmaths-corpus-light dark:text-coopmathsdark-corpus-light"
+        >
+          <slot name="content" />
         </div>
       </div>
       <div class="items-center px-4 py-3">
