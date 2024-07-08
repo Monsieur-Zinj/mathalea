3234b1168a5cbdbc5414b56d41f3a0aec96c859a
Guillaume Valmont <guillaume.valmont@pm.me>
2024-07-08 13:06:16 +0400

    Création de ButtonIconTooltip qui ajoute un tooltip au ButtonIcon mais devient un div et n'est plus un button

---
+++ src/components/shared/forms/ButtonIconTooltip.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import ButtonIcon from './ButtonIcon.svelte'
+  export let tooltip: string
+  export let icon: string
+  export let classForButton: string
+  export let buttonSecondIcon: string = ''
+
+</script>
+
+<!-- À la différence de ButtonIcon qui est vraiment un bouton, ButtonIconTooltip est un div qui contient un bouton -->
+
+<div class="tooltip tooltip-bottom tooltip-neutral" data-tip={tooltip}>
+  <ButtonIcon
+    {icon}
+    class="{classForButton}"
+    {buttonSecondIcon}
+    on:click
+  />
+</div>
