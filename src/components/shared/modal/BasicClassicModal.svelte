a2f1ade826beeea070c81623efbc403a93405ded
Guillaume Valmont <guillaume.valmont@pm.me>
2024-07-09 15:52:59 +0400

    Protection de BasicClassicModal contre les modifications de marge de ses parents

--- src/components/shared/modal/BasicClassicModal.svelte
+++ src/components/shared/modal/BasicClassicModal.svelte
@@ -13,39 +13,43 @@
   $: if (dialog && !isDisplayed) dialog.close()
 </script>
 
-<!-- Ceux qui n'ont pas de souris ont le focus sur le bouton de la croix pour fermer donc ce n'est pas grave s'ils ne peuvent pas fermer en interagissant avec le fond de la modale -->
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<dialog
-  bind:this={dialog}
-  on:click|self={() => dialog.close()}
-  on:close={() => {
-    isDisplayed = false
-    dispatch('close')
-  }}
-  class="rounded-xl
-    w-full md:w-2/3 xl:w-1/2
-    bg-coopmaths-canvas dark:bg-coopmathsdark-canvas"
->
-  <div class="relative p-6 text-center">
-    {#if isWithCloseButton}
-      <ButtonIcon
-      class="absolute top-3 right-3 text-2xl"
-      icon="bx-x"
-      on:click={() => dialog.close()}
-      />
-    {/if}
-    <div class="w-full mb-6
-      text-2xl font-bold
-      text-coopmaths-struct dark:text-coopmathsdark-struct"
-    >
-      <slot name="header" />
+<div class="flex justify-center"> <!-- pour que la modale ne soit pas affectée par les modifications de marge de ses parents -->
+
+  <!-- Ceux qui n'ont pas de souris ont le focus sur le bouton de la croix pour fermer donc ce n'est pas grave s'ils ne peuvent pas fermer en interagissant avec le fond de la modale -->
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+  <dialog
+    bind:this={dialog}
+    on:click|self={() => dialog.close()}
+    on:close={() => {
+      isDisplayed = false
+      dispatch('close')
+    }}
+    class="rounded-xl
+      w-full md:w-2/3 xl:w-1/2
+      bg-coopmaths-canvas dark:bg-coopmathsdark-canvas"
+  >
+    <div class="relative p-6 text-center">
+      {#if isWithCloseButton}
+        <ButtonIcon
+        class="absolute top-3 right-3"
+        buttonClass="text-2xl"
+        icon="bx-x"
+        on:click={() => dialog.close()}
+        />
+      {/if}
+      <div class="w-full mb-4 mt-2
+        text-2xl font-bold
+        text-coopmaths-struct dark:text-coopmathsdark-struct"
+      >
+        <slot name="header" />
+      </div>
+      <div class="w-full">
+        <slot name="content" />
+      </div>
+      <div class="w-full mt-6 mb-3">
+        <slot name="footer" />
+      </div>
     </div>
-    <div class="w-full">
-      <slot name="content" />
-    </div>
-    <div class="w-full mt-6">
-      <slot name="footer" />
-    </div>
-  </div>
-</dialog>
+  </dialog>
+</div>
