a122e2ac6a1bc720379f0cd2f835c517d47146f0
Guillaume Valmont <guillaume.valmont@pm.me>
2024-07-10 12:22:24 +0400

    Tirage du dumb NavBarCapytale

--- src/components/setup/start/presentationalComponents/header/Header.svelte
+++ src/components/setup/start/presentationalComponents/header/Header.svelte
@@ -6,15 +6,7 @@
   import SideMenuWrapper from './SideMenuWrapper.svelte'
   import type { VueType } from '../../../../../lib/types'
   import type { Language } from '../../../../../lib/types/languages'
-    import NavBarIframe from '../../../capytale/NavBarIframe.svelte'
-    import ButtonsDeck from '../../../../shared/ui/ButtonsDeck.svelte'
-    import ButtonIconTooltip from '../../../../shared/forms/ButtonIconTooltip.svelte'
-    import InputText from '../../../capytale/InputText.svelte'
-    import ButtonText from '../../../../shared/forms/ButtonText.svelte'
-    import { canOptions } from '../../../../../lib/stores/canStore'
-    import { exercicesParams, globalOptions } from '../../../../../lib/stores/generalStore'
-    import { mathaleaUpdateExercicesParamsFromUrl } from '../../../../../lib/mathalea'
-    import ButtonWithTooltip from '../../../capytale/ButtonWithTooltip.svelte'
+  import NavBarCapytale from '../../capytale/NavBarCapytale.svelte'
 
   interface SideMenuWrapperComponent extends SvelteComponent {
     toggleMenu: (t: boolean) => void
@@ -34,6 +26,8 @@
   export let urlFeuilleEleve: string
   export let buildUrlAndOpenItInNewTab: (type: 'usual' | 'eleve') => void
   export let showSettingsDialog: () => void
+  export let importExercises: () => void
+  export let isExercisesListEmpty: boolean
 
   let reorderModalDisplayed: boolean
   let sideMenuWrapperComponent: SideMenuWrapperComponent
@@ -50,139 +44,62 @@
 <header
   class="md:sticky md:top-0 md:z-50 flex flex-col scrollbar-hide w-full bg-coopmaths-canvas dark:bg-coopmathsdark-canvas"
 >
-  <!-- Entête -->
-  {#if isNavBarVisible}
-    {#if isCapytale}
-      <div
-        id="headerCapytale"
-        class="bg-coopmaths-canvas dark:bg-coopmathsdark-canvas print-hidden"
-      >
-        <NavBarIframe>
-          <div slot="buttons" class="w-full">
-            <ButtonsDeck class="mt-4 md:mt-0">
-              <div
-                slot="setup-buttons"
-                class="flex flex-row justify-center items-center space-x-4"
-              >
-                <ButtonIconTooltip
-                  icon="bx-zoom-out text-3xl"
-                  tooltip="Réduire la taille du texte"
-                  on:click={() => zoomUpdate('-')}
-                />
-                <ButtonIconTooltip
-                  icon="bx-zoom-in text-3xl"
-                  tooltip="Augmenter la taille du texte"
-                  on:click={() => zoomUpdate('+')}
-                />
-                <ButtonIconTooltip
-                  icon="bx-refresh text-3xl"
-                  tooltip="Nouveaux énoncés"
-                  on:click={newDataForAll}
-                />
-                <ButtonIconTooltip
-                  icon="bx-trash text-3xl"
-                  tooltip="Supprimer tous les exercicess"
-                  on:click={trash}
-                />
-              </div>
-              <div slot="input" class="flex flex-row items-center space-x-4">
-                <InputText
-                  title="Importer les exercices d'une feuille élève"
-                  placeholder="Lien"
-                  bind:value={urlFeuilleEleve}
-                  classAddenda="w-50"
-                />
-                <ButtonText
-                  class="text-sm py-1 px-2 rounded-md h-7"
-                  text="Ajouter"
-                  disabled={urlFeuilleEleve.length === 0}
-                  on:click={() => {
-                    // exemple URL vue élève
-                    // http://localhost:5173/alea/?uuid=01873&id=6C20&uuid=99522&id=6C22&uuid=64422&id=6C23&v=confeleve&v=eleve&title=&es=11101
-                    let url = urlFeuilleEleve.replace('&v=confeleve', '')
-                    url = url.replace('&v=eleve', '&recorder=capytale')
-                    if (url.includes('v=can')) {
-                      $canOptions.isChoosen = true
-                    }
-                    url = url.replace('&v=can', '&recorder=capytale')
-                    url = url.replace(/es=\d/g, 'es=1') // Force la vue 1 page par exercice
-                    if (url.includes('coopmaths.fr/alea')) {
-                      const options = mathaleaUpdateExercicesParamsFromUrl(url)
-                      if (options !== null) {
-                        globalOptions.update(() => {
-                          return options
-                        })
-                      } else {
-                        alert('URL non valide !')
-                      }
-                      // On maintient Capytale car l'import d'une url non valide créé un objet globalOptions vide
-                      $globalOptions.recorder = 'capytale'
-                    }
-                    urlFeuilleEleve = ''
-                  }}
-                />
-              </div>
-              <div
-                slot="export-buttons"
-                class="flex flex-row justify-center items-center space-x-4"
-              >
-                <ButtonIconTooltip
-                  icon="bx-cog text-3xl"
-                  tooltip="Régler l'affichage du mode élève"
-                  disabled={$exercicesParams.length === 0}
-                  on:click={showSettingsDialog}
-                />
-                <div>
-                  <ButtonWithTooltip
-                    tooltipTitle="Rejoindre MathALÉA"
-                    tooltipPlacement="auto"
-                    class="text-3xl"
-                    isDisabled={$exercicesParams.length === 0}
-                    on:click={() => {
-                      buildUrlAndOpenItInNewTab('usual')
-                    }}
-                    title=""
-                    icon="bx-log-out bx-rotate-180"
-                  />
-                </div>
-              </div>
-            </ButtonsDeck>
-          </div>
-        </NavBarIframe>
-      </div>
-    {:else}
+  {#if isCapytale}
+    <div
+      id="headerCapytale"
+      class="bg-coopmaths-canvas dark:bg-coopmathsdark-canvas print-hidden"
+    >
+      <NavBarCapytale
+        {zoomUpdate}
+        {newDataForAll}
+        {trash}
+        {urlFeuilleEleve}
+        {buildUrlAndOpenItInNewTab}
+        {showSettingsDialog}
+        {importExercises}
+        {isExercisesListEmpty}
+      />
+    </div>
+  {:else}
+    <!-- Entête -->
+    {#if isNavBarVisible}
       <div
         id="headerStart"
         class="bg-coopmaths-canvas dark:bg-coopmathsdark-canvas print-hidden"
       >
-        <NavBar subtitle="Conception de document" subtitleType="design" {locale} {handleLanguage} />
+        <NavBar
+          subtitle="Conception de document"
+          subtitleType="design"
+          {locale}
+          {handleLanguage}
+        />
       </div>
     {/if}
-  {/if}
-  <!-- Barre de boutons si non-smartphone  -->
-  <div
-    class="hidden md:flex {isExerciseDisplayed
-      ? 'xl:h-[50px] md:h-[100px]'
-      : 'h-0'}"
-  >
+    <!-- Barre de boutons si non-smartphone  -->
     <div
-      class={!isExerciseDisplayed
-        ? 'hidden'
-        : 'relative w-full flex flex-col justify-center items-center bg-coopmaths-canvas dark:bg-coopmathsdark-canvas'}
-      id="barre-boutons"
+      class="hidden md:flex {isExerciseDisplayed
+        ? 'xl:h-[50px] md:h-[100px]'
+        : 'h-0'}"
     >
-      <SideMenuWrapper bind:this={sideMenuWrapperComponent} />
-      <HeaderButtons
-        bind:reorderModalDisplayed
-        {zoomUpdate}
-        {setAllInteractive}
-        {newDataForAll}
-        {trash}
-        {setFullScreen}
-        {handleExport}
-      />
+      <div
+        class={!isExerciseDisplayed
+          ? 'hidden'
+          : 'relative w-full flex flex-col justify-center items-center bg-coopmaths-canvas dark:bg-coopmathsdark-canvas'}
+        id="barre-boutons"
+      >
+        <SideMenuWrapper bind:this={sideMenuWrapperComponent} />
+        <HeaderButtons
+          bind:reorderModalDisplayed
+          {zoomUpdate}
+          {setAllInteractive}
+          {newDataForAll}
+          {trash}
+          {setFullScreen}
+          {handleExport}
+        />
+      </div>
     </div>
-  </div>
+  {/if}
 </header>
 
 <ModalReorder {reorderModalDisplayed} />
