b1c0e9e54a33cc402602c3d17dba3f9ce2a39ac6
Guillaume Valmont <guillaume.valmont@pm.me>
2024-07-10 12:20:19 +0400

    Ajout d'un paramètre à ButtonIconTooltip permettant de choisir la position du tooltip

--- src/components/shared/forms/ButtonIconTooltip.svelte
+++ src/components/shared/forms/ButtonIconTooltip.svelte
@@ -2,6 +2,7 @@
   import ButtonIcon from './ButtonIcon.svelte'
   export let icon: string
   export let tooltip: string
+  export let tooltipPosition: 'auto' | 'top' | 'bottom' | 'left' | 'right' = 'bottom'
   export let title: string = ''
   export let disabled: boolean = false
   export let floatUnderText: string = ''
@@ -12,7 +13,7 @@
 
 <!-- À la différence de ButtonIcon qui est vraiment un bouton, ButtonIconTooltip est un div qui contient un bouton -->
 
-<div class="tooltip tooltip-bottom tooltip-neutral" data-tip={tooltip}>
+<div class="tooltip tooltip-{tooltipPosition} tooltip-neutral" data-tip={tooltip}>
   <ButtonIcon
     class="{$$props.class || ''}"
     {icon}
