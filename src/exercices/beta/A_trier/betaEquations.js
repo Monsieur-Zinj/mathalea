d1368f7a114d895b7e7ffe7e633db6023c78f237
lhotejc <lhotejc@gmail.com>
2024-07-19 20:49:08 +0200

    rename outilsMathjs.js en .ts

--- src/exercices/beta/A_trier/betaEquations.js
+++ src/exercices/beta/A_trier/betaEquations.js
@@ -7,7 +7,7 @@ import { colorToLatexOrHTML, fixeBordures, mathalea2d, ObjetMathalea2D } from '.
 import { context } from '../../../modules/context.js'
 import { listeQuestionsToContenu } from '../../../modules/outils.js'
 import { parse, simplify, compare, evaluate } from 'mathjs'
-import { aleaName, aleaExpression, resoudre, toTex, calculer, calculExpression2, resoudreEquation, aleaEquation, expressionLitterale, aleaVariables, traduireProgrammeCalcul, appliquerProgrammeCalcul, remonterProgrammeCalcul, ecrireProgrammeCalcul } from '../../../modules/outilsMathjs.js'
+import { aleaName, aleaExpression, resoudre, toTex, calculer, calculExpression2, resoudreEquation, aleaEquation, expressionLitterale, aleaVariables, traduireProgrammeCalcul, appliquerProgrammeCalcul, remonterProgrammeCalcul, ecrireProgrammeCalcul } from '../../../modules/outilsMathjs.ts'
 import Algebrite from 'algebrite'
 
 // eslint-disable-next-line no-debugger
