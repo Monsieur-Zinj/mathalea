8bb35c5565c6d73f5911340cd2a6e12ed59a3c14
@gainmireille <mireille-m.gain@ac-bordeaux.fr>
2024-08-01 10:17:42 +0200

    4S20-3 : Mise en conformité prgm

--- src/exercices/4e/4S20-3.js
+++ src/exercices/4e/4S20-3.js
@@ -25,8 +25,9 @@ export const refs = {
 export default class nomExercice extends Exercice {
   constructor () {
     super()
-    this.consigne = 'Classer les événéments selon qu’ils sont compatibles, incompatibles, contraires.<br>Remarque : Si deux événéments sont contraires (donc incompatibles), on demande de les placer dans la catégorie contraires.<br>On tire une carte dans un jeu de 32 cartes.'
+    this.consigne = 'Classer les événéments selon qu’ils sont compatibles, incompatibles, contraires.<br>On tire une carte dans un jeu de 32 cartes.'
     this.nbQuestions = 5 // Nombre de questions par défaut
+    this.besoinFormulaireNumerique = ['Vocabulaire', 2, '1 : Contraires-Non contraires\n2 : Compatibles-Incompatibles-Contraires']
   }
 
   nouvelleVersion () {
@@ -35,7 +36,7 @@ export default class nomExercice extends Exercice {
     this.autoCorrection = []
     this.spacing = 1.5
     this.spacingCorr = 1.5
-    const typeDeQuestionsDisponibles = ['type1', 'type2', 'type3', 'type4', 'type5'] // On crée les 5 types de questions
+    const typeDeQuestionsDisponibles = this.sup === 2 ? ['type1', 'type2', 'type3', 'type4', 'type5', 'type6', 'type7'] : ['type6', 'type7', 'type8', 'type9', 'type10', 'type11', 'type12']
     const listeTypeDeQuestions = combinaisonListes(typeDeQuestionsDisponibles, this.nbQuestions) // Tous les types de questions sont posées mais l'ordre diffère à chaque cycle
     let texte = ''
     let texteCorr = ''
@@ -45,7 +46,7 @@ export default class nomExercice extends Exercice {
       const famille = choice(['Carreau', 'Coeur', 'Pique', 'Trèfle'])
       const couleur = choice(['Rouge', 'Noire'])
       const nombre = choice(['As', 'Sept', 'Huit', 'Neuf', 'Dix'])
-      const valeur = choice(['As', 'Sept', 'Huit', 'Neuf', 'Dix', 'Valet', 'Dame', 'Roi'])
+      const valeur = choice(['un As', 'un Sept', 'un Huit', 'un Neuf', 'un Dix', 'un Valet', 'une Dame', 'un Roi'])
       const noir = choice(['Trèfle', 'Pique'])
       const rouge = choice(['Carreau', 'Coeur'])
 
@@ -54,77 +55,158 @@ export default class nomExercice extends Exercice {
         case 'type1':
           if (k === 1) {
             texte += numAlpha(i) + `Les événements "Obtenir ${figure}" et "Obtenir un ${nombre}" sont...<br>`
-            texteCorr += `<br>Les événements "Obtenir ${figure}" et "Obtenir un ${nombre}" sont  ` + texteEnCouleurEtGras('incompatibles') + '.'
+            texteCorr += numAlpha(i) + `Les événements "Obtenir ${figure}" et "Obtenir un ${nombre}" sont  ` + texteEnCouleurEtGras('incompatibles') + '.<br>'
           } else {
             texte += numAlpha(i) + `Les événements "Obtenir un ${nombre}" et "Obtenir ${figure}" sont...<br>`
-            texteCorr += `<br>Les événements "Obtenir un ${nombre}" et "Obtenir ${figure}" sont ` + texteEnCouleurEtGras('incompatibles') + '.'
+            texteCorr += numAlpha(i) + `Les événements "Obtenir un ${nombre}" et "Obtenir ${figure}" sont ` + texteEnCouleurEtGras('incompatibles') + '.<br>'
           }
           bonneReponse = 'incompatibles'
           break
         case 'type2':
           if (k === 1) {
-            texte += numAlpha(i) + `<br>Les événements "Obtenir un ${valeur}" et "Obtenir un ${famille}" sont...<br>`
-            texteCorr += `<br>Les événements "Obtenir un ${valeur}" et "Obtenir un ${famille}" sont ` + texteEnCouleurEtGras('compatibles') + '.'
+            texte += numAlpha(i) + `Les événements "Obtenir un ${valeur}" et "Obtenir un ${famille}" sont...<br>`
+            texteCorr += numAlpha(i) + `Les événements "Obtenir un ${valeur}" et "Obtenir un ${famille}" sont ` + texteEnCouleurEtGras('compatibles') + '.<br>'
           } else {
             texte += numAlpha(i) + `Les événements "Obtenir un ${famille}" et "Obtenir un ${valeur}" sont...<br>`
-            texteCorr += `<br>Les événements "Obtenir un ${famille}" et "Obtenir un ${valeur}" sont ` + texteEnCouleurEtGras('compatibles') + '.'
+            texteCorr += numAlpha(i) + `Les événements "Obtenir un ${famille}" et "Obtenir un ${valeur}" sont ` + texteEnCouleurEtGras('compatibles') + '.<br>'
           }
           bonneReponse = 'compatibles'
           break
         case 'type3':
           if (k === 1) {
             texte += numAlpha(i) + 'Les événements "Obtenir une carte de couleur Noire" et "Obtenir une carte de couleur Rouge" sont...<br>'
-            texteCorr += '<br>Les événements "Obtenir une carte de couleur Noire" et "Obtenir une carte de couleur Rouge" sont ' + texteEnCouleurEtGras('contraires') + '.'
+            texteCorr += numAlpha(i) + 'Les événements "Obtenir une carte de couleur Noire" et "Obtenir une carte de couleur Rouge" sont ' + texteEnCouleurEtGras('contraires') + '.<br>'
           } else {
             texte += numAlpha(i) + 'Les événements "Obtenir une carte de couleur Rouge" et "Obtenir une carte de couleur Noire" sont...<br>'
-            texteCorr += '<br>Les événements "Obtenir une carte de couleur Rouge" et "Obtenir une carte de couleur Noire" sont ' + texteEnCouleurEtGras('contraires') + '.'
+            texteCorr += numAlpha(i) + 'Les événements "Obtenir une carte de couleur Rouge" et "Obtenir une carte de couleur Noire" sont ' + texteEnCouleurEtGras('contraires') + '.<br>'
           }
           bonneReponse = 'contraires'
           break
         case 'type4':
           if (k === 2) {
             texte += numAlpha(i) + `Les événements "Obtenir ${figure}" et "Obtenir une carte de couleur ${couleur}" sont...<br>`
-            texteCorr += `<br>Les événements "Obtenir ${figure}" et "Obtenir une carte de couleur ${couleur}" sont ` + texteEnCouleurEtGras('compatibles') + '.'
+            texteCorr += numAlpha(i) + `Les événements "Obtenir ${figure}" et "Obtenir une carte de couleur ${couleur}" sont ` + texteEnCouleurEtGras('compatibles') + '.<br>'
           } else {
             texte += numAlpha(i) + `Les événements "Obtenir une carte de couleur ${couleur}" et "Obtenir ${figure}" sont...<br>`
-            texteCorr += `<br>Les événements "Obtenir une carte de couleur ${couleur}" et "Obtenir ${figure}" sont ` + texteEnCouleurEtGras('compatibles') + '.'
+            texteCorr += numAlpha(i) + `Les événements "Obtenir une carte de couleur ${couleur}" et "Obtenir ${figure}" sont ` + texteEnCouleurEtGras('compatibles') + '.<br>'
           }
-
           bonneReponse = 'compatibles'
           break
         case 'type5':
           if (k === 2) {
             texte += numAlpha(i) + `Les événements "Obtenir un ${noir}" et "Obtenir une carte de couleur Noire" sont...<br>`
-            texteCorr += `<br>Les événements "Obtenir un ${noir}" et "Obtenir une carte de couleur Noire" sont ` + texteEnCouleurEtGras('compatibles') + '.'
+            texteCorr += numAlpha(i) + `Les événements "Obtenir un ${noir}" et "Obtenir une carte de couleur Noire" sont ` + texteEnCouleurEtGras('compatibles') + '.<br>'
           } else {
             texte += numAlpha(i) + `Les événements "Obtenir un ${rouge}" et "Obtenir une carte de couleur Rouge" sont...<br>`
-            texteCorr += `<br>Les événements "Obtenir un ${rouge}" et "Obtenir une carte de couleur Rouge" sont ` + texteEnCouleurEtGras('compatibles') + '.'
+            texteCorr += numAlpha(i) + `Les événements "Obtenir un ${rouge}" et "Obtenir une carte de couleur Rouge" sont ` + texteEnCouleurEtGras('compatibles') + '.<br>'
           }
           bonneReponse = 'compatibles'
+          break
+        case 'type6':
+          if (k === 1) {
+            texte += numAlpha(i) + 'Les événements "Obtenir un Carreau ou un Coeur" et "Obtenir une carte de couleur Noire" sont...<br>'
+            texteCorr += numAlpha(i) + 'Les événements "Obtenir un Carreau ou un Coeur" et "Obtenir une carte de couleur Noire" sont ' + texteEnCouleurEtGras('compatibles') + '.<br>'
+          } else {
+            texte += numAlpha(i) + 'Les événements "Obtenir un Trèfle ou un Pique" et "Obtenir une carte de couleur Rouge" sont...<br>'
+            texteCorr += numAlpha(i) + 'Les événements "Obtenir un Trèfle ou un Pique" et "Obtenir une carte de couleur Rouge" sont ' + texteEnCouleurEtGras('compatibles') + '.<br>'
+          }
+          bonneReponse = 'contraires'
+          break
+        case 'type7':
+          if (k === 1) {
+            texte += numAlpha(i) + 'Les événements "Obtenir une figure autre qu\'un Roi" et "Obtenir une Dame ou un Valet" sont...<br>'
+            texteCorr += numAlpha(i) + 'Les événements "Obtenir une figure autre qu\'un Roi" et "Obtenir une Dame ou un Valet" sont ' + texteEnCouleurEtGras('contraires') + '.<br>'
+          } else {
+            texte += numAlpha(i) + 'Les événements "Obtenir une figure autre qu\'une Dame" et "Obtenir un Valet ou un Roi" sont...<br>'
+            texteCorr += numAlpha(i) + 'Les événements "Obtenir une figure autre qu\'une Dame" et "Obtenir un Valet ou un Roi" sont ' + texteEnCouleurEtGras('contraires') + '.<br>'
+          }
+          bonneReponse = 'contraires'
+          break
+        case 'type8':
+          if (k === 2) {
+            texte += numAlpha(i) + 'Les événements "Obtenir une carte autre qu\'un Roi" et "Obtenir une Dame ou un Valet" sont...<br>'
+            texteCorr += numAlpha(i) + 'Les événements "Obtenir une carte autre qu\'un Roi" et "Obtenir une Dame ou un Valet" sont ' + texteEnCouleurEtGras('non contraires') + '.<br>'
+          } else {
+            texte += numAlpha(i) + 'Les événements "Obtenir une carte autre qu\'une Dame" et "Obtenir un Valet ou un Roi" sont...<br>'
+            texteCorr += numAlpha(i) + 'Les événements "Obtenir une carte autre qu\'une Dame" et "Obtenir un Valet ou un Roi" sont ' + texteEnCouleurEtGras('non contraires') + '.<br>'
+          }
+          bonneReponse = 'non contraires'
+          break
+        case 'type9':
+          if (k === 1) {
+            texte += numAlpha(i) + `Les événements "Obtenir une carte autre qu' ${figure}" et "Obtenir ${figure}" sont...<br>`
+            texteCorr += numAlpha(i) + `Les événements "Obtenir ${figure}" et "Obtenir une carte autre qu' ${figure}" sont ` + texteEnCouleurEtGras('non contraires') + '.<br>'
+          } else {
+            texte += numAlpha(i) + `Les événements "Obtenir une carte autre qu'un ${nombre} " et "Obtenir un ${nombre}" sont...<br>`
+            texteCorr += numAlpha(i) + `Les événements "Obtenir une carte autre qu'un ${nombre} " et "Obtenir un ${nombre}" sont ` + texteEnCouleurEtGras('non contraires') + '.<br>'
+          }
+          bonneReponse = 'contraires'
+          break
+        case 'type10':
+          if (k === 2) {
+            texte += numAlpha(i) + `Les événements "Ne pas obtenir ${valeur}" et "Obtenir ${valeur}" sont...<br>`
+            texteCorr += numAlpha(i) + `Les événements "Ne pas obtenir ${figure}" et "Obtenir ${figure}" sont ` + texteEnCouleurEtGras('non contraires') + '.<br>'
+          } else {
+            texte += numAlpha(i) + `Les événements "Obtenir ${valeur}" et "Ne pas obtenir ${valeur}" sont...<br>`
+            texteCorr += numAlpha(i) + `Les événements "Obtenir ${valeur}" et "Ne pas obtenir ${valeur}" sont ` + texteEnCouleurEtGras('non contraires') + '.<br>'
+          }
+          bonneReponse = 'contraires'
+          break
+        case 'type11':
+          if (k === 2) {
+            texte += numAlpha(i) + `Les événements "Ne pas obtenir un ${couleur}" et "Obtenir un ${noir}" sont...<br>`
+            texteCorr += numAlpha(i) + `Les événements "Ne pas obtenir un ${couleur}" et "Obtenir un ${noir}" sont ` + texteEnCouleurEtGras('non contraires') + '.<br>'
+          } else {
+            texte += numAlpha(i) + `Les événements "Obtenir un ${noir}" et "Ne pas obtenir un ${couleur}" sont...<br>`
+            texteCorr += numAlpha(i) + `Les événements "Obtenir un ${noir}" et "Ne pas obtenir un ${couleur}" sont ` + texteEnCouleurEtGras('non contraires') + '.<br>'
+          }
+          bonneReponse = 'non contraires'
+          break
+        case 'type12':
+          if (k === 1) {
+            texte += numAlpha(i) + `Les événements "Ne pas obtenir un ${couleur}" et "Obtenir un ${rouge}" sont...<br>`
+            texteCorr += numAlpha(i) + `Les événements "Ne pas obtenir un ${couleur}" et "Obtenir un ${rouge}" sont ` + texteEnCouleurEtGras('non contraires') + '.<br>'
+          } else {
+            texte += numAlpha(i) + `Les événements "Obtenir un ${rouge}" et "Ne pas obtenir un ${couleur}" sont...<br>`
+            texteCorr += numAlpha(i) + `Les événements "Obtenir un ${rouge}" et "Ne pas obtenir un ${couleur}" sont ` + texteEnCouleurEtGras('non contraires') + '.<br>'
+          }
+          bonneReponse = 'non contraires'
           break
       }
       this.autoCorrection[i] = {}
       this.autoCorrection[i].options = { ordered: true }
       this.autoCorrection[i].enonce = `${texte}\n`
-      this.autoCorrection[i].propositions = [
-        {
-          texte: 'compatibles',
-          statut: bonneReponse === 'compatibles'
-        },
-        {
-          texte: 'incompatibles',
-          statut: bonneReponse === 'incompatibles'
-        },
-        {
-          texte: 'contraires',
-          statut: bonneReponse === 'contraires'
-        }
-      ]
+      if (this.sup === 2) {
+        this.autoCorrection[i].propositions = [
+          {
+            texte: 'compatibles',
+            statut: bonneReponse === 'compatibles'
+          },
+          {
+            texte: 'incompatibles',
+            statut: bonneReponse === 'incompatibles' || bonneReponse === 'contraires'
+          },
+          {
+            texte: 'contraires',
+            statut: bonneReponse === 'contraires'
+          }]
+      } else {
+        this.autoCorrection[i].propositions = [
+          {
+            texte: 'contraires',
+            statut: bonneReponse === 'contraires'
+          },
+          {
+            texte: 'non contraires',
+            statut: bonneReponse === 'non contraires'
+          }]
+      }
+
       const props = propositionsQcm(this, i)
       if (this.interactif) {
         texte += props.texte
       }
-      if (this.questionJamaisPosee(i, figure, famille, couleur, nombre, valeur, noir, rouge, k)) { // Si la question n'a jamais été posée, on en créé une autre
+      if (this.questionJamaisPosee(i, figure, famille, couleur, nombre, valeur, noir, rouge, k)) { // Si la question n'a jamais été posée, on en crée une autre
         i++
       }
       cpt++
