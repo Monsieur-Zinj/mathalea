a3ab9b9f2a79f4f72266abd1b2a1ca06103374d8
@gainmireille <mireille-m.gain@ac-bordeaux.fr>
2024-08-02 15:34:19 +0200

    Correctifs/ améliorations de 4S20-1&2&3 + Nouvel exo 4S20-4

--- src/exercices/4e/4S20-2.js
+++ src/exercices/4e/4S20-2.js
@@ -3,8 +3,6 @@ import { choice, combinaisonListes } from '../../lib/outils/arrayOutils'
 import { listeQuestionsToContenu, randint } from '../../modules/outils.js'
 import { texNombre } from '../../lib/outils/texNombre'
 import { propositionsQcm } from '../../lib/interactif/qcm.js'
-import { numAlpha } from '../../lib/outils/outilString'
-
 export const interactifReady = true
 export const interactifType = 'qcm'
 
@@ -26,15 +24,15 @@ export default class nomExercice extends Exercice {
     super()
     this.consigne = 'Classer chaque événement selon qu’il est impossible, certain, ou possible.'
     this.nbQuestions = 5 // Nombre de questions par défaut
+    this.nbQuestionsModifiable = false
+    this.spacing = 1.2
+    this.spacingCorr = 1.2
   }
 
   nouvelleVersion () {
     this.listeQuestions = [] // Liste de questions
     this.listeCorrections = [] // Liste de questions corrigées
     this.autoCorrection = []
-    this.nbQuestionsModifiable = false
-    this.spacing = 1.5
-    this.spacingCorr = 1.5
 
     const typeDeQuestionsDisponibles = ['type1', 'type2', 'type3', 'type4', 'type5'] // On crée les 5 types de questions
     const listeTypeDeQuestions = combinaisonListes(typeDeQuestionsDisponibles, this.nbQuestions) // Tous les types de questions sont posées mais l'ordre diffère à chaque "cycle"
@@ -45,61 +43,80 @@ export default class nomExercice extends Exercice {
 
     for (let i = 0, k, p, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
       let bonneReponse
-
+      k = choice([1, 2, 3, 4])
       p = choice([1, 2, 3, 4]) // p est un petit nombre entier qu'on va enlever à n ou ajouter à p
-
       switch (listeTypeDeQuestions[i]) { // Suivant le type de question, le contenu sera différent
         case 'type1':
-          texte += numAlpha(i) + `Obtenir $ ${texNombre(n - p)}$ est un événement...<br>` // Le LateX entre deux symboles $, les variables dans des ${ }
+          texte = `Obtenir $ ${texNombre(n - p)}$ est un événement...<br>` // Le LateX entre deux symboles $, les variables dans des ${ }
           bonneReponse = 'impossible'
-          texteCorr += numAlpha(i) + `Obtenir $ ${texNombre(n - p)}$ est un événement impossible.<br>`
+          texteCorr = `Obtenir $ ${texNombre(n - p)}$ est un événement impossible.<br>`
           break
         case 'type2':
-          k = choice([1, 2, 3, 4])
           if (k === 1) {
-            texte += numAlpha(i) + 'Obtenir un nombre impair est un événement...<br>'
-            texteCorr += numAlpha(i) + 'Obtenir un nombre impair est un événement possible.<br>'
+            texte = 'Obtenir un nombre impair est un événement...<br>'
+            texteCorr = 'Obtenir un nombre impair est un événement possible.<br>'
           } else if (k === 2) {
-            texte += numAlpha(i) + 'Obtenir un nombre pair est un événement...<br>'
-            texteCorr += numAlpha(i) + 'Obtenir un nombre pair est un événement possible.<br>'
+            texte = 'Obtenir un nombre pair est un événement...<br>'
+            texteCorr = 'Obtenir un nombre pair est un événement possible.<br>'
           } else if (k === 3) {
-            texte += numAlpha(i) + 'Ne pas obtenir un nombre impair est un événement...<br>'
-            texteCorr += numAlpha(i) + 'Ne pas obtenir un nombre impair est un événement possible.<br>'
+            texte = 'Ne pas obtenir un nombre impair est un événement...<br>'
+            texteCorr = 'Ne pas obtenir un nombre impair est un événement possible.<br>'
           } else if (k === 4) {
-            texte += numAlpha(i) + 'Ne pas obtenir un nombre pair est un événement...<br>'
-            texteCorr += numAlpha(i) + 'Ne pas obtenir un nombre pair est un événement possible.<br>'
+            texte = 'Ne pas obtenir un nombre pair est un événement...<br>'
+            texteCorr = 'Ne pas obtenir un nombre pair est un événement possible.<br>'
           }
           bonneReponse = 'possible'
           break
         case 'type3':
-          k = choice([1, 2, 3, 4])
           if (k === 1) {
-            texte += numAlpha(i) + `Obtenir un nombre compris entre $ ${texNombre(n)}$ et $ ${texNombre(m)}$ est un événement...<br>`
-            texteCorr += numAlpha(i) + `Obtenir un nombre compris entre $ ${texNombre(n)}$ et $ ${texNombre(m)}$ est un événement certain.<br>`
+            texte = `Obtenir un nombre compris entre $ ${texNombre(n)}$ et $ ${texNombre(m)}$ est un événement...<br>`
+            texteCorr = `Obtenir un nombre compris entre $ ${texNombre(n)}$ et $ ${texNombre(m)}$ est un événement certain.<br>`
           }
           if (k === 2) {
-            texte += numAlpha(i) + `Obtenir un nombre compris entre $ ${texNombre(m)}$ et $ ${texNombre(n)}$ est un événement...<br>`
-            texteCorr += numAlpha(i) + `Obtenir un nombre compris entre $ ${texNombre(m)}$ et $ ${texNombre(n)}$ est un événement certain.<br>`
+            texte = `Obtenir un nombre compris entre $ ${texNombre(m)}$ et $ ${texNombre(n)}$ est un événement...<br>`
+            texteCorr = `Obtenir un nombre compris entre $ ${texNombre(m)}$ et $ ${texNombre(n)}$ est un événement certain.<br>`
           }
           if (k === 3) {
-            texte += numAlpha(i) + `Obtenir un nombre supérieur à $ ${texNombre(n)}$ et inférieur à $ ${texNombre(m)}$ est un événement...<br>`
-            texteCorr += numAlpha(i) + `Obtenir un nombre supérieur à $ ${texNombre(n)}$ et inférieur à $ ${texNombre(m)}$ est un événement certain.<br>`
+            texte = `Obtenir un nombre supérieur à $ ${texNombre(n)}$ et inférieur à $ ${texNombre(m)}$ est un événement...<br>`
+            texteCorr = `Obtenir un nombre supérieur à $ ${texNombre(n)}$ et inférieur à $ ${texNombre(m)}$ est un événement certain.<br>`
           }
           if (k === 4) {
-            texte += numAlpha(i) + `Obtenir un nombre inférieur à $ ${texNombre(m)}$ et supérieur à $ ${texNombre(n)}$ est un événement...<br>`
-            texteCorr += numAlpha(i) + `Obtenir un nombre inférieur à $ ${texNombre(m)}$ et supérieur à $ ${texNombre(n)}$ est un événement certain.<br>`
+            texte = `Obtenir un nombre inférieur à $ ${texNombre(m)}$ et supérieur à $ ${texNombre(n)}$ est un événement...<br>`
+            texteCorr = `Obtenir un nombre inférieur à $ ${texNombre(m)}$ et supérieur à $ ${texNombre(n)}$ est un événement certain.<br>`
           }
           bonneReponse = 'certain'
           break
         case 'type4':
-          texte += numAlpha(i) + `Obtenir $ ${texNombre(m + p)}$ est un événement...<br>`
+          texte = `Obtenir $ ${texNombre(m + p)}$ est un événement...<br>`
           bonneReponse = 'impossible'
-          texteCorr += numAlpha(i) + `Obtenir $ ${texNombre(m + p)}$ est un événement impossible.<br>`
+          texteCorr = `Obtenir $ ${texNombre(m + p)}$ est un événement impossible.<br>`
           break
         case 'type5':
-          texte += numAlpha(i) + `Obtenir $ ${texNombre(n + p)}$ est un événement...<br>`
+          texte = `Obtenir $ ${texNombre(n + p)}$ est un événement...<br>`
           bonneReponse = 'possible'
-          texteCorr += numAlpha(i) + `Obtenir $ ${texNombre(n + p)}$ est un événement possible.<br>`
+          texteCorr = `Obtenir $ ${texNombre(n + p)}$ est un événement possible.<br>`
+          break
+        case 'type6':
+          if (k === 1) {
+            texte = `Obtenir un nombre supérieur à $ ${texNombre(m)}$ est un événement...<br>`
+            texteCorr = `Obtenir un nombre supérieur à $ ${texNombre(m)}$ est un événement certain.<br>`
+            bonneReponse = 'impossible'
+          }
+          if (k === 2) {
+            texte = `Obtenir un nombre inférieur à $ ${texNombre(n)}$ est un événement...<br>`
+            texteCorr = `Obtenir un nombre inférieur à $ ${texNombre(n)}$ est un événement certain.<br>`
+            bonneReponse = 'impossible'
+          }
+          if (k === 3) {
+            texte = `Obtenir un nombre supérieur à $ ${texNombre(n)}$ est un événement...<br>`
+            texteCorr = `Obtenir un nombre supérieur à $ ${texNombre(n)}$ est un événement certain.<br>`
+            bonneReponse = 'possible'
+          }
+          if (k === 4) {
+            texte = `Obtenir un nombre inférieur à $ ${texNombre(m)}$ est un événement...<br>`
+            texteCorr = `Obtenir un nombre inférieur à $ ${texNombre(m)}$ est un événement certain.<br>`
+            bonneReponse = 'possible'
+          }
           break
       }
       this.autoCorrection[i] = {}
@@ -123,13 +140,12 @@ export default class nomExercice extends Exercice {
       if (this.interactif) {
         texte += props.texte
       }
-      // Si la question n'a jamais été posée, on l'enregistre
-      if (this.questionJamaisPosee(i, texte, texteCorr, k, n, m, p)) { // <- laisser le i et ajouter toutes les variables qui rendent les exercices différents (par exemple a, b, c et d)
-        i++
-      }
+      this.listeQuestions.push(texte)
+      this.listeCorrections.push(texteCorr)
+      i++
       cpt++
-    } this.listeQuestions.push(texte)
-    this.listeCorrections.push(texteCorr)
+    }
+
     listeQuestionsToContenu(this) // On envoie l'exercice à la fonction de mise en page
   }
 }
---
+++ src/exercices/4e/4S20-4.js
@@ -0,0 +1,101 @@
+import Exercice from '../Exercice'
+import { choice, combinaisonListes } from '../../lib/outils/arrayOutils'
+import { listeQuestionsToContenu } from '../../modules/outils.js'
+import { fraction } from '../../modules/fractions'
+
+export const titre = 'Calculer des probabilités.'
+
+export const dateDePublication = '02/08/2024' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
+export const uuid = '24db8'
+export const refs = {
+  'fr-fr': ['4S20-4'],
+  'fr-ch': []
+}
+
+/**
+ * Description didactique de l'exercice Calculer des probabilités avec des cartes à jouer.
+ * @author Mireille Gain
+ * Référence 4S20-3
+*/
+export default class ExerciceQcmStatistiques extends Exercice {
+  constructor () {
+    super()
+    this.consigne = 'On tire une carte dans un jeu de 32 cartes.<br>Calculer la probabilité d\'obtenir chacun des événements suivants.'
+    this.nbQuestions = 5 // Nombre de questions par défaut
+    this.nbQuestionsModifiable = false
+    this.spacing = 1.2
+    this.spacingCorr = 1.2
+  }
+
+  nouvelleVersion () {
+    this.listeQuestions = [] // Liste de questions
+    this.listeCorrections = [] // Liste de questions corrigées
+
+    const typeDeQuestionsDisponibles = ['type1', 'type2', 'type3', 'type4', 'type5', 'type6', 'type7', 'type8']
+    const listeTypeDeQuestions = combinaisonListes(typeDeQuestionsDisponibles, this.nbQuestions) // Tous les types de questions sont posées mais l'ordre diffère à chaque cycle
+    let texte = ''
+    let texteCorr = ''
+    const a = fraction(1, 2)
+    const b = fraction(16, 32)
+    const c = fraction(1, 4)
+    const d = fraction(8, 32)
+    const e = fraction(1, 32)
+    const f = fraction(2, 32)
+    const g = fraction(1, 16)
+    const h = fraction(4, 32)
+    const p = fraction(1, 8)
+    const j = fraction(28, 32)
+    const k = fraction(7, 8)
+    const l = fraction(20, 32)
+    const m = fraction(5, 8)
+    const n = fraction(30, 32)
+    const o = fraction(15, 16)
+    for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+      const famille = choice(['Carreau', 'Coeur', 'Pique', 'Trèfle'])
+      const couleur = choice(['Rouge', 'Noire'])
+      const valeur = choice(['un As', 'un Sept', 'un Huit', 'un Neuf', 'un Dix', 'un Valet', 'une Dame', 'un Roi'])
+      const valeur2 = choice(['l\'As', 'le Sept', 'le Huit', 'le Neuf', 'le Dix', 'le Valet', 'la Dame', 'le Roi'])
+
+      switch (listeTypeDeQuestions[i]) { // Suivant le type de question, le contenu sera différent
+        case 'type1':
+          texte = `Tirer une carte de couleur ${couleur}.<br>`
+          texteCorr = `La probabilité de tirer une carte de couleur ${couleur} est de $${b.texFraction}$ ou encore $${a.texFraction}$.`
+          break
+        case 'type2':
+          texte = `Tirer un ${famille}.<br>`
+          texteCorr = `La probabilité de tirer un ${famille} est de $${d.texFraction}$ ou encore $${c.texFraction}$.`
+          break
+        case 'type3':
+          texte = `Tirer ${valeur2} de ${famille}.<br>`
+          texteCorr = `La probabilité de tirer ${valeur2} de ${famille} est de $${e.texFraction}$.`
+          break
+        case 'type4':
+          texte = `Tirer ${valeur} de couleur ${couleur}.<br>`
+          texteCorr = `La probabilité de tirer ${valeur} de couleur ${couleur} est de $${f.texFraction}$ ou encore $${g.texFraction}$.`
+          break
+        case 'type5':
+          texte = `Tirer ${valeur}.<br>`
+          texteCorr = `La probabilité de tirer ${valeur} est de $${h.texFraction}$ ou encore $${p.texFraction}$.`
+          break
+        case 'type6':
+          texte = `Tirer une autre carte qu'${valeur}.<br>`
+          texteCorr = `La probabilité de tirer une autre carte qu'${valeur} est de $${j.texFraction}$ ou encore $${k.texFraction}$.`
+          break
+        case 'type7':
+          texte = 'Tirer une carte qui ne soit pas une figure.<br>'
+          texteCorr = `La probabilité de tirer une carte qui ne soit pas une figure est de $${l.texFraction}$ ou encore $${m.texFraction}$.`
+          break
+        case 'type8':
+          texte = `Tirer une carte autre qu'${valeur} de couleur ${couleur}.<br>`
+          texteCorr = `La probabilité de tirer une carte autre qu'${valeur} de couleur ${couleur} est de $${n.texFraction}$ ou encore $${o.texFraction}$.`
+          break
+      }
+      this.listeQuestions.push(texte)
+      this.listeCorrections.push(texteCorr)
+      i++
+      cpt++
+    }
+
+    listeQuestionsToContenu(this) // On envoie l'exercice à la fonction de mise en page
+  }
+}
