a1373fee5c11ec637a4a3edcd2855050487a78b8
Matthieu Devillers <matthieu.devillers@ac-rennes.fr>
2024-10-06 18:01:56 +0200

    lastCommitBeforeDeparture

--- src/exercices/4e/4C11-1.ts
+++ src/exercices/4e/4C11-1.ts
@@ -1,7 +1,7 @@
 import Exercice from '../Exercice'
 import { listeQuestionsToContenu, randint } from '../../modules/outils.js'
-import { choice, combinaisonListes, combinaisonListesSansChangerOrdre } from '../../lib/outils/arrayOutils'
-import { ecritureParentheseSiMoins, ecritureParentheseSiNegatif } from '../../lib/outils/ecritures'
+import { choice, combinaisonListes } from '../../lib/outils/arrayOutils'
+import { ecritureParentheseSiNegatif } from '../../lib/outils/ecritures'
 // export const dateDeModificationImportante = '29/12/2034'
 
 /**
@@ -10,7 +10,9 @@ import { ecritureParentheseSiMoins, ecritureParentheseSiNegatif } from '../../li
  * Auteur : Matthieu DEVILLERS matthieu.devillers@ac-rennes.fr
 */
 import { handleAnswers } from '../../lib/interactif/gestionInteractif.js' // fonction qui va préparer l'analyse de la saisie
-import { ajouteChampTexteMathLive, remplisLesBlancs } from '../../lib/interactif/questionMathLive.js'
+import { ajouteChampTexte, ajouteChampTexteMathLive, remplisLesBlancs } from '../../lib/interactif/questionMathLive.js'
+import { createList } from '../../lib/format/lists'
+import { KeyboardType } from '../../lib/interactif/claviers/keyboard'
 
 export const titre = 'Résoudre un problème avec les relatifs'
 export const dateDePublication = '05/10/2024' // fonctions de mise en place des éléments interactifs
@@ -24,7 +26,7 @@ export const refs = {
   'fr-ch': []
 }
 
-export default class nomExercice extends Exercice {
+export default class resoudreProblemeRelatifs extends Exercice {
   constructor () {
     super()
 
@@ -35,21 +37,42 @@ export default class nomExercice extends Exercice {
     this.consigne = ''
     // const typeQuestionsDisponibles = ['type1', 'type2', 'type3']
     // const listeTypeQuestions = combinaisonListesSansChangerOrdre(typeQuestionsDisponibles, this.nbQuestions)
+    this.nbCols = 1 // Uniquement pour la sortie LaTeX
+    this.nbColsCorr = 1 // Uniquement pour la sortie LaTeX
+    // this.tailleDiaporama = 2 // Pour les exercices chronométrés. 50 par défaut pour les exercices avec du texte
+    this.spacing = 1.5 // Interligne des questions
+    this.spacingCorr = 1.5// Interligne des réponses
+    /*
+    function solutionsScoreNul (nombQuestions, nombPoints) {
+      let solutions: number[]
+      for (let n = 0; n < nombQuestions; n++) {
+        for (let m = 0; m < (nombQuestions - n); m++) {
+          if ((n * nombPoints[0] - m * nombPoints[1] - (20 - n - m) * nombPoints[2]) === 0) {
+            solutions.push(n)
+            solutions.push(m)
+            solutions.push(20 - n - m)
+          }
+        }
+      }
+      return solutions
+    }
+*/
 
     for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
       const nombreQuestions = choice([10, 20, 30])
       const nombresPoints = choice([[5, 3, 2], [7, 5, 3], [11, 7, 5]])
-      const candidats = combinaisonListes(['Pierre', 'Lucie', 'Julie', 'Antoine', 'Léila', 'Gurvan', 'Elisabeth', 'Enora', 'Quentin'], 3)
+      const candidats = combinaisonListes(['Margaux', 'Celestin', 'Maxime', 'Georges', 'Clémentine', 'Éléonore', 'François', 'Martine'], 3)
+      // const scoreNul = 'oui'
 
       let texte = `Dans un jeu télévisé, les candidats doivent répondre à ${nombreQuestions} questions.<br>
       Pour chaque bonne réponse, ils marquent ${nombresPoints[0]} points.<br>
       Pour chaque mauvaise réponse, ils perdent ${nombresPoints[1]} points.<br>
-      Enfin pour une absence de réponse, ils perdent ${nombresPoints[2]} points.<br>`
+      Enfin pour une absence de réponse, ils perdent ${nombresPoints[2]} points.<br><br>`
 
       let texteCorr = ''
       texte += 'a) Quel est le score maximal à ce jeu ? '
-      texte += ajouteChampTexteMathLive(this, 5 * i, 'inline largeur01 college6eme')
-      handleAnswers(this, 5 * i, { reponse: { value: String(nombreQuestions * nombresPoints[0]) } })
+      texte += ajouteChampTexteMathLive(this, 8 * i, 'inline largeur01 college6eme')
+      handleAnswers(this, 8 * i, { reponse: { value: String(nombreQuestions * nombresPoints[0]) } })
       texteCorr = `<br> a) On obtient le score maximal en répondant 
           correctement aux ${nombreQuestions} questions <br> et en marquant ${nombresPoints[0]} points
           à chaque fois. <br>
@@ -57,8 +80,8 @@ export default class nomExercice extends Exercice {
           $\\text{Score maximal} = ${nombreQuestions} \\times ${nombresPoints[0]}$<br>
           $\\phantom{\\text{Score maximal}} = ${nombreQuestions * nombresPoints[0]}$`
       texte += '<br> b) Quel est le score minimal à ce jeu ? '
-      texte += ajouteChampTexteMathLive(this, 5 * i + 1, 'inline largeur01 college6eme')
-      handleAnswers(this, 5 * i + 1, { reponse: { value: String(-nombreQuestions * nombresPoints[1]) } })
+      texte += ajouteChampTexteMathLive(this, 8 * i + 1, 'inline largeur01 college6eme')
+      handleAnswers(this, 8 * i + 1, { reponse: { value: String(-nombreQuestions * nombresPoints[1]) } })
       texteCorr += `<br> b) On obtient le score minimal en répondant 
           faux aux ${nombreQuestions} questions <br> et en marquant ${ecritureParentheseSiNegatif(-nombresPoints[1])} points
           à chaque fois. <br>
@@ -68,8 +91,8 @@ export default class nomExercice extends Exercice {
       texte += `<br> c) ${candidats[0]} a répondu à toutes les questions 
            dont ${nombreQuestions * 0.6} correctement.<br>
            Quel est son score ? `
-      texte += ajouteChampTexteMathLive(this, 5 * i + 2, 'inline largeur01 college6eme')
-      handleAnswers(this, 5 * i + 2, { reponse: { value: String(nombreQuestions * 0.6 * nombresPoints[0] - nombreQuestions * 0.4 * nombresPoints[1]) } })
+      texte += ajouteChampTexteMathLive(this, 8 * i + 2, 'inline largeur01 college6eme')
+      handleAnswers(this, 8 * i + 2, { reponse: { value: String(nombreQuestions * 0.6 * nombresPoints[0] - nombreQuestions * 0.4 * nombresPoints[1]) } })
       texteCorr += `<br> c) ${candidats[0]} a répondu à ${nombreQuestions} questions en tout,
           dont ${nombreQuestions * 0.6} correctement, <br> donc ${candidats[0]} a répondu faux à 
           ${nombreQuestions} - ${nombreQuestions * 0.6} = ${nombreQuestions * 0.4} questions.<br>
@@ -80,8 +103,8 @@ export default class nomExercice extends Exercice {
       texte += `<br> d) ${candidats[1]} n'a répondu qu'à ${nombreQuestions * 0.5} questions et ${nombreQuestions * 0.2}
       de ses réponses sont fausses.<br>
           Quel est son score ? `
-      texte += ajouteChampTexteMathLive(this, 5 * i + 3, 'inline largeur01 college6eme')
-      handleAnswers(this, 5 * i + 3, { reponse: { value: String(nombreQuestions * 0.3 * nombresPoints[0] - nombreQuestions * 0.2 * nombresPoints[1] - nombreQuestions * 0.5 * nombresPoints[2]) } })
+      texte += ajouteChampTexteMathLive(this, 8 * i + 3, 'inline largeur01 college6eme')
+      handleAnswers(this, 8 * i + 3, { reponse: { value: String(nombreQuestions * 0.3 * nombresPoints[0] - nombreQuestions * 0.2 * nombresPoints[1] - nombreQuestions * 0.5 * nombresPoints[2]) } })
       texteCorr += `<br> d) ${candidats[1]} n'a répondu qu'à ${nombreQuestions * 0.5} questions et ${nombreQuestions * 0.2} sont fausses.<br>
         donc ${candidats[1]} a répondu correctement à ${nombreQuestions * 0.5} - ${nombreQuestions * 0.2} = ${nombreQuestions * 0.3} questions.<br>
          Son score est donc : <br>
@@ -90,15 +113,30 @@ export default class nomExercice extends Exercice {
          $\\phantom{\\text{Score}_\\text{ ${candidats[1]}}} = ${nombreQuestions * 0.3 * nombresPoints[0] - nombreQuestions * 0.2 * nombresPoints[1] - nombreQuestions * 0.5 * nombresPoints[2]}$`
 
       texte += `<br> e) ${candidats[2]} a trouvé ${nombreQuestions * 0.7} mauvaises réponses, 
-      et il n'a pas répondu aux autres questions.<br>
+      et ${candidats[2]} n'a pas répondu aux autres questions.<br>
           Quel est son score ? `
-      texte += ajouteChampTexteMathLive(this, 5 * i + 4, 'inline largeur01 college6eme')
-      handleAnswers(this, 5 * i + 4, { reponse: { value: String(-nombreQuestions * 0.7 * nombresPoints[1] - nombreQuestions * 0.3 * nombresPoints[2]) } })
+      texte += ajouteChampTexteMathLive(this, 8 * i + 4, 'inline largeur01 college6eme')
+      handleAnswers(this, 8 * i + 4, { reponse: { value: String(-nombreQuestions * 0.7 * nombresPoints[1] - nombreQuestions * 0.3 * nombresPoints[2]) } })
       texteCorr += `<br> e) ${candidats[2]} a ${nombreQuestions * 0.7} réponses fausses et n'a pas répondu à ${nombreQuestions * 0.3} questions.<br>
          Son score est donc : <br>
          $\\text{Score}_\\text{ ${candidats[2]}} = ${nombreQuestions * 0.7} \\times  ${ecritureParentheseSiNegatif(-nombresPoints[1])} + ${nombreQuestions * 0.3} \\times ${ecritureParentheseSiNegatif(-nombresPoints[2])}$<br>
          $\\phantom{\\text{Score}_\\text{ ${candidats[2]}}} = ${ecritureParentheseSiNegatif(-nombreQuestions * 0.7 * nombresPoints[1])} + ${ecritureParentheseSiNegatif(-nombreQuestions * 0.3 * nombresPoints[2])}$<br>
          $\\phantom{\\text{Score}_\\text{ ${candidats[2]}}} = ${-nombreQuestions * 0.7 * nombresPoints[1] - nombreQuestions * 0.3 * nombresPoints[2]}$`
+
+      if (this.interactif) {
+        // Question sans version interactive : il s'agit d'encourager la recherche d'une solution par essai/erreur, et le format interactif ne parait pas le plus adapté.
+        // De plus techniquement, ce n'est suffisament simple pour moi.
+        // Remi m'a suggéré de tenter quelque chose avec "Remplis les blancs" mais je préfère faire une MR en l'état : c'est utiisable.
+      } else {
+        texte += '<br> f) Est-il possible d\'obtenir une score nul à ce jeu ? Si oui, comment ?<br>'
+        /*
+        if (solutionsScoreNul(nombreQuestions, nombresPoints).length === 0) {
+          texteCorr += '<br> Il n\'est pas possible d\'avoir un score nul dans ce jeu'
+        } else {
+          texteCorr += '<br> Il est possible d\'avoir un score nul dans ce jeu : '
+        }
+        **/
+      }
 
       if (this.questionJamaisPosee(i, nombreQuestions, nombresPoints.join(''))) {
         this.listeQuestions.push(texte)
