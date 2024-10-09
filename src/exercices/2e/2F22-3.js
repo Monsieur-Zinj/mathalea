1cd2e247c35ef912dbd6dcfdb2b1e6348cb314b0
Rémi Angot <remi.angot@gmail.com>
2024-10-09 07:13:31 +0200

    2F22-3 retour du champ texte

--- src/exercices/2e/2F22-3.js
+++ src/exercices/2e/2F22-3.js
@@ -7,9 +7,9 @@ import { mathalea2d } from '../../modules/2dGeneralites.js'
 import { gestionnaireFormulaireTexte, listeQuestionsToContenu, randint } from '../../modules/outils.js'
 import { texteEnCouleurEtGras } from '../../lib/outils/embellissements'
 import Exercice from '../Exercice'
-import { ajouteChampTexteMathLive } from '../../lib/interactif/questionMathLive.js'
 import { handleAnswers } from '../../lib/interactif/gestionInteractif'
 import { fonctionComparaison } from '../../lib/interactif/comparisonFunctions'
+import { ajouteChampTexte } from '../../lib/interactif/questionMathLive'
 export const titre = 'Déterminer le tableau de signes d\'une fonction graphiquement'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -175,7 +175,7 @@ export default class BetaModeleSpline extends Exercice {
         texteEnonce += '<br>Le tableau de signes de la fonction $f$ est : <br>'
         texteEnonce += tableauChoisi
         texteEnonce += '<br>Répondre par Oui ou Non. '
-        texteEnonce += ajouteChampTexteMathLive(this, i, 'largeur01 inline')
+        texteEnonce += ajouteChampTexte(this, i)
       }
       let texteCorrection
       texteCorrection = `L'ensemble de définition de $f$ est $[${maSpline.x[0]}\\,;\\,${maSpline.x[maSpline.n - 1]}]$.<br>`
