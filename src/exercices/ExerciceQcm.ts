979965926821eda72319c9af52b695a93143e850
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2024-10-24 05:49:37 +0000

    New qcms

---
+++ src/exercices/ExerciceQcm.ts
@@ -0,0 +1,118 @@
+import qcmCamExport from '../lib/amc/qcmCam'
+import { propositionsQcm } from '../lib/interactif/qcm'
+import Exercice from './Exercice'
+
+// export const uuid = 'UUID à modifier'
+// export const titre = 'Titre de l'exercice à modifier'
+// export const refs = [{'fr-fr',['ref française à renseigner']},{'fr-ch', ['ref suisse à renseigner]}]
+
+export const interactifReady = true
+export const interactifType = 'qcm'
+export const amcReady = 'true'
+export const amcType = 'qcmMono'
+// class à utiliser pour fabriquer des Qcms sans aléatoirisation (en cas d'aléatoirisation, on utilisera ExerciceQcmA à la place)
+class ExerciceQcm extends Exercice {
+  enonce: string
+  reponses: string[]
+  bonneReponse: number
+  correction: string
+  reponsesA?: string[]
+  enonceA?: string
+  correctionA?: string
+  options: {vertical?: boolean, ordered: boolean, lastchoice?: number}
+  constructor () {
+    super()
+    // Il n'est pas prévu d'avoir plus d'une question car ceci est prévu pour un seul énoncé statique à la base même si on pourra changer les valeurs et prévoir une aléatoirisation
+    this.nbQuestions = 1
+    this.nbQuestionsModifiable = false
+
+    // #####################################################################
+    // #### Les propriétés à modifier dans l'instance se trouvent ici ! ####
+    // ####                    Ne pas modifier ce moule !               ####
+    // #####################################################################
+    this.spacing = 1 // à adapter selon le contenu de l'énoncé
+    this.spacingCorr = 3 // idem pour la correction
+    // Les options pour le qcm à modifier éventuellement (vertical à true pour les longues réponses par exemple)
+    this.options = { vertical: false, ordered: false }
+    // Le texte récupéré avant le bloc des réponses (il ne faut pas oublier de doubler les \ du latex et de vérifier que les commandes latex sont supportées par Katex)
+    this.enonce = 'Enoncé de la question'
+    // Ici, on colle les différentes réponses prise dans le latex et on renseigne le statut (bonne réponse = true)
+    this.reponses = [
+      'réponse A',
+      'réponse B',
+      'réponse C',
+      'réponse D'
+    ]
+
+    this.correction = 'La correction'
+    this.bonneReponse = 0 // index de la bonne réponse dans l'array this.reponses
+
+    // ####################################################
+    // #### à partir d'ici, il n'y a rien à modifier ! ####
+    // ####################################################
+  }
+
+  nouvelleVersion () {
+    let texte = this.enonce
+    this.autoCorrection[0] = {}
+    if (this.options != null) {
+      this.autoCorrection[0].options = this.options
+    }
+    const originale = Boolean(this.sup)
+    if (this.enonceA != null) {
+      this.autoCorrection[0].propositions = [
+        {
+          texte: originale ? this.reponses[0] : this.reponsesA![0],
+          statut: this.bonneReponse === 0
+        },
+        {
+          texte: originale ? this.reponses[1] : this.reponsesA![1],
+          statut: this.bonneReponse === 1
+        },
+        {
+          texte: originale ? this.reponses[2] : this.reponsesA![2],
+          statut: this.bonneReponse === 2
+        },
+        {
+          texte: originale ? this.reponses[3] : this.reponsesA![3],
+          statut: this.bonneReponse === 3
+        }
+      ]
+    } else {
+      this.autoCorrection[0].propositions = [
+        {
+          texte: this.reponses[0],
+          statut: this.bonneReponse === 0
+        },
+        {
+          texte: this.reponses[1],
+          statut: this.bonneReponse === 1
+        },
+        {
+          texte: this.reponses[2],
+          statut: this.bonneReponse === 2
+        },
+        {
+          texte: this.reponses[3],
+          statut: this.bonneReponse === 3
+        }
+      ]
+    }
+
+    const monQcm = propositionsQcm(this, 0)
+    texte += monQcm.texte
+
+    // Ici on colle le texte de la correction à partir du latex d'origine (vérifier la compatibilité Katex et doubler les \)s
+    const texteCorr = `${monQcm.texteCorr}${originale ? this.correction : this.correctionA!}`
+
+    this.listeQuestions[0] = texte
+    this.listeCorrections[0] = texteCorr
+  }
+
+  // Pour permettre d'exporter tous les qcm pour en faire des séries de questions pour QcmCam. Ne pas y toucher
+  qcmCamExport (): string {
+    return qcmCamExport(this)
+  }
+}
+
+export default ExerciceQcm
---
+++ src/exercices/QCMBac/modele_polynesieJuin2024Ex2Q2.ts
@@ -0,0 +1,49 @@
+import { repere } from '../../lib/2d/reperes'
+import { Spline } from '../../lib/mathFonctions/Spline'
+import { fixeBordures, mathalea2d } from '../../modules/2dGeneralites'
+import ExerciceQcm from '../ExerciceQcm'
+
+export const uuid = 'PJE2Q2'
+export const interactifReady = true
+export const interactifType = 'qcm'
+export const amcReady = 'true'
+export const amcType = 'qcmMono'
+/**
+ * Ceci est un exo construit à partir d'une question de qcm de Bac.
+ * Il utilise la classe ExerciceQcm qui définit les contours de l'exo (sans version aléatoire)
+ * Ce moule à exo dispose d'une méthode qcmCamExport qui permet de récupérer le JSON de la question et de la reponse pour qcmCam.
+ * Il est interactif et dispose d'un export AMC d'office
+ */
+class PolynesieJuin2024Ex2Q2 extends ExerciceQcm {
+  constructor () {
+    super()
+    this.reponses = [
+      '$0 \\leqslant I \\leqslant 4$',
+      '$1 \\leqslant I \\leqslant 5$',
+      '$5 \\leqslant I \\leqslant 10$',
+      '$10 \\leqslant I \\leqslant 15$'
+    ]
+    let texte = 'La courbe d\'une fonction $f$ définie sur $[~0~;~+\\infty~[$ est donnée ci-dessous.:<br>'
+    const maSpline = new Spline(
+      [
+        { x: 0, y: 0, deriveeGauche: 6, deriveeDroit: 6, isVisible: false },
+        { x: 1, y: 3, deriveeGauche: 1, deriveeDroit: 1, isVisible: false },
+        { x: 2, y: 3, deriveeGauche: -0.5, deriveeDroit: -0.5, isVisible: false },
+        { x: 3, y: 2, deriveeGauche: -1, deriveeDroit: -1, isVisible: false },
+        { x: 4, y: 1, deriveeGauche: -0.5, deriveeDroit: -0.5, isVisible: false },
+        { x: 5, y: 0.5, deriveeGauche: -0.25, deriveeDroit: -0.25, isVisible: false },
+        { x: 6, y: 0.3, deriveeGauche: -0.1, deriveeDroit: -0.1, isVisible: false }
+      ]
+    )
+    const objets = [
+      maSpline.courbe({ color: 'red', epaisseur: 2 }),
+      repere({ xMin: 0, yMin: 0, xMax: 6, yMax: 4 })
+    ]
+    texte += mathalea2d(Object.assign({ pixelsParCm: 40, scale: 2 }, fixeBordures(objets)), objets)
+    texte += 'Un encadrement de l\'intégrale $I = \\displaystyle\\int_1^5 f(x) \\:\\text{d}x$ est :<br>'
+    this.enonce = texte
+    this.correction = `Le dessin est clair : la fonction est positive sur l'intervalle $[~1~;~5~]$.<br>L'intégrale est (en unités d'aire) la mesure de la surface limitée par la représentation graphique de $f$, l'axe des abscisses et les droites d'équations $x = 1$ et $x = 5$.<br>
+La surface grise contient les 5 carreaux hachurés et est inscrite dans le polygone de 10 unités en bleu.<br>`
+  }
+}
+export default PolynesieJuin2024Ex2Q2
