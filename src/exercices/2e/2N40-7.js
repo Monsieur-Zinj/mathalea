c240a89b5d3e85e5480d16ae8551ddaada4adf01
Monsieur-Zinj <ludwig.trossel@gmail.com>
2024-11-04 16:31:37 +0100

    formulaire texte

--- src/exercices/2e/2N40-7.js
+++ src/exercices/2e/2N40-7.js
@@ -1,5 +1,5 @@
 import Exercice from '../Exercice'
-import { randint, listeQuestionsToContenu } from '../../modules/outils'
+import { randint, listeQuestionsToContenu, gestionnaireFormulaireTexte } from '../../modules/outils'
 import { handleAnswers } from '../../lib/interactif/gestionInteractif'
 import { ajouteChampTexteMathLive } from '../../lib/interactif/questionMathLive'
 import { fonctionComparaison } from '../../lib/interactif/comparisonFunctions'
@@ -29,7 +29,8 @@ export default class nomExercice extends Exercice {
     this.consigne = 'Calcule'
     this.nbQuestions = 10
 
-    this.besoinFormulaireNumerique = ['Difficulté', 3, '1 : Facile\n2 : Moyen\n3 : Difficile'] // le paramètre sera numérique de valeur max 3 (le 3 en vert)
+    // this.besoinFormulaireTexte= ['Difficulté', 3, '1 : Facile\n2 : Moyen\n3 : Difficile'] // le paramètre sera numérique de valeur max 3 (le 3 en vert)
+    this.besoinFormulaireTexte = ['Niveau de difficulté', 'Nombres séparés par des tirets\n1: k(ax+b)\n2: (ax+b)×k\n3: kx(ax+b)\n4: (ax+b)×kx\n5: k(ax+b)+c\n6: c+k(ax+b)\n7: Mélange']
     this.sup = 2 // Valeur du paramètre par défaut
     // Remarques : le paramètre peut aussi être un texte avec : this.besoinFormulaireTexte = [texte, tooltip]
     //              il peut aussi être une case à cocher avec : this.besoinFormulaireCaseACocher = [texte] (dans ce cas, this.sup = true ou this.sup = false)
@@ -40,15 +41,30 @@ export default class nomExercice extends Exercice {
     this.listeCorrections = []
     this.autoCorrection = []
 
+    const typesDeQuestionsDisponibles = [1, 2, 3]
+    const listeTypeDeQuestions = gestionnaireFormulaireTexte({
+      saisie: this.sup,
+      min: 1,
+      max: 3,
+      defaut: 2,
+      listeOfCase: typesDeQuestionsDisponibles,
+      nbQuestions: this.nbQuestions,
+      melange: 4
+    })
+
     for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) {
       const a = randint(1, 12) // Comme la valeur ne sera pas modifiée, on la déclare avec const
       let NombreAAjouter // Comme la valeur sera modifiée, on la déclare avec let
-      if (this.sup === 1) {
-        NombreAAjouter = 2
-      } else if (this.sup === 2) {
-        NombreAAjouter = 5
-      } else {
-        NombreAAjouter = 100
+      switch (listeTypeDeQuestions[i]) {
+        case 1:
+          NombreAAjouter = 2
+          break
+        case 2:
+          NombreAAjouter = 5
+          break
+        case 3:
+          NombreAAjouter = 100
+          break
       }
       texte = `$${a} + ${NombreAAjouter} $`
       texte = `$${texte}$`
